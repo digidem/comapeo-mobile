6302ac0d46771edc387f0a774d16a5938e0b8618
Andrew Chou <andrewchou@fastmail.com>
2022-11-29 11:22:05 -0500

    basic nodejs mobile working

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,120 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import * as React from 'react';
+import nodejs from 'nodejs-mobile-react-native';
+import {SafeAreaView, Button, TextInput} from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [messageText, setMessageText] = React.useState('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const channel = useNodejsMobile();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
+      <TextInput
+        onChangeText={setMessageText}
+        value={messageText}
+        style={{
+          backgroundColor: 'white',
+          borderColor: 'black',
+          color: 'black',
+        }}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Button
+        title="Send message"
+        disabled={messageText.length === 0}
+        onPress={() => channel.send(messageText)}
+      />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+function useNodejsMobile() {
+  React.useEffect(() => {
+    nodejs.start('main.js');
+
+    const subscription = nodejs.channel.addListener('message', msg => {
+      console.log('RECEIVED MESSAGE', msg);
+    });
+
+    return () => {
+      // @ts-expect-error
+      subscription.remove();
+    };
+  }, []);
+
+  return {
+    send: (msg: string) => nodejs.channel.send(msg),
+  };
+}
 
 export default App;

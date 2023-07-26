15dedb6886ac0e5a39af6f7dc641025425c0dbcd
Tomás Ciccola <tciccola@digital-democracy.com>
2023-07-26 11:43:25 -0300

    added src/utils/mockData.ts

--- src/backend/index.js
+++ src/backend/index.js
@@ -5,6 +5,7 @@ import rn_bridge from 'rn-bridge';
 import {createServer} from 'rpc-reflector';
 import {MapeoClient} from './mapeo-core/index';
 import MessagePortLike from './message-port-like.js';
+import {mockData} from '../utils/mockData.js';
 
 // TODO: Account for args passed from node.startWithArgs
 debug.enable('*');
@@ -14,6 +15,13 @@ const channel = new MessagePortLike(rn_bridge.channel);
 
 try {
   const mapeoClient = new MapeoClient();
+  mockData.forEach(async doc => {
+    try {
+      await mapeoClient.observation.create(doc);
+    } catch (e) {
+      console.error('error creating the document', e);
+    }
+  });
   const {close} = createServer(mapeoClient, channel);
 } catch (e) {
   rn_bridge.channel.send(`error initializing rpc-reflector: ${e}`);

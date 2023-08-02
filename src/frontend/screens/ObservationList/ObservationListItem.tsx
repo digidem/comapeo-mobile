99f962dde2d1825b002c5c9d289c6777db8c4931
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-08-02 09:39:55 -0700

    chore: observation screens

---
+++ src/frontend/screens/ObservationList/ObservationListItem.tsx
@@ -0,0 +1,91 @@
+import * as React from "react";
+import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+
+import { useObservation } from "../../hooks/useObservation";
+
+
+interface ObservationListItemProps {
+  observationId: string;
+  testID: string;
+  onPress: (id: string) => void;
+}
+
+const ObservationListItem = ({
+  observationId,
+  testID,
+  onPress = () => {},
+}: ObservationListItemProps) => {
+  const {data:observation} = useObservation(observationId);
+
+  console.log(observation)
+  
+  if (!observation) return null; // Should never get here!
+
+  const isMine = false
+  return (
+    <TouchableHighlight
+      onPress={() => onPress(observationId)}
+      testID={testID}
+      style={{ flex: 1, height: 80 }}
+    >
+      <View
+        style={[styles.container, !isMine && styles.syncedObservation]}
+      >
+        <View style={styles.text}>
+          <Text style={styles.title}>
+            {/* @ts-ignore */}
+            {observation['preset']}
+          </Text>
+          <Text>
+            {/* @ts-ignore */}
+            {observation['date']}
+          </Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+export default React.memo<ObservationListItemProps>(ObservationListItem);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    borderBottomColor: "#EAEAEA",
+    borderBottomWidth: 1,
+    width: "100%",
+    paddingHorizontal: 20,
+    flex: 1,
+    height: 80,
+  },
+  syncedObservation: {
+    borderLeftWidth: 5,
+    borderLeftColor: "#3C69F6",
+  },
+  text: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  title: { fontSize: 18, fontWeight: "700", color: "black" },
+  photoContainer: {
+    position: "relative",
+    marginRight: -5,
+  },
+  photo: {
+    borderRadius: 5,
+    overflow: "hidden",
+    position: "absolute",
+    width: 60,
+    height: 60,
+    top: 0,
+    borderWidth: 1,
+    borderColor: "white",
+    borderStyle: "solid",
+  },
+  smallIconContainer: { position: "absolute", right: -3, bottom: -3 },
+});
--- src/frontend/screens/ObservationList/index.tsx
+++ src/frontend/screens/ObservationList/index.tsx
@@ -1,17 +1,134 @@
+import * as React from "react";
+import { View, FlatList, Dimensions, StyleSheet, Text } from "react-native";
+import { defineMessages, FormattedMessage } from "../../hooks/useReactIntlMock";
+import ObservationListItem from "./ObservationListItem";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AppStackParamList } from "../../navigation/AppScreens";
+import { useObservations } from "../../hooks/useObservations";
+import { MapeoDoc } from "../../../backend/mapeo-core/drivers";
 
-import * as React from 'react'
-import { useObservations } from '../../hooks/useObservations'
+const m = defineMessages({
+  loading: {
+    id: "screens.ObservationsList.loading",
+    defaultMessage:
+      "Loading… this can take a while after synchronizing with a new device",
+    description: "message shown whilst observations are loading",
+  },
+  error: {
+    id: "screens.ObservationsList.error",
+    defaultMessage:
+      "Error loading observations. Try quitting and restarting Mapeo.",
+    description:
+      "message shown when there is an unexpected error when loading observations",
+  },
+});
 
-export const ObservationsList = () => {
-    const {data, isLoading} = useObservations()
+const OBSERVATION_CELL_HEIGHT = 80;
 
-    if (isLoading){
-        return (
+const getItemLayout = (
+  data: MapeoDoc<Object>[] | null | undefined,
+  index: number
+) => ({
+  length: OBSERVATION_CELL_HEIGHT,
+  offset: OBSERVATION_CELL_HEIGHT * index,
+  index,
+});
+
+const keyExtractor = (item: MapeoDoc<Object>) => item.id.toString();
+
+type Props = {
+  // Called when the user presses a list item, called with observation id
+  onPressObservation: (id: string) => void;
+  navigation:NativeStackNavigationProp<AppStackParamList, "ObservationsList", undefined>
+};
+
+/**
+ * Renders a list view of observations
+ */
+const ObservationsListView = ({
+}: Props) => {
+
+    const {data:observations, isLoading, error} = useObservations()
+
+    if(!observations){
+        return(
             <React.Fragment>
-                Loading
+                <Text> No Observations </Text>
             </React.Fragment>
         )
     }
+  const rowsPerWindow = Math.ceil(
+    (Dimensions.get("window").height - 65) / OBSERVATION_CELL_HEIGHT
+  );
 
+  const sortedObservations = React.useMemo(
+    () => observations.sort((a, b) => (a.created_at < b.created_at ? 1 : -1)),
+    [observations]
+  );
 
-}
+  if (isLoading) {
+    return (
+      <View style={styles.messageContainer}>
+        <Text>
+          <FormattedMessage {...m.loading} />
+        </Text>
+      </View>
+    );
+  }
+  
+  if (error) {
+    return (
+      <View style={styles.messageContainer}>
+        <Text>
+          <FormattedMessage {...m.error} />
+        </Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container} testID="observationsListView">
+      <FlatList
+        initialNumToRender={rowsPerWindow}
+        getItemLayout={getItemLayout}
+        keyExtractor={keyExtractor}
+        style={styles.container}
+        windowSize={3}
+        removeClippedSubviews
+        renderItem={({ item, index }) => {
+          return (
+            <ObservationListItem
+              key={item.id}
+              testID={`observationListItem:${index}`}
+              observationId={item.id}
+              onPress={()=>{}}
+            />
+          );
+        }}
+        data={sortedObservations}
+      />
+    </View>
+  );
+};
+
+export default ObservationsListView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  messageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  listItem: {
+    height: OBSERVATION_CELL_HEIGHT,
+  },
+  scrollViewContent: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+  },
+});

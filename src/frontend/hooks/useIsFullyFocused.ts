565af1bad4c9f00c073920814a39fe630f68d800
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-09-04 09:54:17 -0700

    chore: ported over no refactor hooks (#44)
    * chore: added dependencies for hooks
    
    * chore: added useIsFullyFocused hook
    
    * chore: added useIsMounted Hook
    
    * chore: added useWifiStatus hook

---
+++ src/frontend/hooks/useIsFullyFocused.ts
@@ -0,0 +1,44 @@
+import {useEffect, useState} from 'react';
+import {AppState, AppStateStatus} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+/**
+ * Similar to `react-navigation/core`'s [`useIsFocused`](https://reactnavigation.org/docs/use-is-focused/)
+ * but considers the screen to be focused only when:
+ * 1) the screen animation finishes (as opposed to when the animation starts)
+ * 2) the app is not in the background
+ */
+export const useIsFullyFocused = () => {
+  const navigation = useNavigation();
+
+  const [isNavigationFullyFocused, setIsNavigationFullyFocused] = useState(
+    navigation.isFocused(),
+  );
+  const [isAppActive, setIsAppActive] = useState(
+    AppState.currentState === 'active',
+  );
+
+  useEffect(() => {
+    const onAppStateChange = (nextAppState: AppStateStatus) =>
+      setIsAppActive(nextAppState === 'active');
+
+    const changeSubscription = AppState.addEventListener(
+      'change',
+      onAppStateChange,
+    );
+
+    return () => changeSubscription.remove();
+  }, []);
+
+  useEffect(() => {
+    // let navSubscriptions: EventListenerCallback<any, any>  [] = [];
+
+    const navSubscriptions = [
+      navigation.addListener('focus', () => setIsNavigationFullyFocused(true)),
+      navigation.addListener('blur', () => setIsNavigationFullyFocused(false)),
+    ];
+
+    return () => navSubscriptions.forEach(s => s());
+  }, [navigation]);
+
+  return isNavigationFullyFocused && isAppActive;
+};

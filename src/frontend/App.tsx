4a905e984c5db1769a433adeb259675f38404960
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-09-05 13:31:15 -0700

    chore:added sensors

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -7,10 +7,19 @@ import {
 import {AppNavigator} from './Navigation/AppNavigator';
 import {AppStackList} from './Navigation/AppStack';
 import {IntlProvider} from './contexts/IntlContext';
-import {Loading} from './components/Loading';
+// import {Loading} from './components/Loading';
+import {lockAsync, OrientationLock} from 'expo-screen-orientation';
 
 const App = () => {
   const navRef = useNavigationContainerRef<AppStackList>();
+
+  React.useLayoutEffect(() => {
+    async function lockScreenToPortrait() {
+      await lockAsync(OrientationLock.PORTRAIT);
+    }
+
+    lockScreenToPortrait();
+  }, [lockAsync]);
 
   return (
     <IntlProvider>

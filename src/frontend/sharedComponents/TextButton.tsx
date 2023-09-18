288517d87af31f345a8880349c9ce251a6811357
lightlii <32508751+lightlii@users.noreply.github.com>
2023-09-18 16:23:43 +0200

    chore: add Select, TextButton, WifiBar

---
+++ src/frontend/sharedComponents/TextButton.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet} from 'react-native';
+import {Text} from './Text';
+import {TouchableNativeFeedback} from '../sharedComponents/Touchables';
+
+import {VERY_LIGHT_BLUE} from '../lib/styles';
+import type {ViewStyleProp} from '../sharedTypes';
+
+type Props = {
+  onPress: () => void;
+  containerStyle?: ViewStyleProp;
+  textStyle?: ViewStyleProp;
+  title: string;
+  testID?: string;
+  style?: ViewStyleProp;
+};
+
+const TextButton = ({
+  onPress,
+  containerStyle,
+  textStyle,
+  title,
+  testID,
+}: Props) => (
+  <TouchableNativeFeedback
+    style={[styles.buttonContainer, containerStyle]}
+    background={TouchableNativeFeedback.Ripple(VERY_LIGHT_BLUE, true)}
+    onPress={onPress}>
+    <Text style={[styles.buttonText, textStyle]}>{title.toUpperCase()}</Text>
+  </TouchableNativeFeedback>
+);
+
+export default TextButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'blue',
+    fontWeight: '700',
+  },
+});

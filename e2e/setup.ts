8b9d6c0d42168916409c209736d2fe7743421aa2
lightlii <32508751+lightlii@users.noreply.github.com>
2023-09-20 17:28:41 +0200

    chore: wip port detox config(s)

---
+++ e2e/setup.ts
@@ -0,0 +1,8 @@
+/* eslint-env detox/detox, jest/globals */
+
+beforeAll(async () => {
+  await device.launchApp();
+  await device.setURLBlacklist(['.*api.mapbox.com.*']);
+});
+
+export {};
---
+++ jest/setup.js
@@ -0,0 +1,21 @@
+/* eslint-env jest/globals */
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
+import 'react-native-gesture-handler/jestSetup';
+
+// jest.mock("react-native-reanimated", () => {
+//   const Reanimated = require("react-native-reanimated/mock");
+
+//   // The mock for `call` immediately calls the callback which is incorrect
+//   // So we override it with a no-op
+//   Reanimated.default.call = () => {};
+
+//   return Reanimated;
+// });
+
+// As of react-native@0.64.X file has moved
+// jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
+// jest.mock("react-native-device-info", () => mockRNDeviceInfo);
+// jest.mock("../src/frontend/lib/AppInfo.ts");
+// jest.mock("@bugsnag/react-native");
+// jest.mock("ky");

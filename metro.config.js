83aa7c5ea5a6add79da24328b4d8c768313bcf14
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-10-09 13:06:08 -0700

    added support for svgs

--- metro.config.js
+++ metro.config.js
@@ -6,17 +6,26 @@
  */
 
 const exclusionList = require('metro-config/src/defaults/exclusionList');
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    blockList: exclusionList([/nodejs-assets\/.*/, /android\/.*/, /ios\/.*/]),
-  },
-};
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+      blockList: exclusionList([/nodejs-assets\/.*/, /android\/.*/, /ios\/.*/]),
+    },
+  };
+})();

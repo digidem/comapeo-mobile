f2e28f49c709ddc67a0ebea85eda2e7002acf2af
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-10-22 14:13:02 +0900

    rollup plugin for native modules

--- src/backend/scripts/bundle-backend.mjs
+++ src/backend/scripts/bundle-backend.mjs
@@ -5,26 +5,23 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import esmShim from '@rollup/plugin-esm-shim';
+import nativePaths from './rollup-plugin-native-paths.mjs';
 
 const [entry, outfile] = process.argv.slice(2);
 
 /** @type {import('rollup').RollupOptions} */
 const inputOptions = {
-  external: [
-    'rn-bridge',
-    'better-sqlite3',
-    'crc-native',
-    'fs-native-extensions',
-    'quickbit-native',
-    'simdle-native',
-    'sodium-native',
-    'udx-native',
-    'node-gyp-build',
-    'bindings',
-    'napi-macros',
-  ],
+  external: ['rn-bridge'],
   input: entry,
-  plugins: [commonjs(), esmShim(), nodeResolve({preferBuiltins: true}), json()],
+  plugins: [
+    nativePaths(),
+    commonjs({
+      ignoreDynamicRequires: true,
+    }),
+    esmShim(),
+    nodeResolve({preferBuiltins: true}),
+    json(),
+  ],
 };
 
 /** @type {import('rollup').OutputOptions} */

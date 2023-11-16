eb44e3f112c1eda1a6e3aecd28994f270af439a4
Andrew Chou <andrewchou@fastmail.com>
2023-11-16 11:33:27 -0500

    chore: add minification option for bundling backend (#95)

--- src/backend/scripts/bundle-backend.mjs
+++ src/backend/scripts/bundle-backend.mjs
@@ -1,35 +1,45 @@
 #!/usr/bin/env node
-
+import { parseArgs } from 'util'
 import { rollup } from 'rollup'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import esmShim from '@rollup/plugin-esm-shim'
+import { minify } from 'rollup-plugin-esbuild'
 import nativePaths from './rollup-plugin-native-paths.mjs'
 
-const [entry, outfile] = process.argv.slice(2)
+const { values } = parseArgs({
+  options: {
+    entry: { type: 'string' },
+    output: { type: 'string' },
+    minify: { type: 'boolean' },
+  },
+})
 
-/** @type {import('rollup').RollupOptions} */
-const inputOptions = {
-  external: ['rn-bridge'],
-  input: entry,
-  plugins: [
-    nativePaths(),
-    commonjs({
-      ignoreDynamicRequires: true,
-    }),
-    esmShim(),
-    nodeResolve({ preferBuiltins: true }),
-    json(),
-  ],
+const { entry, output, minify: shouldMinify } = values
+
+/** @type {import('rollup').RollupOptions['plugins']} */
+const plugins = [
+  nativePaths(),
+  commonjs({
+    ignoreDynamicRequires: true,
+  }),
+  esmShim(),
+  nodeResolve({ preferBuiltins: true }),
+  json(),
+]
+
+if (shouldMinify) {
+  plugins.push(minify())
 }
 
-/** @type {import('rollup').OutputOptions} */
-const outputOptions = { file: outfile, format: 'esm' }
-
 async function build() {
-  const bundle = await rollup(inputOptions)
-  await bundle.write(outputOptions)
+  const bundle = await rollup({
+    external: ['rn-bridge'],
+    input: entry,
+    plugins,
+  })
+  await bundle.write({ file: output, format: 'esm' })
   await bundle.close()
 }
 

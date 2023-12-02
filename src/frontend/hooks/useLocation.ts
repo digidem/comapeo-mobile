700655edfe0bc66d687ff2f1043bb2eaf773f5b2
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-12-02 13:05:10 -0800

    chore: updated useLocation hook to check for location services after timeout

--- src/frontend/hooks/useLocation.ts
+++ src/frontend/hooks/useLocation.ts
@@ -3,9 +3,8 @@ import CheapRuler from 'cheap-ruler';
 import {
   watchPositionAsync,
   hasServicesEnabledAsync,
-  type LocationCallback,
   type LocationObject,
-  type LocationSubscription,
+  Accuracy,
 } from 'expo-location';
 import React from 'react';
 import {
@@ -14,8 +13,6 @@ import {
   usePermissionContext,
   RESULTS,
 } from '../contexts/PermissionsContext';
-import {storage} from './persistedState/createPersistedState';
-import {Position} from '../sharedTypes';
 
 interface LocationOptions {
   /** Only update location if it has changed by at least this distance in meters (or maxTimeInterval has passed) */
@@ -34,96 +31,37 @@ const MAX_RULER_LAT_DISPLACEMENT = 1;
 // so we need to check it.
 const LOCATION_TIMEOUT = 10000;
 
-const STORE_KEY = '@MapeoPosition@1';
-
 export function useLocation(options: LocationOptions) {
   const {minTimeInterval: timeInterval = 200, ...debounceOptions} = options;
   const [location, setLocation] = React.useState<LocationObject | undefined>();
   const {permissions} = usePermissionContext();
-  const timeoutId = React.useRef<ReturnType<typeof setTimeout>>();
-
-  const [locationServiceEnabled, setLocationServiceEnabled] =
-    React.useState(false);
-  const LocationSubscriptionPromise =
-    React.useRef<Promise<LocationSubscription>>();
 
   const fineLocationPermissionResult = permissions[
     PERMISSIONS.ACCESS_FINE_LOCATION
   ] as PermissionResult;
 
-  const checkLocationServiceEnabled = React.useCallback(async () => {
-    if (!(await hasServicesEnabledAsync())) {
-      setLocationServiceEnabled(false);
-      // If location services are disabled, clear the position stored in state,
-      // so that we don't create observations with a stale position.
-      setLocation(undefined);
-      // if location service is not enabled, check every 10 seconds to determine if it gets turned on again
-      if (timeoutId.current) clearTimeout(timeoutId.current);
-      timeoutId.current = setTimeout(
-        checkLocationServiceEnabled,
-        LOCATION_TIMEOUT,
-      );
-      return;
-    }
-
-    setLocationServiceEnabled(true);
-  }, []);
-
   useFocusEffect(
     React.useCallback(() => {
       if (fineLocationPermissionResult !== RESULTS.GRANTED) return;
 
-      if (!locationServiceEnabled) {
-        unsubLocationPromise();
-        return;
-      }
-
       let ignore = false;
-      LocationSubscriptionPromise.current = watchPositionAsync(
-        {timeInterval},
-        debounceLocation(debounceOptions)(location => {
+      const LocationSubscriptionProm = watchPositionAsync(
+        {accuracy: Accuracy.BestForNavigation},
+        debounceLocation({
+          ...debounceOptions,
+        })(location => {
           if (ignore) return;
-          // The user can turn off location services via the quick settings dropdown
-          // (swiping down from the top of their phone screen) without moving away
-          // from the app. In this case the location will just stop updating and we
-          // won't know why. If we haven't had a location update for a while, we check
-          // on the provider status to see if location services are enabled, so that
-          // we can update the state with the current status
-          if (timeoutId.current) clearTimeout(timeoutId.current);
-          timeoutId.current = setTimeout(
-            checkLocationServiceEnabled,
-            LOCATION_TIMEOUT,
-          );
           setLocation(location);
-          try {
-            storage.set(STORE_KEY, JSON.stringify(location));
-          } catch (err) {
-            // TO DO log error
-            // log('Error writing to storage', err);
-          }
         }),
       );
-
-      function unsubLocationPromise() {
-        if (LocationSubscriptionPromise.current) {
-          LocationSubscriptionPromise.current.then(sub => {
-            sub.remove();
-            LocationSubscriptionPromise.current = undefined;
-          });
-        }
-      }
       return () => {
         ignore = true;
-        unsubLocationPromise();
+        LocationSubscriptionProm.then(sub => sub.remove());
       };
-    }, [locationServiceEnabled]),
+    }, [fineLocationPermissionResult]),
   );
-  return {location, latLon: location ? getCoords(location) : undefined};
-}
 
-export function getLastSavedLocation() {
-  const storageResults = storage.getString(STORE_KEY);
-  return !storageResults ? undefined : (JSON.parse(storageResults) as Position);
+  return {location, latLon: location ? getCoords(location) : undefined};
 }
 
 function debounceLocation({
@@ -131,11 +69,26 @@ function debounceLocation({
   minDistanceInterval = 1,
   maxTimeInterval = 1000,
 }: Omit<LocationOptions, 'minTimeInterval'>) {
-  let lastLocation: LocationObject;
+  let lastLocation: LocationObject | undefined;
   let ruler: any;
   let rulerLat = 0;
-  return function (callback: LocationCallback) {
+  let timeout: ReturnType<typeof setTimeout>;
+  return function (callback: (location: LocationObject | undefined) => any) {
     return function (location: LocationObject) {
+      // The user can turn off location services via the quick settings dropdown
+      // (swiping down from the top of their phone screen) without moving away
+      // from the app. In this case the location will just stop updating and we
+      // won't know why. If we haven't had a location update for a while, we check
+      // on the provider status to see if location services are enabled, so that
+      // we can update the state with the current status
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(async () => {
+        if (!(await hasServicesEnabledAsync())) {
+          lastLocation = undefined;
+          callback(undefined);
+          return;
+        }
+      }, LOCATION_TIMEOUT);
       if (!lastLocation) {
         lastLocation = location;
         callback(location);

ac6c01e405953627db9d82be51a871d36d6636e8
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-12-11 10:32:24 -0800

    chore: update useWifiStatus hook

--- src/frontend/hooks/useWifiStatus.ts
+++ src/frontend/hooks/useWifiStatus.ts
@@ -1,41 +1,13 @@
-import * as React from 'react';
 import NetInfo from '@react-native-community/netinfo';
-import {NetworkInfo} from 'react-native-network-info';
 
-// import bugsnag from "../lib/logger";
+/**
+ * @returns name of wifi or null if user is not connected to wifi
+ */
+export const useWifiName = () => {
+  // netInfo.type will not be 'wifi' if the user is connected to a router that does not connect to the internet (common use case). So intead we look for an ssid.
+  const netInfo = NetInfo.useNetInfo();
 
-const useWifiStatus = () => {
-  const [ssid, setSsid] = React.useState<string | null>(null);
-
-  React.useEffect(() => {
-    const handleConnectionChange = async () => {
-      // NetInfoData does not actually tell us whether wifi is turned on, it just
-      // tells us what connection the phone is using for data. E.g. it could be
-      // connected to a wifi network but instead using 4g for data, in which case
-      // `data.type` will not be wifi. So instead we just use the event listener
-      // from NetInfo, and when the connection changes we look up the SSID to see
-      // whether the user is connected to a wifi network.
-      // TODO: We currently do not know whether wifi is turned off, we only know
-      // whether the user is connected to a wifi network or not.
-      let ssid = null;
-      try {
-        ssid = await NetworkInfo.getSSID();
-      } catch (e) {
-        // bugsnag.notify(e);
-      } finally {
-        // Even if we don't get the SSID, we still want to show that a wifi
-        // network is connected.
-        setSsid(ssid);
-      }
-    };
-
-    // Subscribe to NetInfo to know when the user connects/disconnects to wifi
-    const unsubscribe = NetInfo.addEventListener(handleConnectionChange);
-
-    return () => unsubscribe();
-  }, []);
-
-  return {ssid};
+  return netInfo.details && 'ssid' in netInfo.details
+    ? (netInfo.details.ssid as string)
+    : null;
 };
-
-export default useWifiStatus;

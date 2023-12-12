e4c232b5b83b6fb6960b6449101215b33958e3f6
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-12-11 16:35:22 -0800

    chore: backend rebase fixes

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,19 +2,31 @@ import { parseArgs } from 'util'
 
 import { init } from './src/app.js'
 
-/** @type {import('util').ParseArgsConfig['options']} */
-const options = {
-  version: { type: 'string' },
-}
+// We define this here so we don't need to do additional bundling adjustments to get the path correct when running on the device
+// This assumes that we keep the relevant directory as part of the built assets when building for nodejs mobile
+// (see `KEEP_THESE` variable in build-backend.mjs)
+const MIGRATIONS_FOLDER_PATH = new URL(
+  './node_modules/@mapeo/core/drizzle',
+  import.meta.url,
+).pathname
 
 try {
-  const args = parseArgs({ options })
+  const { values } = parseArgs({
+    options: {
+      version: { type: 'string' },
+      rootKey: { type: 'string' },
+    },
+  })
 
-  const { values } = args
+  if (typeof values.rootKey !== 'string') {
+    throw new Error('backend did not receive root key from front end')
+  }
 
   // Do not await this as we want this to run indefinitely
   init({
-    version: typeof values.version === 'string' ? values.version : undefined,
+    version: values.version,
+    rootKey: Buffer.from(values.rootKey, 'hex'),
+    migrationsFolderPath: MIGRATIONS_FOLDER_PATH,
   }).catch((err) => {
     console.error('Server startup error:', err)
   })

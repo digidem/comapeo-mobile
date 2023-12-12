6c584440c1c4d10b92e36730bb901179c86047fd
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-12-12 10:38:04 +0900

    remove watchPosition version, use polling

--- src/frontend/hooks/useLocationProviderStatus.ts
+++ src/frontend/hooks/useLocationProviderStatus.ts
@@ -2,14 +2,13 @@ import React from 'react';
 import {
   type LocationProviderStatus,
   getProviderStatusAsync,
-  watchPositionAsync,
   useForegroundPermissions,
 } from 'expo-location';
 
 // How frequently to poll the location provider status
 const POLL_PROVIDER_STATUS_INTERVAL = 10_000; // 10 seconds
 
-export function useLocationProviderStatusPolling() {
+export function useLocationProviderStatus() {
   const [providerStatus, setProviderStatus] = React.useState<
     LocationProviderStatus | undefined
   >(undefined);
@@ -18,7 +17,7 @@ export function useLocationProviderStatusPolling() {
   React.useEffect(() => {
     if (!permissions || !permissions.granted) return;
     let ignore = false;
-    function checkProviderStatus() {
+    async function checkProviderStatus() {
       getProviderStatusAsync()
         .then(status => {
           if (ignore) return;
@@ -35,44 +34,6 @@ export function useLocationProviderStatusPolling() {
     return () => {
       clearInterval(intervalId);
       ignore = true;
-    };
-  }, [permissions]);
-
-  return providerStatus;
-}
-
-// How long without a position update before we check the location provider status
-const PROVIDER_STATUS_TIMEOUT = 10_000; // 10 seconds
-
-export function useLocationProviderStatusWatch() {
-  const [providerStatus, setProviderStatus] = React.useState<
-    LocationProviderStatus | undefined
-  >(undefined);
-  const [permissions] = useForegroundPermissions();
-
-  React.useEffect(() => {
-    if (!permissions || !permissions.granted) return;
-    let ignore = false;
-    function checkProviderStatus() {
-      getProviderStatusAsync()
-        .then(status => {
-          if (ignore) return;
-          setProviderStatus(status);
-        })
-        // Shouldn't happen because we check permissions.granted above, but just in case
-        .catch(noop);
-    }
-    checkProviderStatus();
-    let timeoutId = setTimeout(checkProviderStatus, PROVIDER_STATUS_TIMEOUT);
-    const locationSubscriptionProm = watchPositionAsync({}, () => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(checkProviderStatus, PROVIDER_STATUS_TIMEOUT);
-    });
-    // Shouldn't happen because we check permissions.granted above, but just in case
-    locationSubscriptionProm.catch(noop);
-    return () => {
-      ignore = true;
-      clearTimeout(timeoutId);
     };
   }, [permissions]);
 

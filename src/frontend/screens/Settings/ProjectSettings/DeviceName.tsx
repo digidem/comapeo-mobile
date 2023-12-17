51f98061ed3b74833d0e73d5fae5386c7d836b1d
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-12-17 15:48:05 -0800

    chore: added react hook form to deviceName Screen and header

--- src/frontend/screens/Settings/ProjectSettings/DeviceName.tsx
+++ src/frontend/screens/Settings/ProjectSettings/DeviceName.tsx
@@ -1,23 +1,17 @@
 import * as React from 'react';
+import {useForm, Controller} from 'react-hook-form';
 import {Alert, Keyboard, StyleSheet, TextInput} from 'react-native';
 import {NativeNavigationComponent} from '../../../sharedTypes';
 import {defineMessages, useIntl} from 'react-intl';
-import {IconButton} from '../../../sharedComponents/IconButton';
-import {EditIcon, SaveIcon} from '../../../sharedComponents/icons';
 import {Text} from '../../../sharedComponents/Text';
 import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 import {BLACK, MEDIUM_GREY, RED} from '../../../lib/styles';
 import {
   useDeviceInfo,
   useEditDeviceInfo,
-  useOptimisticDeviceName,
 } from '../../../hooks/server/deviceInfo';
 import {Loading} from '../../../sharedComponents/Loading';
-import {
-  useDeviceNameStore,
-  useDeviceNameStoreActions,
-} from '../../../hooks/store/useDeviceNameStore';
-import {useMutationState} from '@tanstack/react-query';
+import {HeaderRightDeviceName} from './HeaderRightDeviceName';
 
 const m = defineMessages({
   title: {
@@ -42,16 +36,38 @@ const m = defineMessages({
   },
 });
 
+type FormData = {
+  name: string;
+};
+
 export const DeviceName: NativeNavigationComponent<'DeviceName'> = ({
   navigation,
 }) => {
-  const isEditting = useDeviceNameStore(store => store.isEditting);
-  const error = useDeviceNameStore(store => store.error);
-  const newName = useDeviceNameStore(store => store.newName);
-  const {setNewName, setIsEditting} = useDeviceNameStoreActions();
-  const updatedName = useOptimisticDeviceName();
+  const [isEditting, setIsEditting] = React.useState(false);
   const deviceInfo = useDeviceInfo();
+  const {mutate, isPending, variables: newDeviceName} = useEditDeviceInfo();
   const {formatMessage: t} = useIntl();
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: {errors},
+  } = useForm<FormData>({defaultValues: {name: ''}});
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderRightDeviceName
+          handleSubmit={handleSubmit(val => {
+            mutate(val.name);
+            setIsEditting(false);
+          })}
+          setIsEditting={setIsEditting}
+          isEditting={isEditting}
+        />
+      ),
+    });
+  }, [navigation, isEditting, handleSubmit, setIsEditting]);
 
   React.useEffect(() => {
     const unsubscribe = navigation.addListener('beforeRemove', e => {
@@ -63,7 +79,7 @@ export const DeviceName: NativeNavigationComponent<'DeviceName'> = ({
       // Prevent default behavior of leaving the screen
       e.preventDefault();
 
-      if (newName.length < 1) {
+      if (watch().name.length < 1) {
         setIsEditting(false);
         return;
       }
@@ -78,7 +94,7 @@ export const DeviceName: NativeNavigationComponent<'DeviceName'> = ({
     });
 
     return () => unsubscribe();
-  }, [navigation, isEditting]);
+  }, [navigation, isEditting, watch]);
 
   return (
     <TouchableWithoutFeedback
@@ -91,31 +107,33 @@ export const DeviceName: NativeNavigationComponent<'DeviceName'> = ({
         deviceInfo.isLoading ? (
           <Loading />
         ) : (
-          <Text>{updatedName ? updatedName : deviceInfo.data?.name}</Text>
+          // Optimistically updates the device name
+          <Text>{isPending ? newDeviceName : deviceInfo.data?.name}</Text>
         )
       ) : (
         <React.Fragment>
-          <TextInput
-            style={{
-              borderColor: error ? RED : MEDIUM_GREY,
-              borderWidth: 1,
-              borderRadius: 5,
-              color: BLACK,
-              paddingStart: 10,
-            }}
-            value={newName || undefined}
-            onChangeText={setNewName}
-            placeholderTextColor={MEDIUM_GREY}
-            // placeholder={deviceInfo.data?.name || undefined}
+          <Controller
+            name="name"
+            control={control}
+            rules={{required: true, maxLength: 60}}
+            render={({field: {value, onChange, onBlur}}) => (
+              <TextInput
+                style={{
+                  borderColor: errors.name ? RED : MEDIUM_GREY,
+                  borderWidth: 1,
+                  borderRadius: 5,
+                  color: BLACK,
+                  paddingStart: 10,
+                }}
+                value={value}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                placeholderTextColor={MEDIUM_GREY}
+                placeholder={deviceInfo.data?.name}
+              />
+            )}
           />
-          <Text
-            style={{
-              alignSelf: 'flex-end',
-              marginTop: 10,
-              color: error ? RED : MEDIUM_GREY,
-            }}>
-            {`${newName ? newName.length : 0}/60`}
-          </Text>
+          <Text>{`${watch().name.length}/60`}</Text>
         </React.Fragment>
       )}
     </TouchableWithoutFeedback>
--- src/frontend/screens/Settings/ProjectSettings/HeaderRightDeviceName.tsx
+++ src/frontend/screens/Settings/ProjectSettings/HeaderRightDeviceName.tsx
@@ -1,36 +1,25 @@
 import * as React from 'react';
 import {useEditDeviceInfo} from '../../../hooks/server/deviceInfo';
-import {
-  useDeviceNameStore,
-  useDeviceNameStoreActions,
-} from '../../../hooks/store/useDeviceNameStore';
 import {IconButton} from '../../../sharedComponents/IconButton';
 import {EditIcon, SaveIcon} from '../../../sharedComponents/icons';
 useEditDeviceInfo;
 
-export const HeaderRightDeviceName = () => {
-  const isEditting = useDeviceNameStore(store => store.isEditting);
-  const newName = useDeviceNameStore(store => store.newName);
-  const {setIsEditting, setError, setNewName} = useDeviceNameStoreActions();
-  const {mutate} = useEditDeviceInfo();
+export const HeaderRightDeviceName = ({
+  isEditting,
+  setIsEditting,
+  handleSubmit,
+}: {
+  isEditting: boolean;
+  setIsEditting: (val: boolean) => void;
+  handleSubmit: () => void;
+}) => {
+  const renderCounter = React.useRef(0);
+  renderCounter.current = renderCounter.current + 1;
+  console.log('from header', renderCounter.current);
 
-  const validateAndMutateName = React.useCallback(() => {
-    const newNameTrimmed = newName.trim();
-    const nameLength = newNameTrimmed.length;
-    if (nameLength < 1 || nameLength > 60) {
-      setError(true);
-      return;
-    }
-
-    mutate(newName);
-    setIsEditting(false);
-    setNewName('');
-  }, [newName]);
   return (
     <IconButton
-      onPress={() =>
-        !isEditting ? setIsEditting(true) : validateAndMutateName()
-      }>
+      onPress={() => (!isEditting ? setIsEditting(true) : handleSubmit())}>
       {!isEditting ? <EditIcon /> : <SaveIcon />}
     </IconButton>
   );

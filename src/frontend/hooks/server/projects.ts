9dd764d66ae685a27945ad1cb8ee259b4cafb3df
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-12-18 15:55:35 -0800

    updated query

--- src/frontend/hooks/server/projects.ts
+++ src/frontend/hooks/server/projects.ts
@@ -1,3 +1,5 @@
+import {useQuery} from '@tanstack/react-query';
+import {useApi} from '../../contexts/ApiContext';
 import {useActiveProjectContext} from '../../contexts/ProjectContext';
 
 export function useUpdateActiveProjectId() {
@@ -8,4 +10,14 @@ export function useUpdateActiveProjectId() {
 export function useProject() {
   const projectContext = useActiveProjectContext();
   return projectContext.project;
+}
+
+export function useAllProjects() {
+  const api = useApi();
+
+  return useQuery({
+    queryFn: async () => await api.listProjects(),
+    queryKey: ['projects'],
+    throwOnError: true,
+  });
 }

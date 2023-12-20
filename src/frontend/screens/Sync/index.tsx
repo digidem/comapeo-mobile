818e72d9825b14b387d6e185f271503164d05628
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-12-19 17:22:52 -0800

    chore: updated ui stying

--- src/frontend/screens/Sync/index.tsx
+++ src/frontend/screens/Sync/index.tsx
@@ -4,12 +4,53 @@ import {useWifiName} from '../../hooks/useWifiStatus';
 import {NativeNavigationComponentNoHeader} from '../../sharedTypes';
 import {Text} from '../../sharedComponents/Text';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import {BLACK} from '../../lib/styles';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {BLACK, WHITE} from '../../lib/styles';
 import {SettingsButton} from '../ObservationsList/SettingsButton';
 import {Button} from '../../sharedComponents/Button';
 import {useProject, useProjectSettings} from '../../hooks/server/projects';
 import {State} from '@mapeo/core/dist/sync/sync-api';
 import {useDeviceInfo} from '../../hooks/server/deviceInfo';
+import {defineMessages, useIntl} from 'react-intl';
+
+const m = defineMessages({
+  sync: {
+    id: 'screens.SyncModal.SyncView.sync',
+    defaultMessage: 'Sync',
+    description: 'Button to start Synchronization',
+  },
+  stop: {
+    id: 'screens.SyncModal.SyncView.stop',
+    defaultMessage: 'Stop',
+    description: 'Button to stop Synchronization',
+  },
+  caughtUp: {
+    id: 'screens.SyncModal.SyncView.caughtUp',
+    defaultMessage: "You're all caught up!",
+  },
+  yourName: {
+    id: 'screens.SyncModal.SyncView.yourName',
+    defaultMessage: 'Your device name is {deviceName}',
+  },
+  connectedPeers: {
+    id: 'screens.SyncModal.SyncView.connectedPeers',
+    defaultMessage: '{numberOfDevices} Devices nearby/connected',
+  },
+  numberWaitingSync: {
+    id: 'screens.SyncModal.SyncView.numberWaitingSync',
+    defaultMessage: '{numberOfDevices} Devices',
+    description: "Sentence is 'x devices' waiting to sync with you",
+  },
+  waitingToSync: {
+    id: 'screens.SyncModal.SyncView.waitingToSync',
+    defaultMessage: 'Waiting to Sync with you',
+    description: "Sentence is x devices 'waiting to sync with you'",
+  },
+  noDevices: {
+    id: 'screens.SyncModal.SyncView.noDevices',
+    defaultMessage: 'No Device are Syncing',
+  },
+});
 
 /**
  *
@@ -27,14 +68,13 @@ export const Sync: NativeNavigationComponentNoHeader<'Sync'> = ({
   const sync = useProject().$sync;
   const projectSettings = useProjectSettings();
   const deviceInfo = useDeviceInfo();
-  const [connectedPeers, setConnectedPeers] = React.useState<number>();
+  const [connectedPeers, setConnectedPeers] = React.useState<number>(0);
   const [isSyncing, setIsSyncing] = React.useState(false);
   const [hasDataToSync, setHasDataToSync] = React.useState<boolean>();
+  const {formatMessage} = useIntl();
 
-  console.log({isSyncing});
   React.useEffect(() => {
     const syncListener = (val: State) => {
-      console.log(val);
       setConnectedPeers(val.connectedPeers);
       setIsSyncing(val.data.syncing);
       setHasDataToSync(val.data.dataToSync);
@@ -70,29 +110,36 @@ export const Sync: NativeNavigationComponentNoHeader<'Sync'> = ({
     <View style={styles.container}>
       <View>
         {projectSettings.data && projectSettings.data.name && (
-          <Text>{projectSettings.data.name}</Text>
+          <Text style={{textAlign: 'center'}}>{projectSettings.data.name}</Text>
         )}
         {deviceInfo.data && deviceInfo.data.name && (
-          <Text>Your device name is {deviceInfo.data.name}</Text>
+          <Text style={{textAlign: 'center'}}>
+            {formatMessage(m.yourName, {deviceName: deviceInfo.data.name})}
+          </Text>
         )}
-        <MaterialIcons name="wifi" size={20} color={BLACK} />
         {connectedPeers !== undefined && (
-          <Text>{`Connected Peers: ${connectedPeers}`}</Text>
+          <View style={styles.flexRow}>
+            <MaterialIcons name="wifi" size={20} color={BLACK} />
+            <Text style={{marginLeft: 10}}>
+              {formatMessage(m.connectedPeers, {
+                numberOfDevices: connectedPeers,
+              })}
+            </Text>
+          </View>
         )}
         {isSyncing ? (
           <View>{/* syncing here */}</View>
         ) : (
-          <View>
-            {hasDataToSync ? (
-              <Text>devices want to sync</Text>
-            ) : (
-              <Text>No data to sync</Text>
-            )}
-          </View>
+          <Text style={{textAlign: 'center'}}>devices want to sync</Text>
         )}
       </View>
 
-      <SyncButton isSyncing={false} hasDataToSync={true} />
+      <SyncButton
+        startSync={sync.start}
+        stopSync={sync.stop}
+        isSyncing={false}
+        hasDataToSync={true}
+      />
     </View>
   );
 };
@@ -100,13 +147,26 @@ export const Sync: NativeNavigationComponentNoHeader<'Sync'> = ({
 const SyncButton = ({
   isSyncing,
   hasDataToSync,
+  stopSync,
+  startSync,
 }: {
   isSyncing: boolean;
   hasDataToSync?: boolean;
+  stopSync: () => void;
+  startSync: () => void;
 }) => {
-  const sync = useProject().$sync;
+  const {formatMessage} = useIntl();
   if (isSyncing) {
-    return <Button onPress={() => sync.stop()}>Stop</Button>;
+    return (
+      <Button variant="outlined" fullWidth onPress={stopSync}>
+        <View style={styles.flexRow}>
+          <MaterialIcons name="stop" color={BLACK} size={40} />
+          <Text style={{fontSize: 16, marginLeft: 5}}>
+            {formatMessage(m.stop)}
+          </Text>
+        </View>
+      </Button>
+    );
   }
 
   if (hasDataToSync === undefined) {
@@ -114,15 +174,36 @@ const SyncButton = ({
   }
 
   if (!hasDataToSync) {
-    return <Button onPress={() => {}}>You're all caught up</Button>;
+    return (
+      <Button fullWidth onPress={() => {}}>
+        {formatMessage(m.caughtUp)}
+      </Button>
+    );
   }
 
-  return <Button onPress={() => sync.start()}>Sync</Button>;
+  return (
+    <Button fullWidth onPress={startSync}>
+      <View style={styles.flexRow}>
+        <MaterialCommunityIcons name="lightning-bolt" color={WHITE} size={25} />
+        <Text style={{fontSize: 16, marginLeft: 5, color: WHITE}}>
+          {formatMessage(m.sync)}
+        </Text>
+      </View>
+    </Button>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 80,
     padding: 20,
+    height: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  flexRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });

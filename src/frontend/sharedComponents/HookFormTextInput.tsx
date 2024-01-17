13938e662e71e66602a12f950a9aa7a6b0468308
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-01-16 18:04:37 -0800

    chore: created input form with validation

--- src/frontend/sharedComponents/HookFormTextInput.tsx
+++ src/frontend/sharedComponents/HookFormTextInput.tsx
@@ -5,17 +5,41 @@ import {
   FieldValues,
   RegisterOptions,
   Path,
+  useWatch,
+  ValidationRule,
+  useFormState,
 } from 'react-hook-form';
-import {TextInput as RNTextInput} from 'react-native';
+import {TextInput as RNTextInput, StyleSheet, View} from 'react-native';
+import {BLACK, LIGHT_GREY, RED} from '../lib/styles';
+import {ErrorIcon} from './icons';
+import {ViewStyleProp} from '../sharedTypes';
+import {Text} from './Text';
+import {MessageDescriptor, useIntl} from 'react-intl';
+
+type RulesWithMessage = {
+  [key in keyof RegisterOptions]?: {
+    value: RegisterOptions[key];
+    message?: MessageDescriptor;
+  };
+};
 
 type TextInputProps<InputFields extends FieldValues> = {
   name: Path<InputFields>;
   control: Control<InputFields>;
-  rules?: RegisterOptions;
+  rulesWithMessage?: RulesWithMessage;
+  style?: ViewStyleProp;
+  showCount?: boolean;
 } & Omit<
   React.ComponentProps<typeof RNTextInput>,
-  'value' | 'onChangeText' | 'onBlur'
+  'value' | 'onChangeText' | 'onBlur' | 'style'
 >;
+
+type CounterProps<InputFields extends FieldValues> = {
+  name: Path<InputFields>;
+  control: Control<InputFields>;
+  maxLength: ValidationRule<number>;
+  showError: boolean;
+};
 
 /**
  *
@@ -24,22 +48,115 @@ type TextInputProps<InputFields extends FieldValues> = {
 export const HookFormTextInput = <InputFields extends FieldValues>({
   name,
   control,
-  rules,
+  rulesWithMessage,
+  style,
+  showCount,
   ...RNtextInputProps
 }: TextInputProps<InputFields>) => {
+  const {formatMessage} = useIntl();
+  const errors = useFormState({control}).errors;
+  const errorType =
+    errors && errors[name] && errors[name]?.type
+      ? (errors[name]?.type as keyof RulesWithMessage)
+      : undefined;
+
+  const errorMessage =
+    errorType && rulesWithMessage
+      ? rulesWithMessage[errorType]?.message
+      : undefined;
+
+  const maxLength = rulesWithMessage && rulesWithMessage['maxLength']?.value;
+
+  const rules = transformRules(rulesWithMessage);
   return (
-    <Controller
-      name={name}
-      control={control}
-      rules={rules}
-      render={({field: {value, onChange, onBlur}}) => (
-        <RNTextInput
-          value={value}
-          onBlur={onBlur}
-          onChangeText={onChange}
-          {...RNtextInputProps}
+    <React.Fragment>
+      <View style={[styles.input, style, errorType ? styles.error : {}]}>
+        <Controller
+          name={name}
+          control={control}
+          rules={rules}
+          render={({field: {value, onChange, onBlur}}) => (
+            <RNTextInput
+              style={[{flex: 1, color: BLACK}]}
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              placeholderTextColor={LIGHT_GREY}
+              {...RNtextInputProps}
+            />
+          )}
         />
-      )}
-    />
+        {errorType && <ErrorIcon style={{position: undefined}} color={RED} />}
+      </View>
+      <View
+        style={[
+          styles.underContainer,
+          {justifyContent: !errorMessage ? 'flex-end' : 'space-between'},
+        ]}>
+        {errorMessage && (
+          <Text style={{color: RED}}>{formatMessage(errorMessage)}</Text>
+        )}
+        {maxLength && showCount && (
+          <Counter
+            showError={errorType === 'maxLength'}
+            control={control}
+            name={name}
+            maxLength={maxLength}
+          />
+        )}
+      </View>
+    </React.Fragment>
   );
 };
+
+// Utility function to transform rulesWithMessage to the correct type
+function transformRules(
+  rawRules?: RulesWithMessage,
+): RegisterOptions | undefined {
+  return rawRules
+    ? Object.fromEntries(
+        Object.entries(rawRules).map(([key, rule]) => [key, rule?.value]),
+      )
+    : undefined;
+}
+
+const Counter = <InputFields extends FieldValues>({
+  maxLength,
+  control,
+  name,
+  showError,
+}: CounterProps<InputFields>) => {
+  const watcher = useWatch({control, name});
+  const inputCount = watcher?.length || 0;
+  const highlightError = showError && inputCount > maxLength;
+
+  return (
+    <Text
+      style={[
+        {color: highlightError ? RED : LIGHT_GREY},
+      ]}>{`${inputCount}/${maxLength}`}</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    paddingLeft: 20,
+    paddingRight: 10,
+    paddingVertical: 10,
+    borderWidth: 2,
+    borderColor: LIGHT_GREY,
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  error: {
+    borderColor: RED,
+  },
+  underContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});

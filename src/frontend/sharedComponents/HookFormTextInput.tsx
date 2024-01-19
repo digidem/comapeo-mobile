a5eebfba866de2b028236937bbb1440b4128c1c1
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-01-18 16:32:34 -0800

    React hook form component (#156)
    * chore: added react-hook-form dependency
    
    * chore: created reusable input component
    
    * chore:updated light grey to match design schema
    
    * chore: created input form with validation
    
    * chore: simplify error handling
    
    * chore: simplified error handling and props
    
    * chore:added back input props
    
    * chore:fix styling
    
    * chore:more styling

---
+++ src/frontend/sharedComponents/HookFormTextInput.tsx
@@ -0,0 +1,133 @@
+import * as React from 'react';
+import {
+  Control,
+  Controller,
+  FieldValues,
+  Path,
+  useWatch,
+  ValidationRule,
+  useFormState,
+  UseControllerProps,
+} from 'react-hook-form';
+import {TextInput as RNTextInput, StyleSheet, View} from 'react-native';
+import {BLACK, LIGHT_GREY, RED} from '../lib/styles';
+import {ErrorIcon} from './icons';
+import {ViewStyleProp} from '../sharedTypes';
+import {Text} from './Text';
+
+type TextInputProps<InputFields extends FieldValues> = {
+  containerStyle?: ViewStyleProp;
+  showCharacterCount?: boolean;
+  control: Control<InputFields>;
+} & Omit<
+  React.ComponentProps<typeof RNTextInput>,
+  'value' | 'onBlur' | 'onChange'
+> &
+  Omit<UseControllerProps<InputFields>, 'control'>;
+
+type CounterProps<InputFields extends FieldValues> = {
+  name: Path<InputFields>;
+  control: Control<InputFields>;
+  maxLength: ValidationRule<number>;
+  isMaxLengthError: boolean;
+};
+
+/**
+ *
+ * Uses React Hook Form. The name and control should come from the `useForm<T>`. More info here https://www.react-hook-form.com/get-started/#ReactNative. This component also has access to all the same props as React Native's `<TextInput/>`
+ */
+export const HookFormTextInput = <InputFields extends FieldValues>({
+  name,
+  control,
+  rules,
+  containerStyle,
+  showCharacterCount,
+  ...RNInputProp
+}: TextInputProps<InputFields>) => {
+  const error = useFormState({control}).errors[name];
+
+  const maxLengthRule = rules ? rules['maxLength'] : undefined;
+  const maxLength =
+    typeof maxLengthRule === 'number' ? maxLengthRule : maxLengthRule?.value;
+  const errorMessage = error?.message;
+
+  return (
+    <View>
+      <View style={[styles.input, containerStyle, error ? styles.error : {}]}>
+        <Controller
+          name={name}
+          control={control}
+          rules={rules}
+          render={({field: {value, onChange, onBlur}}) => (
+            <RNTextInput
+              style={[{flex: 1, color: BLACK}]}
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              {...RNInputProp}
+            />
+          )}
+        />
+        {error && <ErrorIcon style={{position: undefined}} color={RED} />}
+      </View>
+      <View style={styles.underContainer}>
+        <View>
+          {errorMessage && (
+            <Text style={{color: RED}}>{errorMessage.toString()}</Text>
+          )}
+        </View>
+        <View>
+          {maxLength && showCharacterCount && (
+            <Counter
+              isMaxLengthError={error?.type === 'maxLength'}
+              control={control}
+              name={name}
+              maxLength={maxLength}
+            />
+          )}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const Counter = <InputFields extends FieldValues>({
+  maxLength,
+  control,
+  name,
+  isMaxLengthError,
+}: CounterProps<InputFields>) => {
+  const watcher = useWatch({control, name});
+  const inputCount = watcher?.length || 0;
+
+  return (
+    <Text
+      style={[
+        {color: isMaxLengthError ? RED : LIGHT_GREY},
+      ]}>{`${inputCount}/${maxLength}`}</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    paddingLeft: 20,
+    paddingRight: 10,
+    paddingVertical: 10,
+    borderWidth: 2,
+    borderColor: LIGHT_GREY,
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  error: {
+    borderColor: RED,
+  },
+  underContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});

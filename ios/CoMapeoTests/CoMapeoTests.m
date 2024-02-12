e76416b23477115ebfc78092c0d93826c1305544
Andrew Chou <andrewchou@fastmail.com>
2024-02-12 11:52:33 -0500

    chore: update app name to CoMapeo

--- ios/CoMapeoTests/CoMapeoTests.m
+++ ios/CoMapeoTests/CoMapeoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MapeoNextTests : XCTestCase
+@interface CoMapeoTests : XCTestCase
 
 @end
 
-@implementation MapeoNextTests
+@implementation CoMapeoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

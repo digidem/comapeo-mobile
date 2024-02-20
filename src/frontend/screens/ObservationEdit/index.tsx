4729bfa8763aab7eb5ee854f1bed3cbd6a196646
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-02-19 22:58:55 -0800

    chore:added observation fields to navigation

--- src/frontend/screens/ObservationEdit/index.tsx
+++ src/frontend/screens/ObservationEdit/index.tsx
@@ -12,6 +12,8 @@ import {PresetView} from './PresetView';
 import {useBottomSheetModal} from '../../sharedComponents/BottomSheetModal';
 import {ErrorModal} from '../../sharedComponents/ErrorModal';
 import {SaveButton} from './SaveButton';
+import {useDraftObservation} from '../../hooks/useDraftObservation';
+import {DetailsIcon} from '../../sharedComponents/icons';
 
 const m = defineMessages({
   editTitle: {
@@ -29,6 +31,11 @@ const m = defineMessages({
     defaultMessage: 'Add Photo',
     description: 'Button label for adding photo',
   },
+  detailsButton: {
+    id: 'screens.ObservationEdit.ObservationEditView.detailsButton',
+    defaultMessage: 'Add Details',
+    description: 'Button label to add details',
+  },
 });
 
 export const ObservationEdit: NativeNavigationComponent<'ObservationEdit'> & {
@@ -37,7 +44,8 @@ export const ObservationEdit: NativeNavigationComponent<'ObservationEdit'> & {
   const observationId = usePersistedDraftObservation(
     store => store.observationId,
   );
-
+  const {usePreset} = useDraftObservation();
+  const preset = usePreset();
   const isNew = !observationId;
   const {formatMessage: t} = useIntl();
   const {openSheet, sheetRef, isOpen, closeSheet} = useBottomSheetModal({
@@ -67,14 +75,14 @@ export const ObservationEdit: NativeNavigationComponent<'ObservationEdit'> & {
       onPress: handleCameraPress,
     },
   ];
-  // if (preset && preset.fields && preset.fields.length) {
-  //   // Only show the option to add details if preset fields are defined.
-  //   bottomSheetItems.push({
-  //     icon: <DetailsIcon />,
-  //     label: t(m.detailsButton),
-  //     onPress: handleDetailsPress,
-  //   });
-  // }
+  if (preset && preset.fieldIds.length) {
+    // Only show the option to add details if preset fields are defined.
+    bottomSheetItems.push({
+      icon: <DetailsIcon />,
+      label: t(m.detailsButton),
+      onPress: handleDetailsPress,
+    });
+  }
 
   return (
     <View style={styles.container}>

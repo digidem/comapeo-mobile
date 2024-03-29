16d563e10f3f217983619d117d2eb98eb538009e
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-03-28 17:56:44 -0700

    chore: created invite hooks

--- src/frontend/hooks/server/invites.ts
+++ src/frontend/hooks/server/invites.ts
@@ -1,4 +1,4 @@
-import {useSuspenseQuery} from '@tanstack/react-query';
+import {useMutation, useSuspenseQuery} from '@tanstack/react-query';
 import {useApi} from '../../contexts/ApiContext';
 
 export const INVITE_KEY = 'invites';
@@ -9,6 +9,31 @@ export function useInvites() {
     queryKey: [INVITE_KEY],
     queryFn: async () => {
       return await mapeoApi.invite.getPending();
+    },
+  });
+}
+
+export function useAcceptInvite(inviteId: string) {
+  const mapeoApi = useApi();
+  return useMutation({
+    mutationKey: [INVITE_KEY, inviteId],
+    mutationFn: async () => {
+      mapeoApi.invite.accept({inviteId});
+    },
+  });
+}
+
+export function useRejectInvite(inviteId: string) {
+  const mapeoApi = useApi();
+  const invites = useInvites().data;
+  return useMutation({
+    mutationKey: [INVITE_KEY, inviteId],
+    mutationFn: async () => {
+      mapeoApi.invite.reject({inviteId});
+    },
+    onSuccess: () => {
+      //clears all pending invites after user accepts an invite
+      Promise.allSettled(invites.map(inv => mapeoApi.invite.reject(inv)));
     },
   });
 }
---
+++ src/frontend/hooks/useProjectInvite.ts
@@ -0,0 +1,27 @@
+import {Invite} from '@mapeo/core/dist/invite-api';
+import {useAcceptInvite, useInvites, useRejectInvite} from './server/invites';
+
+export function useProjectInvite(): {
+  accept: ReturnType<typeof useAcceptInvite>;
+  reject: ReturnType<typeof useRejectInvite>;
+  resetState: () => void;
+  invite: Invite;
+} {
+  const invites = useInvites().data;
+  // this will eventually sort invite by date
+  const invite = invites[0];
+  const acceptMutation = useAcceptInvite(invite.inviteId);
+  const rejectMutation = useRejectInvite(invite.inviteId);
+
+  const resetState = () => {
+    acceptMutation.reset();
+    rejectMutation.reset();
+  };
+
+  return {
+    accept: acceptMutation,
+    reject: rejectMutation,
+    resetState,
+    invite: invite,
+  };
+}

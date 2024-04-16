465ea18ee3ff68e04edd75cb2757abda76b9bc4f
Bartosz Stefańczyk <bartosz.stefanczyk@swmansion.com>
2024-04-16 14:32:55 +0200

    use luxon for duration formatting

--- src/frontend/hooks/useFormattedTimeSince.ts
+++ src/frontend/hooks/useFormattedTimeSince.ts
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react';
+import {Duration} from 'luxon';
 
 export const useFormattedTimeSince = (start: Date, interval: number) => {
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -10,24 +11,6 @@ export const useFormattedTimeSince = (start: Date, interval: number) => {
     return () => clearInterval(timer);
   }, [interval]);
 
-  return secondsToHMS(
-    Math.floor((currentTime.getTime() - start.getTime()) / 1000),
-  );
-};
-
-const secondsToHMS = (secs: number) => {
-  function z(n: number) {
-    return (n < 10 ? '0' : '') + n;
-  }
-  var sign = secs < 0 ? '-' : '';
-  secs = Math.abs(secs);
-  /* eslint-disable no-bitwise */
-  return (
-    sign +
-    z((secs / 3600) | 0) +
-    ':' +
-    z(((secs % 3600) / 60) | 0) +
-    ':' +
-    z(secs % 60)
-  );
+  const millisPassed = currentTime.getTime() - start.getTime();
+  return Duration.fromMillis(millisPassed).toFormat('hh:mm:ss');
 };

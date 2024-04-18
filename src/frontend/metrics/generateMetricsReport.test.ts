ea4d36b709522e99ef58a80d904b5d44c81e642f
Evan Hahn <me@evanhahn.com>
2024-04-18 21:27:28 +0000

    chore: add app version to metric reports

--- src/frontend/metrics/generateMetricsReport.test.ts
+++ src/frontend/metrics/generateMetricsReport.test.ts
@@ -1,15 +1,23 @@
 import generateMetricsReport from './generateMetricsReport';
 
 describe('generateMetricsReport', () => {
+  const defaultOptions: Parameters<typeof generateMetricsReport>[0] = {
+    packageJson: {version: '1.2.3'},
+  };
+
   it('can be serialized and deserialized as JSON', () => {
-    const report = generateMetricsReport();
+    const report = generateMetricsReport(defaultOptions);
     const actual = JSON.parse(JSON.stringify(report));
     const expected = removeUndefinedEntries(report);
     expect(actual).toEqual(expected);
   });
 
   it('includes a report type', () => {
-    expect(generateMetricsReport().type).toBe('metrics-v1');
+    expect(generateMetricsReport(defaultOptions).type).toBe('metrics-v1');
+  });
+
+  it('includes the app version', () => {
+    expect(generateMetricsReport(defaultOptions).appVersion).toBe('1.2.3');
   });
 });
 
--- src/frontend/metrics/generateMetricsReport.ts
+++ src/frontend/metrics/generateMetricsReport.ts
@@ -1,5 +1,12 @@
-export default function generateMetricsReport() {
+import type {ReadonlyDeep} from 'type-fest';
+
+export default function generateMetricsReport({
+  packageJson,
+}: ReadonlyDeep<{
+  packageJson: {version: string};
+}>) {
   return {
     type: 'metrics-v1',
+    appVersion: packageJson.version,
   };
 }

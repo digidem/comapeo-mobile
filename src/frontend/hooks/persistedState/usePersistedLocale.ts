3a4ce0035feeee051a82b7ddcad22f8bc12be46d
Evan Hahn <me@evanhahn.com>
2024-04-22 15:13:17 +0000

    chore: fix type error in local slice
    This is a types-only change and should have no impact on functionality.
    
    `usePersistedLocale` had a type error because `getLocales()[0]` might be
    undefined. However, the docs for `getLocales()` say that we are
    "guaranteed" to have at least one element, so we can use a non-null
    assertion.

--- src/frontend/hooks/persistedState/usePersistedLocale.ts
+++ src/frontend/hooks/persistedState/usePersistedLocale.ts
@@ -9,7 +9,10 @@ type LocaleSlice = {
 };
 
 const localeSlice: StateCreator<LocaleSlice> = (set, get) => ({
-  locale: getSupportedLocale(getLocales()[0].languageTag) || 'en',
+  // We can use this non-null assertion with `getLocales()` because, according
+  // to [the docs][1], the result is "guaranteed to contain at least 1 element."
+  // [1]: https://github.com/expo/expo/blob/5585320eec9271038cd7c672b4cf9f0e945ca658/packages/expo-localization/src/Localization.ts#L123
+  locale: getSupportedLocale(getLocales()[0]!.languageTag) || 'en',
   setLocale: newlocale => set({locale: newlocale}),
 });
 

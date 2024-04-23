e1ba81667ecb74ecab628045c2bcededbbb93ebd
Evan Hahn <me@evanhahn.com>
2024-04-23 13:05:11 -0500

    chore: add function to generate some of the metrics report (#261)
    This introduces a (dormant) function for generating metrics reports.
    It returns a result like this:
    
        {
          "type": "metrics-v1",
          "appVersion": "0.0.1",
          "os": "android",
          "osVersion": 10,
          "screen": { "width": 1024, "height": 768 },
          "countries": ["CAN", "USA", "MEX"]
        }
    
    This isn't all the metrics we want to collect but I think it's a useful
    starting point.

---
+++ src/frontend/metrics/positionToCountries.test.ts
@@ -0,0 +1,27 @@
+import positionToCountries from './positionToCountries';
+
+describe('positionToCountries', () => {
+  it('returns nothing for invalid values', () => {
+    expect(positionToCountries(-91, 181)).toEqual(new Set());
+    expect(positionToCountries(Infinity, -Infinity)).toEqual(new Set());
+    expect(positionToCountries(NaN, NaN)).toEqual(new Set());
+  });
+
+  it('returns nothing for the middle of the Atlantic ocean', () => {
+    expect(positionToCountries(10, -33)).toEqual(new Set());
+  });
+
+  it('returns Mexico for a point in Mexico City', () => {
+    expect(positionToCountries(19.419914, -99.088059)).toEqual(
+      new Set(['MEX']),
+    );
+  });
+
+  it('returns multiple countries for disputed territories', () => {
+    // [Machias Seal Island][0] is a disputed territory.
+    // [0]: https://en.wikipedia.org/wiki/Machias_Seal_Island
+    expect(positionToCountries(44.5, -67.101111)).toEqual(
+      new Set(['CAN', 'USA']),
+    );
+  });
+});

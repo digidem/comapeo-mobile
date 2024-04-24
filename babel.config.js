f3a63f432b2efeb878c6653a05888a84903586a9
Bartosz Stefańczyk <bartosz@cdfn.dev>
2024-04-24 18:09:59 +0200

    fix order of babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    'transform-inline-environment-variables',
     // react-native-reanimated/plugin has to be last
     'react-native-reanimated/plugin',
-    'transform-inline-environment-variables',
   ],
 };

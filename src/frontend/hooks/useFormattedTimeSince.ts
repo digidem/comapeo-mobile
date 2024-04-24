09c3bc322a083cc06df56d002ba69edca03e1f6e
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-04-24 10:19:51 +0200

    fixed timer nulable case

--- src/frontend/hooks/useFormattedTimeSince.ts
+++ src/frontend/hooks/useFormattedTimeSince.ts
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import {Duration} from 'luxon';
 
-export const useFormattedTimeSince = (start: Date | null, interval: number) => {
+export const useFormattedTimeSince = (start: Date, interval: number) => {
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
@@ -13,10 +13,6 @@ export const useFormattedTimeSince = (start: Date | null, interval: number) => {
     }, interval);
     return () => clearInterval(timer);
   }, [start, interval]);
-
-  if (!start) {
-    return 'Unknown';
-  }
 
   const millisPassed = Math.abs(currentTime.getTime() - start.getTime());
   return Duration.fromMillis(millisPassed).toFormat('hh:mm:ss');

97e7162d84a77871bedbe670b444f7d9e2ddf65f
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-04-26 18:16:38 -0700

    chore: replaced tab enum with type

--- src/frontend/hooks/useCurrentTab.ts
+++ src/frontend/hooks/useCurrentTab.ts
@@ -3,13 +3,27 @@ import {useGPSModalContext} from '../contexts/GPSModalContext';
 import {useTabNavigationStore} from './useTabNavigationStore.ts';
 import {TabName} from '../Navigation/types';
 
+const tabNames: TabName[] = ['Map', 'Camera', 'Tracking', 'ObservationList'];
+
+// Function to check if a given string is a valid TabName
+const isTabName = (name: string | undefined): name is TabName => {
+  return tabNames.includes(name as TabName);
+};
+
 export const useCurrentTab = () => {
   const {setCurrentTab} = useTabNavigationStore();
   const {bottomSheetRef} = useGPSModalContext();
 
   const handleTabPress = ({target}: EventArg<'tabPress', true, undefined>) => {
     const targetTab = target?.split('-')[0];
-    if (targetTab === TabName.Tracking) {
+
+    if (!isTabName(targetTab)) {
+      throw new Error(
+        `Invalid tab name: ${targetTab}, Makes sure tabNames array in useCurrentTab includes all possible names of tabs`,
+      );
+    }
+
+    if (targetTab === 'Tracking') {
       bottomSheetRef.current?.present();
     } else {
       bottomSheetRef.current?.close();

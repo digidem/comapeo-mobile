ade0668af18a941a360105fcb1bee5d3736b76d4
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-04-29 09:54:19 +0200

    add eas different build config

---
+++ app.config.js
@@ -0,0 +1,10 @@
+module.exports = ({config}) => ({
+  ...config,
+  extra: {
+    ...config.extra,
+    eas: {
+      projectId: '2d5b8137-12ec-45aa-9c23-56b6a1c522b7',
+    },
+  },
+  name: 'CoMapeo',
+});

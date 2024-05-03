f9d51661aa91108a46ce61f1f35952d34bd30eb7
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-05-02 17:34:26 -0700

    chore: consolidate providers

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,27 +1,20 @@
 import * as React from 'react';
-import {useNavigationContainerRef} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from '@react-navigation/native';
 import {createMapeoClient} from '@mapeo/ipc';
-
 import {AppNavigator} from './Navigation/AppNavigator';
 import {AppStackList} from './Navigation/AppStack';
 import {IntlProvider} from './contexts/IntlContext';
-import {ApiProvider} from './contexts/ApiContext';
-import {PhotoPromiseProvider} from './contexts/PhotoPromiseContext';
-import {SecurityProvider} from './contexts/SecurityContext';
-import {QueryClient} from '@tanstack/react-query';
 import {MessagePortLike} from './lib/MessagePortLike';
-import {ServerLoading} from './ServerLoading';
-import {ActiveProjectProvider} from './contexts/ProjectContext';
 import {initializeNodejs} from './initializeNodejs';
 import {PermissionsAndroid} from 'react-native';
-import {ExternalProviders} from './contexts/ExternalProviders';
-import {
-  LocalDiscoveryProvider,
-  createLocalDiscoveryController,
-} from './contexts/LocalDiscoveryContext';
+import {AppProviders} from './contexts/AppProviders';
+import {createLocalDiscoveryController} from './contexts/LocalDiscoveryContext';
 import {Loading} from './sharedComponents/Loading';
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
-const queryClient = new QueryClient();
 const messagePort = new MessagePortLike();
 const mapeoApi = createMapeoClient(messagePort, {timeout: Infinity});
 const localDiscoveryController = createLocalDiscoveryController(mapeoApi);
@@ -41,23 +34,18 @@ const App = () => {
 
   return (
     <IntlProvider>
-      <ExternalProviders queryClient={queryClient} navRef={navRef}>
-        <ServerLoading messagePort={messagePort}>
-          <LocalDiscoveryProvider value={localDiscoveryController}>
-            <ApiProvider api={mapeoApi}>
-              <ActiveProjectProvider>
-                <PhotoPromiseProvider>
-                  <SecurityProvider>
-                    <React.Suspense fallback={<Loading />}>
-                      <AppNavigator permissionAsked={permissionsAsked} />
-                    </React.Suspense>
-                  </SecurityProvider>
-                </PhotoPromiseProvider>
-              </ActiveProjectProvider>
-            </ApiProvider>
-          </LocalDiscoveryProvider>
-        </ServerLoading>
-      </ExternalProviders>
+      <AppProviders
+        messagePort={messagePort}
+        localDiscoveryController={localDiscoveryController}
+        mapeoApi={mapeoApi}>
+        <React.Suspense fallback={<Loading />}>
+          <NavigationContainer ref={navRef}>
+            <BottomSheetModalProvider>
+              <AppNavigator permissionAsked={permissionsAsked} />
+            </BottomSheetModalProvider>
+          </NavigationContainer>
+        </React.Suspense>
+      </AppProviders>
     </IntlProvider>
   );
 };

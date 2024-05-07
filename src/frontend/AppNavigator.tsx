26dd5aff1325bd14753363d05ba717376de4b182
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-05-07 15:02:13 -0700

    chore: one off suspense query

--- src/frontend/AppNavigator.tsx
+++ src/frontend/AppNavigator.tsx
@@ -3,7 +3,7 @@ import {NavigatorScreenOptions, RootStack} from './Navigation/Stack';
 import {useIntl} from 'react-intl';
 
 import BootSplash from 'react-native-bootsplash';
-import {useDeviceInfo} from './hooks/server/deviceInfo';
+import {DEVICE_INFO_KEY} from './hooks/server/deviceInfo';
 import {createDeviceNamingScreens} from './Navigation/Stack/DeviceNamingScreens';
 import {usePrefetchLastKnownLocation} from './hooks/useLastSavedLocation';
 import {usePersistedDraftObservation} from './hooks/persistedState/usePersistedDraftObservation';
@@ -12,18 +12,26 @@ import {initializeInviteListener} from './initializeInviteListener';
 import {ProjectInviteBottomSheet} from './sharedComponents/ProjectInviteBottomSheet';
 import {getInitialRouteName} from './utils/navigation';
 import {createDefaultScreenGroup} from './Navigation/Stack/AppScreens';
+import {useSuspenseQuery} from '@tanstack/react-query';
+import {useApi} from './contexts/ApiContext';
 
 export const AppNavigator = ({permissionAsked}: {permissionAsked: boolean}) => {
   const {formatMessage} = useIntl();
   const existingObservation = usePersistedDraftObservation(
     store => store.value,
   );
+  const mapeoApi = useApi();
   const {data: presets} = usePresetsQuery();
-  const deviceInfo = useDeviceInfo();
+  const deviceInfo = useSuspenseQuery({
+    queryKey: [DEVICE_INFO_KEY],
+    queryFn: async () => {
+      return await mapeoApi.getDeviceInfo();
+    },
+  });
   usePrefetchLastKnownLocation();
   initializeInviteListener();
 
-  if (permissionAsked && !deviceInfo.isPending) {
+  if (permissionAsked) {
     BootSplash.hide();
   }
 
@@ -31,13 +39,13 @@ export const AppNavigator = ({permissionAsked}: {permissionAsked: boolean}) => {
     <React.Fragment>
       <RootStack.Navigator
         initialRouteName={getInitialRouteName({
-          hasDeviceName: !!deviceInfo.data?.name,
+          hasDeviceName: !!deviceInfo.data.name,
           existingObservation,
           presets,
         })}
         screenOptions={NavigatorScreenOptions}>
         {/*  Note that this does the same things as the strange syntax found in https://github.com/react-navigation/react-navigation/issues/9578#issuecomment-1022991270 `{createScreens()}` is equivalent to `{(() => createScreens())()}` Note that screen groups should have a `key` prop, so that React knows how to update them efficiently. */}
-        {deviceInfo.data?.name
+        {deviceInfo.data.name
           ? createDefaultScreenGroup(formatMessage)
           : createDeviceNamingScreens()}
       </RootStack.Navigator>

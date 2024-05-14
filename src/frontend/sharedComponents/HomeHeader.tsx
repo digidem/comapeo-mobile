683b6121ba72b308a544b3cb79df812edc36a38a
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-05-14 15:14:50 -0700

    chore: fix cache revalidation issues

--- src/frontend/sharedComponents/HomeHeader.tsx
+++ src/frontend/sharedComponents/HomeHeader.tsx
@@ -7,12 +7,26 @@ import {GPSPill} from './GPSPill';
 import {BottomTabHeaderProps} from '@react-navigation/bottom-tabs';
 import {EdgeInsets, useSafeAreaInsets} from 'react-native-safe-area-context';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import {useQueryClient} from '@tanstack/react-query';
+import {PROJECT_SETTINGS_KEY, useProject} from '../hooks/server/projects';
 
 export const HomeHeader: FC<
   BottomTabHeaderProps & {openDrawer: () => void}
 > = ({navigation, openDrawer}) => {
   const insets = useSafeAreaInsets();
   const styles = createStyles(insets);
+  const queryClient = useQueryClient();
+  const project = useProject();
+
+  //This is to fix a bug. After a project is created, we invalidate the PROJECT_SETTINGS_KEY cache. This should refetch this query, and re-render any consuming component with the new name. But the drawer (which displays the prject name) is not responding to the cache being invalidated. I tested the hook in other components, and it is properly being invalidated. So on opening of the drawer im manually refetching which seems to fix the problem
+  function prefetchProjectSettings() {
+    queryClient.prefetchQuery({
+      queryKey: [PROJECT_SETTINGS_KEY],
+      queryFn: () => {
+        return project.$getProjectSettings();
+      },
+    });
+  }
 
   return (
     <View style={styles.header}>
@@ -28,7 +42,12 @@ export const HomeHeader: FC<
         <SyncIconCircle />
       </IconButton>
       <GPSPill navigation={navigation} />
-      <IconButton onPress={openDrawer} testID="observationListButton">
+      <IconButton
+        onPress={() => {
+          prefetchProjectSettings();
+          openDrawer();
+        }}
+        testID="observationListButton">
         <MaterialIcon name="menu" size={32} />
       </IconButton>
     </View>

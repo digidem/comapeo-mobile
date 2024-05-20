1088e1fbb00ce0dcff5a1e27d63b2a4684d9a57e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2024-05-20 11:33:58 +0100

    fix: Fix metro config heirarchical lookup option

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,4 @@
 const {getSentryExpoConfig} = require('@sentry/react-native/metro');
-const path = require('path');
-
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot);
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getSentryExpoConfig(__dirname);
@@ -17,15 +13,9 @@ module.exports = {
   resolver: {
     ...config.resolver,
     // For nodejs-mobile
-    blockList: [/nodejs-assets\/.*/],
+    blockList: [...config.resolver.blockList, /nodejs-assets\/.*/],
     // For https://github.com/kristerkari/react-native-svg-transformer
     assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...config.resolver.sourceExts, 'svg'],
-    nodeModulesPaths: [
-      path.resolve(projectRoot, 'node_modules'),
-      path.resolve(workspaceRoot, 'node_modules'),
-    ],
-    // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-    disableHierarchicalLookup: true,
   },
 };

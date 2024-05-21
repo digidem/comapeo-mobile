1576a50bb7777717c652406fa41fc756aeaa3bdb
Bohdan Artiukhov <69891500+bohdanprog@users.noreply.github.com>
2024-05-21 22:10:41 +0200

    Feat/redesign edit observation (#368)
    * redesign observation edit screen
    
    * change translations
    
    * update icons, create separate components for ActionTab
    
    * update translates
    
    * Update ActionTab component
    
    * change translations in the audio button
    
    * update description field edit observation screen
    
    * remove import
    
    * fixed problem with Item Button, created custom hook KeyboardListener, added changes after review
    
    * fixed problem with a feature flag
    
    * update imports and rename the folder with icons
    
    * resolve conflict, move the divider to LocationView

---
+++ src/frontend/screens/ObservationEdit/PresetAndLocationHeader.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {LIGHT_GREY, WHITE} from '../../lib/styles';
+import {PresetView} from './PresetView';
+import {LocationView} from './LocationView';
+
+interface PresetInformation {
+  isNew: boolean;
+}
+
+export function PresetAndLocationHeader({isNew}: PresetInformation) {
+  return (
+    <View style={styles.container}>
+      <PresetView />
+      {isNew && <LocationView />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+    backgroundColor: WHITE,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: LIGHT_GREY,
+  },
+});
---
+++ src/frontend/sharedComponents/ActionTab/ItemButton.tsx
@@ -0,0 +1,31 @@
+import React, {FC} from 'react';
+import {ActionTabItems} from '.';
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+import {CustomCircleIcon} from './CustomCircleIcon';
+
+export const ItemButton: FC<ActionTabItems> = ({onPress, icon, label}) => (
+  <TouchableOpacity onPress={onPress} style={styles.itemContainer}>
+    <View style={styles.itemIcon}>
+      <CustomCircleIcon icon={icon} />
+    </View>
+    <Text numberOfLines={1} style={styles.itemLabel}>
+      {label}
+    </Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  itemIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemLabel: {
+    fontFamily: 'Rubik',
+    fontSize: 12,
+  },
+});

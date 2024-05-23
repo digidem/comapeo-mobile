c82d563ec178ad0c622c9c41a55746a7838a71f4
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-05-23 15:54:35 +0200

    add animatedTiemrConponent

---
+++ src/frontend/screens/Audio/AnimatedTimer.tsx
@@ -0,0 +1,50 @@
+import React, {FC} from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import Animated, {SharedValue, useAnimatedProps} from 'react-native-reanimated';
+import {WHITE} from '../../lib/styles';
+
+Animated.addWhitelistedNativeProps({text: true});
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
+interface AnimatedTimer {
+  elapsedTime: SharedValue<number>;
+}
+
+function convertMsToTime(ms: number) {
+  'worklet';
+  let minutes = Math.floor(ms / 60000);
+  let seconds = Number(((ms % 60000) / 1000).toFixed(0));
+  return (
+    (minutes < 10 ? '0' : '') +
+    minutes +
+    ':' +
+    (seconds < 10 ? '0' : '') +
+    seconds
+  );
+}
+
+export const AnimatedTimer: FC<AnimatedTimer> = ({elapsedTime}) => {
+  const animatedProps = useAnimatedProps(() => {
+    const currentValue = convertMsToTime(elapsedTime.value);
+    return {text: currentValue, defaultValue: currentValue};
+  }, [elapsedTime]);
+  return (
+    <AnimatedTextInput
+      animatedProps={animatedProps}
+      style={styles.timerStyle}
+      editable={false}
+      underlineColorAndroid="transparent"
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  timerStyle: {
+    fontFamily: 'Rubik',
+    fontSize: 96,
+    color: WHITE,
+    marginTop: 'auto',
+    textAlign: 'center',
+    marginBottom: 120,
+  },
+});

0efee1ac0b25ec0c22502999ad691e6786a47659
Bohdan Artiukhov <69891500+bohdanprog@users.noreply.github.com>
2024-05-28 08:41:17 +0200

    Feat/microfone permission (#375)
    * update icons, create separate components for ActionTab
    
    * update translates
    
    * create modal for audio permission, add audio permission check, add translations, add new microphone icon, handle when user back from settings and permission state is changed

--- src/frontend/screens/ObservationEdit/ThumbnailAndActionTab.tsx
+++ src/frontend/screens/ObservationEdit/ThumbnailAndActionTab.tsx
@@ -1,15 +1,18 @@
-import React, {FC} from 'react';
+import React, {FC, useCallback} from 'react';
 import {View} from 'react-native';
 import {defineMessages, useIntl} from 'react-intl';
 import {MediaScrollView} from '../../sharedComponents/Thumbnail/MediaScrollView';
 import {ActionTab} from '../../sharedComponents/ActionTab';
-import Photo from '../../images/observationEdit/Photo.svg';
-import Audio from '../../images/observationEdit/Audio.svg';
-import Details from '../../images/observationEdit/Details.svg';
+import PhotoIcon from '../../images/observationEdit/Photo.svg';
+import AudioIcon from '../../images/observationEdit/Audio.svg';
+import DetailsIcon from '../../images/observationEdit/Details.svg';
 import {usePersistedDraftObservation} from '../../hooks/persistedState/usePersistedDraftObservation';
 import {useDraftObservation} from '../../hooks/useDraftObservation';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {AppStackParamsList} from '../../sharedTypes/navigation';
+import {useBottomSheetModal} from '../../sharedComponents/BottomSheetModal';
+import {PermissionAudio} from '../../sharedComponents/PermissionAudio';
+import {Audio} from 'expo-av';
 
 const m = defineMessages({
   audioButton: {
@@ -46,6 +49,16 @@ export const ThumbnailAndActionTab: FC<ThumbnailAndActionTab> = ({
   const {photos, audioRecordings, observationId} = usePersistedDraftObservation(
     store => store,
   );
+  const {
+    openSheet: openAudioPermissionSheet,
+    sheetRef: audioPermissionSheetRef,
+    isOpen: isAudioPermissionSheetOpen,
+    closeSheet: closeAudioPermissionSheet,
+  } = useBottomSheetModal({
+    openOnMount: false,
+  });
+  const [permissionResponse] = Audio.usePermissions({request: false});
+
   const handleCameraPress = () => {
     navigation.navigate('AddPhoto');
   };
@@ -54,9 +67,17 @@ export const ThumbnailAndActionTab: FC<ThumbnailAndActionTab> = ({
     navigation.navigate('ObservationFields', {question: 1});
   };
 
+  const handleAudioPress = useCallback(() => {
+    if (permissionResponse?.granted) {
+      navigation.navigate('Home', {screen: 'Map'});
+    } else {
+      openAudioPermissionSheet();
+    }
+  }, [navigation, openAudioPermissionSheet, permissionResponse?.granted]);
+
   const bottomSheetItems = [
     {
-      icon: <Photo width={30} height={30} />,
+      icon: <PhotoIcon width={30} height={30} />,
       label: t(m.photoButton),
       onPress: handleCameraPress,
     },
@@ -64,29 +85,36 @@ export const ThumbnailAndActionTab: FC<ThumbnailAndActionTab> = ({
 
   if (process.env.EXPO_PUBLIC_FEATURE_AUDIO) {
     bottomSheetItems.unshift({
-      icon: <Audio width={30} height={30} />,
+      icon: <AudioIcon width={30} height={30} />,
       label: t(m.audioButton),
-      onPress: () => {},
+      onPress: handleAudioPress,
     });
   }
 
   if (preset?.fieldIds.length) {
     // Only show the option to add details if preset fields are defined.
     bottomSheetItems.push({
-      icon: <Details width={30} height={30} />,
+      icon: <DetailsIcon width={30} height={30} />,
       label: t(m.detailsButton),
       onPress: handleDetailsPress,
     });
   }
 
   return (
-    <View>
-      <MediaScrollView
-        photos={photos}
-        audioRecordings={audioRecordings}
-        observationId={observationId}
+    <>
+      <View>
+        <MediaScrollView
+          photos={photos}
+          audioRecordings={audioRecordings}
+          observationId={observationId}
+        />
+        <ActionTab items={bottomSheetItems} />
+      </View>
+      <PermissionAudio
+        closeSheet={closeAudioPermissionSheet}
+        isOpen={isAudioPermissionSheetOpen}
+        sheetRef={audioPermissionSheetRef}
       />
-      <ActionTab items={bottomSheetItems} />
-    </View>
+    </>
   );
 };

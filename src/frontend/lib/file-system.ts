c4b3a3b522b362137546a38f8d0a5173f64de25a
Andrew Chou <andrewchou@fastmail.com>
2024-06-04 17:56:49 -0400

    feat: add config importing to project creation flow (#379)

--- src/frontend/lib/file-system.ts
+++ src/frontend/lib/file-system.ts
@@ -1,3 +1,7 @@
 import {ExternalDirectoryPath} from '@dr.pogodin/react-native-fs';
 
+export function convertFileUriToPosixPath(fileUri: string) {
+  return fileUri.replace(/^file:\/\//, '');
+}
+
 export {ExternalDirectoryPath as EXTERNAL_FILES_DIR};

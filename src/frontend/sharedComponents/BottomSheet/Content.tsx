82fb2ba96cffc02a336df015e03c5c6d44c7973b
Andrew Chou <andrewchou@fastmail.com>
2024-06-17 20:04:52 -0400

    more proper fixing of base components

--- src/frontend/sharedComponents/BottomSheet/Content.tsx
+++ src/frontend/sharedComponents/BottomSheet/Content.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
-import {StyleSheet, TextStyle, ScrollView, View} from 'react-native';
+import {
+  StyleSheet,
+  TextStyle,
+  ScrollView,
+  View,
+  Dimensions,
+} from 'react-native';
 import {TouchableHighlight} from '@gorhom/bottom-sheet';
 import {UIActivityIndicator} from 'react-native-indicators';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import {LIGHT_BLUE, MAGENTA, COMAPEO_BLUE, RED, WHITE} from '../../lib/styles';
 import {Button} from '../Button';
@@ -23,111 +30,128 @@ interface SecondaryActionButtonConfig extends BaseActionButtonConfig {
   variation: 'outlined';
 }
 
-type ActionButtonConfig =
+export type ActionButtonConfig =
   | PrimaryActionButtonConfig
   | SecondaryActionButtonConfig;
 
 export interface Props extends React.PropsWithChildren {
   buttonConfigs: ActionButtonConfig[];
   description?: React.ReactNode;
+  descriptionStyle?: TextStyle;
+  fullScreen?: boolean;
   icon?: React.ReactNode;
+  loading?: boolean;
   title: React.ReactNode;
   titleStyle?: TextStyle;
-  descriptionStyle?: TextStyle;
-  loading?: boolean;
 }
 
 export const Content = ({
-  children,
-  icon,
   buttonConfigs,
+  children,
   description,
+  descriptionStyle,
+  fullScreen,
+  icon,
+  loading,
   title,
   titleStyle,
-  descriptionStyle,
-  loading,
-}: Props) => (
-  <View style={styles.container}>
-    <View style={styles.infoContainer}>
-      {icon ? <View style={styles.iconContainer}>{icon}</View> : null}
-      <View style={styles.textContainer}>
-        <Text style={[styles.title, styles.bold, titleStyle]}>{title}</Text>
-        {description ? (
-          <Text style={[styles.description, descriptionStyle]}>
-            {description}
-          </Text>
+}: Props) => {
+  const {top: insetTop} = useSafeAreaInsets();
+
+  return (
+    <View
+      style={[
+        styles.container,
+        fullScreen
+          ? {height: '100%', paddingTop: insetTop}
+          : {maxHeight: Dimensions.get('window').height * 0.75},
+      ]}>
+      <View style={[styles.infoContainer, fullScreen && {flex: 1}]}>
+        {icon ? <View style={styles.iconContainer}>{icon}</View> : null}
+        <View style={styles.textContainer}>
+          <Text style={[styles.title, styles.bold, titleStyle]}>{title}</Text>
+          {description ? (
+            <Text style={[styles.description, descriptionStyle]}>
+              {description}
+            </Text>
+          ) : null}
+        </View>
+        {children ? (
+          <ScrollView style={fullScreen ? {flex: 1} : {maxHeight: 300}}>
+            {children}
+          </ScrollView>
         ) : null}
       </View>
-      {children ? (
-        <ScrollView style={styles.scrollView}>{children}</ScrollView>
-      ) : (
-        <ScrollView style={styles.scrollView}>
-          {/* <View style={{backgroundColor: 'cyan', height: 400}} /> */}
-        </ScrollView>
-      )}
-    </View>
-    <View style={styles.buttonsContainer}>
-      {loading ? (
-        <UIActivityIndicator />
-      ) : (
-        buttonConfigs.map((config, index) => {
-          return (
-            <Button
-              fullWidth
-              key={index}
-              TouchableComponent={props => (
-                <TouchableHighlight
-                  {...props}
-                  underlayColor={
+      <View style={styles.buttonsContainer}>
+        {loading ? (
+          <UIActivityIndicator />
+        ) : (
+          buttonConfigs.map((config, index) => {
+            return (
+              <Button
+                fullWidth
+                key={index}
+                TouchableComponent={props => (
+                  <TouchableHighlight
+                    {...props}
+                    underlayColor={
+                      config.variation === 'outlined'
+                        ? WHITE
+                        : config.dangerous
+                          ? RED
+                          : LIGHT_BLUE
+                    }
+                  />
+                )}
+                onPress={config.onPress}
+                style={{
+                  backgroundColor:
                     config.variation === 'outlined'
                       ? WHITE
                       : config.dangerous
-                        ? RED
-                        : LIGHT_BLUE
-                  }
-                />
-              )}
-              onPress={config.onPress}
-              style={{
-                backgroundColor:
-                  config.variation === 'outlined'
-                    ? WHITE
-                    : config.dangerous
-                      ? MAGENTA
-                      : COMAPEO_BLUE,
-              }}
-              variant={
-                config.variation === 'outlined' ? 'outlined' : undefined
-              }>
-              <View style={styles.buttonTextContainer}>
-                {config.icon ? <View>{config.icon}</View> : null}
-                <Text
-                  style={[
-                    styles.buttonText,
-                    styles.bold,
-                    {
-                      color:
-                        config.variation === 'outlined' ? COMAPEO_BLUE : WHITE,
-                    },
-                  ]}>
-                  {config.text}
-                </Text>
-              </View>
-            </Button>
-          );
-        })
-      )}
+                        ? MAGENTA
+                        : COMAPEO_BLUE,
+                }}
+                variant={
+                  config.variation === 'outlined' ? 'outlined' : undefined
+                }>
+                <View style={styles.buttonTextContainer}>
+                  {config.icon ? <View>{config.icon}</View> : null}
+                  <Text
+                    style={[
+                      styles.buttonText,
+                      styles.bold,
+                      {
+                        color:
+                          config.variation === 'outlined'
+                            ? COMAPEO_BLUE
+                            : WHITE,
+                      },
+                    ]}>
+                    {config.text}
+                  </Text>
+                </View>
+              </Button>
+            );
+          })
+        )}
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'space-between'},
-  infoContainer: {flex: 1, gap: 20, paddingTop: 20},
+  container: {
+    justifyContent: 'space-between',
+    minHeight: 400,
+  },
+  infoContainer: {
+    gap: 20,
+    paddingTop: 20,
+  },
   iconContainer: {alignItems: 'center', justifyContent: 'center'},
   textContainer: {gap: 20, paddingHorizontal: 20},
   title: {textAlign: 'center', fontSize: 24},
-  scrollView: {flex: 1},
   buttonsContainer: {gap: 20, padding: 20},
   description: {fontSize: 20, textAlign: 'center'},
   buttonTextContainer: {
--- src/frontend/sharedComponents/BottomSheetModal/index.tsx
+++ src/frontend/sharedComponents/BottomSheetModal/index.tsx
@@ -1,11 +1,5 @@
 import * as React from 'react';
-import {
-  BackHandler,
-  NativeEventSubscription,
-  Keyboard,
-  StyleSheet,
-  useWindowDimensions,
-} from 'react-native';
+import {BackHandler, NativeEventSubscription, Keyboard} from 'react-native';
 import {
   BottomSheetModal as RNBottomSheetModal,
   BottomSheetView,
@@ -14,16 +8,10 @@ import {
 } from '@gorhom/bottom-sheet';
 import {NativeStackNavigationOptions} from '@react-navigation/native-stack';
 
-import {DARK_GREY} from '../../lib/styles';
-import {useCallback} from 'react';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-
 export const MODAL_NAVIGATION_OPTIONS: NativeStackNavigationOptions = {
   presentation: 'transparentModal',
   animation: 'none',
 };
-
-const MINIMUM_SHEET_HEIGHT = 400;
 
 export const useBottomSheetModal = ({openOnMount}: {openOnMount: boolean}) => {
   const initiallyOpenedRef = React.useRef(false);
@@ -37,7 +25,7 @@ export const useBottomSheetModal = ({openOnMount}: {openOnMount: boolean}) => {
     }
   }, []);
 
-  const openSheet = useCallback(() => {
+  const openSheet = React.useCallback(() => {
     if (sheetRef.current) {
       setIsOpen(true);
       if (Keyboard.isVisible()) {
@@ -83,49 +71,26 @@ interface Props extends React.PropsWithChildren<{}> {
   onDismiss?: () => void;
   // Triggered by: Android hardware back press and gesture back swipe
   onBack?: () => void;
-  fullHeight?: boolean;
 }
 
 export const BottomSheetModal = React.forwardRef<RNBottomSheetModal, Props>(
-  ({children, isOpen, onBack, fullHeight, onDismiss}, ref) => {
+  ({children, isOpen, onBack, onDismiss}, ref) => {
     useBackHandler(isOpen, onBack);
-
-    const {top: insetTop} = useSafeAreaInsets();
-    const {height: windowHeight} = useWindowDimensions();
-
-    const snapPoints = fullHeight
-      ? ['100%']
-      : [Math.max(MINIMUM_SHEET_HEIGHT, windowHeight * 0.75)];
 
     return (
       <RNBottomSheetModal
+        enableDynamicSizing
         ref={ref}
-        backgroundStyle={[
-          fullHeight
-            ? {borderRadius: 0}
-            : {borderColor: DARK_GREY, borderWidth: 1},
-        ]}
         backdropComponent={DefaultBackdrop}
         onDismiss={onDismiss}
         enableContentPanningGesture={false}
         enableHandlePanningGesture={false}
-        snapPoints={snapPoints}
         handleComponent={() => null}>
-        <BottomSheetView
-          style={[
-            styles.bottomSheetViewContainer,
-            fullHeight ? {paddingTop: insetTop} : null,
-          ]}>
-          {children}
-        </BottomSheetView>
+        <BottomSheetView>{children}</BottomSheetView>
       </RNBottomSheetModal>
     );
   },
 );
-
-const styles = StyleSheet.create({
-  bottomSheetViewContainer: {flex: 1},
-});
 
 function DefaultBackdrop(props: BottomSheetBackdropProps) {
   return (

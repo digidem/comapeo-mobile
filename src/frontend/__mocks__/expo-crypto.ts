d08e69d8727f4b7ff6114317688ec42fdc5f8ff2
Evan Hahn <me@evanhahn.com>
2024-07-04 13:26:36 -0500

    Simplify `getMonthlyHash`
    Co-Authored-By: Gregor MacLennan <gmaclennan@digital-democracy.org>

--- src/frontend/__mocks__/expo-crypto.ts
+++ src/frontend/__mocks__/expo-crypto.ts
@@ -1,22 +1,31 @@
-import {createHash} from 'node:crypto';
-import {type BufferSource} from 'node:stream/web';
+import {createHash, type BinaryToTextEncoding} from 'node:crypto';
 
 export enum CryptoDigestAlgorithm {
   SHA512 = 'sha512',
 }
 
-export function digest(
-  algorithm: CryptoDigestAlgorithm,
-  data: BufferSource,
-): Promise<ArrayBuffer> {
-  const hash = createHash(algorithm);
-
-  const dataToHash = new Uint8Array(
-    data instanceof ArrayBuffer ? data : data.buffer,
-  );
-  hash.update(dataToHash);
-
-  const result = hash.digest().buffer;
-
-  return Promise.resolve(result);
+export enum CryptoEncoding {
+  HEX = 'hex',
+  BASE64 = 'base64',
 }
+
+type CryptoDigestOptions = {
+  encoding: CryptoEncoding;
+};
+
+/**
+ * Ponyfill of `crypto.hash` for old environments.
+ *
+ * [0]: https://nodejs.org/api/crypto.html#cryptohashalgorithm-data-outputencoding
+ */
+const hash = (
+  algorithm: string,
+  data: string | Uint8Array,
+  outputEncoding: BinaryToTextEncoding,
+): string => createHash(algorithm).update(data).digest(outputEncoding);
+
+export const digestStringAsync = (
+  algorithm: CryptoDigestAlgorithm,
+  data: string,
+  options: CryptoDigestOptions = {encoding: CryptoEncoding.HEX},
+): Promise<string> => Promise.resolve(hash(algorithm, data, options.encoding));
--- src/frontend/metrics/getMonthlyHash.ts
+++ src/frontend/metrics/getMonthlyHash.ts
@@ -1,46 +1,11 @@
-import {CryptoDigestAlgorithm, digest} from 'expo-crypto';
-import {uint8ArrayToHex} from 'uint8array-extras';
+import {CryptoDigestAlgorithm, digestStringAsync} from 'expo-crypto';
 
-function writeMonthAndYearToBytes(
-  bytes: Uint8Array,
-  date: Readonly<Date>,
-): void {
-  new DataView(bytes.buffer).setUint16(0, date.getUTCFullYear(), true);
-  bytes[2] = date.getUTCMonth();
-}
-
-function writeStringToBytes(
-  bytes: Uint8Array,
-  offset: number,
-  str: string,
-): void {
-  for (let i = 0; i < str.length; i++) {
-    bytes[i + offset] = str.charCodeAt(i) % 255;
-  }
-}
-
-function dataToBytes(
+export const getMonthlyHash = (
   salt: string,
   metricsDeviceId: string,
   now: Date,
-): Uint8Array {
-  const result = new Uint8Array(3 + salt.length + metricsDeviceId.length);
-  writeMonthAndYearToBytes(result, now);
-  writeStringToBytes(result, 3, salt);
-  writeStringToBytes(result, 3 + salt.length, metricsDeviceId);
-  return result;
-}
-
-export async function getMonthlyHash(
-  salt: string,
-  metricsDeviceId: string,
-  now: Date,
-): Promise<string> {
-  const hashedBuffer = await digest(
+): Promise<string> =>
+  digestStringAsync(
     CryptoDigestAlgorithm.SHA512,
-    dataToBytes(salt, metricsDeviceId, now),
+    `${now.getUTCFullYear()}-${now.getUTCMonth()}-${metricsDeviceId}-${salt}`,
   );
-
-  const hashedBytes = new Uint8Array(hashedBuffer);
-  return uint8ArrayToHex(hashedBytes);
-}

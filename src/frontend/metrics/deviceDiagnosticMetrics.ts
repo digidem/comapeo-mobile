a5b55f246c9a7f5eecd2ace1fdeb2ceb25d986e8
Evan Hahn <me@evanhahn.com>
2024-07-04 14:11:31 -0500

    Use expo-device for device metrics

--- src/frontend/metrics/deviceDiagnosticMetrics.ts
+++ src/frontend/metrics/deviceDiagnosticMetrics.ts
@@ -1,32 +1,54 @@
-import pProps from 'p-props';
 import {Dimensions, PixelRatio, Platform} from 'react-native';
-import DeviceInfo from 'react-native-device-info';
+import * as Device from 'expo-device';
 import type {ReadonlyDeep} from 'type-fest';
 import {getMonthlyHash} from './getMonthlyHash';
 
 type DeviceDiagnosticMetricsData = {
   type: 'device diagnostics v1';
   monthlyDeviceHash: string;
-  os: string;
-  screen: {width: number; height: number; pixelRatio: number};
-  manufacturer?: string;
   brand?: string;
-  model?: string;
   deviceType?: string;
   isEmulator?: true;
-  supportedAbis: string[];
-  totalMemory: number;
-  installerPackageName: string;
+  manufacturer?: string;
+  model?: string;
+  os: string;
+  screen: {width: number; height: number; pixelRatio: number};
+  supportedCpuArchitectures?: string[];
+  totalMemory?: number;
 };
+
+function setIfNotNull<ObjT, KeyT extends keyof ObjT>(
+  obj: ObjT,
+  key: KeyT,
+  value: null | ObjT[KeyT],
+): void {
+  if (value !== null) obj[key] = value;
+}
+
+function deviceTypeToString(deviceType: null | Device.DeviceType): string {
+  switch (deviceType) {
+    case Device.DeviceType.PHONE:
+      return 'PHONE';
+    case Device.DeviceType.TABLET:
+      return 'TABLET';
+    case Device.DeviceType.DESKTOP:
+      return 'DESKTOP';
+    case Device.DeviceType.TV:
+      return 'TV';
+    case null:
+    case Device.DeviceType.UNKNOWN:
+    default:
+      return 'UNKNOWN';
+  }
+}
 
 export async function generateDeviceDiagnosticMetricsData({
   metricsDeviceId,
 }: ReadonlyDeep<{
   metricsDeviceId: string;
 }>): Promise<DeviceDiagnosticMetricsData> {
-  const isEmulatorPromise = DeviceInfo.isEmulator();
-
   const screen = Dimensions.get('screen');
+
   const result: DeviceDiagnosticMetricsData = {
     type: 'device diagnostics v1',
     os: Platform.OS,
@@ -35,34 +57,26 @@ export async function generateDeviceDiagnosticMetricsData({
       height: screen.height,
       pixelRatio: PixelRatio.get(),
     },
-    ...(await pProps({
-      monthlyDeviceHash: getMonthlyHash(
-        'device diagnostics',
-        metricsDeviceId,
-        new Date(),
-      ),
-      supportedAbis: DeviceInfo.supportedAbis(),
-      totalMemory: DeviceInfo.getTotalMemory(),
-      installerPackageName: DeviceInfo.getInstallerPackageName(),
-    })),
+    monthlyDeviceHash: await getMonthlyHash(
+      'device diagnostics',
+      metricsDeviceId,
+      new Date(),
+    ),
   };
 
-  switch (Platform.OS) {
-    case 'android':
-      result.manufacturer = Platform.constants.Manufacturer;
-      result.brand = Platform.constants.Brand;
-      result.model = Platform.constants.Model;
-      result.deviceType = Platform.constants.uiMode;
-      break;
-    case 'ios':
-      result.model = DeviceInfo.getDeviceId();
-      result.deviceType = Platform.constants.interfaceIdiom;
-      break;
-    default:
-      break;
-  }
+  setIfNotNull(result, 'brand', Device.brand);
+  setIfNotNull(result, 'deviceType', deviceTypeToString(Device.deviceType));
+  setIfNotNull(result, 'manufacturer', Device.manufacturer);
+  setIfNotNull(result, 'model', Device.modelId || Device.modelName);
+  setIfNotNull(
+    result,
+    'supportedCpuArchitectures',
+    Device.supportedCpuArchitectures,
+  );
+  setIfNotNull(result, 'totalMemory', Device.totalMemory);
 
-  if (await isEmulatorPromise) result.isEmulator = true;
+  const isEmulator = !Device.isDevice;
+  if (isEmulator) result.isEmulator = true;
 
   return result;
 }

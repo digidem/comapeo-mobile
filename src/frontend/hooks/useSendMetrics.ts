94c925e93ff5e9ab56fd5fddda5eb74fabcee009
Evan Hahn <me@evanhahn.com>
2024-07-08 17:28:46 +0000

    Remove old useSendMetrics hook

--- src/frontend/hooks/useSendMetrics.ts
+++
@@ -1,7 +0,0 @@
-import {useSendDeviceDiagnosticMetrics} from './useSendDeviceDiagnosticMetrics';
-
-export function useSendMetrics(): void {
-  // TODO: Send other metrics.
-  // See <https://github.com/digidem/comapeo-mobile/issues/460>.
-  useSendDeviceDiagnosticMetrics();
-}

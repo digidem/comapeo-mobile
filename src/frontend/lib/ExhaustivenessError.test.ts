423496be9684fe87b9e2befe7fe1f709174caf5e
Evan Hahn <me@evanhahn.com>
2024-07-11 20:22:36 +0000

    Fix tests

--- src/frontend/lib/ExhaustivenessError.test.ts
+++ src/frontend/lib/ExhaustivenessError.test.ts
@@ -1,17 +1,19 @@
 import {ExhaustivenessError} from './ExhaustivenessError';
 
 describe('ExhaustivenessError', () => {
-  const bools = [true, false];
+  it('handles exhaustiveness', () => {
+    const bools = [true, false];
 
-  expect(() => {
-    bools.forEach(bool => {
-      switch (bool) {
-        case true:
-        case false:
-          break;
-        default:
-          throw new ExhaustivenessError(bool);
-      }
-    });
-  }).not.toThrow();
+    expect(() => {
+      bools.forEach(bool => {
+        switch (bool) {
+          case true:
+          case false:
+            break;
+          default:
+            throw new ExhaustivenessError(bool);
+        }
+      });
+    }).not.toThrow();
+  });
 });

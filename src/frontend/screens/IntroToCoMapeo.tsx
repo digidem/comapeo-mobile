ee3416c3d68fc5e329b0ffd150fceffd971b6b1d
Cindy Green <cimigreen@hotmail.com>
2024-08-01 17:14:01 -0400

    Adds png emoji images. Starts to build intro screen.

--- src/frontend/screens/IntroToCoMapeo.tsx
+++ src/frontend/screens/IntroToCoMapeo.tsx
@@ -3,7 +3,6 @@ import TopoBackground from '../images/TopoLogo.svg';
 import CoMapeoTextAsSVG from '../images/CoMapeoText.svg';
 import {StyleSheet, View, Image} from 'react-native';
 import {COMAPEO_DARK_BLUE, WHITE} from '../lib/styles';
-import {getVersion} from 'react-native-device-info';
 import {defineMessages, useIntl} from 'react-intl';
 import {Text} from '../sharedComponents/Text';
 import {Button} from '../sharedComponents/Button';
@@ -11,24 +10,30 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {DeviceNamingParamsList} from '../sharedTypes/navigation';
 
 const m = defineMessages({
-  isNow: {
-    id: 'screens.IntroToCoMapeo.isNow',
-    defaultMessage: 'is now',
-    description: "The full sentence is mapeo 'is now' comapeo. ",
-  },
-  version: {
-    id: 'screens.IntroToCoMapeo.version',
-    defaultMessage: 'V',
-    description:
-      'v is short for version. the translation for version can be used instead',
-  },
-  collaborate: {
-    id: 'screens.IntroToCoMapeo.collaborate',
-    defaultMessage: 'Collaborate on mapping and monitoring projects',
-  },
   getStarted: {
     id: 'screens.IntroToCoMapeo.getStarted',
     defaultMessage: 'Get Started',
+  },
+  mapWorldTogether: {
+    id: 'screens.IntroToCoMapeo.mapWorldTogether',
+    defaultMessage: 'Map your world, together',
+  },
+  mapAnywhere: {
+    id: 'screens.IntroToCoMapeo.mapAnywhere',
+    defaultMessage: 'Map anywhere and everywhere',
+  },
+  collaborate: {
+    id: 'screens.IntroToCoMapeo.collaborate',
+    defaultMessage: 'Collaborate with others',
+  },
+  ownData: {
+    id: 'screens.IntroToCoMapeo.ownData',
+    defaultMessage: 'Own and control your data',
+  },
+  designedFor: {
+    id: 'screens.IntroToCoMapeo.designedFor',
+    defaultMessage:
+      'Designed with and for Indigenous peoples & frontline communities',
   },
 });
 
@@ -37,64 +42,103 @@ export const IntroToCoMapeo = ({
 }: NativeStackScreenProps<DeviceNamingParamsList, 'IntroToCoMapeo'>) => {
   const {formatMessage} = useIntl();
   return (
-    <View style={{backgroundColor: COMAPEO_DARK_BLUE}}>
-      <TopoBackground height={'100%'} />
+    <View style={styles.container}>
+      <TopoBackground style={styles.background} />
       <View style={styles.content}>
-        <View style={{width: '100%', alignItems: 'center'}}>
-          <View style={styles.mapeoContainer}>
-            {/* TO DO: change this image to an svg so it can inherit the blue background (I don't have edit access to figma to export it and Sabella is on vacay) */}
+        <CoMapeoTextAsSVG style={{marginTop: -30}} width={'90%'} />
+        <Text style={styles.mainText}>{formatMessage(m.mapWorldTogether)}</Text>
+        <View style={styles.textBox}>
+          <View style={styles.textItem}>
             <Image
-              style={{marginRight: 10}}
-              source={require('../images/icon_mapeo_pin.png')}
+              source={require('../images/world-map-emoji.png')}
+              style={styles.icon}
             />
-            <Text style={[styles.text, {fontSize: 40}]}>MAPEO</Text>
+            <Text style={styles.text}>{formatMessage(m.mapAnywhere)}</Text>
           </View>
-          <Text style={[styles.text, {fontSize: 20, marginTop: 20}]}>
-            {formatMessage(m.isNow)}
-          </Text>
-          <CoMapeoTextAsSVG style={{marginTop: -30}} width={'90%'} />
-          <Text style={styles.text}>{`${formatMessage(
-            m.version,
-          )} ${getVersion()}`}</Text>
+          <View style={styles.textItem}>
+            <Image
+              source={require('../images/mobile-phone-with-arrow.png')}
+              style={styles.icon}
+            />
+            <Text style={styles.text}>{formatMessage(m.collaborate)}</Text>
+          </View>
+          <View style={styles.textItem}>
+            <Image
+              source={require('../images/locked-with-key.png')}
+              style={styles.icon}
+            />
+            <Text style={styles.text}>{formatMessage(m.ownData)}</Text>
+          </View>
+          <View style={styles.textItem}>
+            <Image
+              source={require('../images/raised-fist-medium-skin-tone.png')}
+              style={styles.icon}
+            />
+            <Text style={styles.text}>{formatMessage(m.designedFor)}</Text>
+          </View>
         </View>
-        <View style={{width: '100%'}}>
-          <Text
-            style={[
-              styles.text,
-              {fontSize: 20, marginBottom: 30, fontWeight: '500'},
-            ]}>
-            {formatMessage(m.collaborate)}
-          </Text>
-          <Button
-            testID="ONBOARDING.get-started-btn"
-            fullWidth
-            onPress={() => {
-              navigation.navigate('DeviceNaming');
-            }}>
-            {formatMessage(m.getStarted)}
-          </Button>
-        </View>
+        <Button
+          testID="ONBOARDING.get-started-btn"
+          fullWidth
+          onPress={() => {
+            navigation.navigate('DeviceNaming');
+          }}
+          style={styles.getStartedButton}>
+          {formatMessage(m.getStarted)}
+        </Button>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  content: {
+  container: {
+    flex: 1,
+    backgroundColor: COMAPEO_DARK_BLUE,
+  },
+  background: {
+    paddingTop: 80,
     position: 'absolute',
     width: '100%',
     height: '100%',
-    padding: 20,
-    paddingTop: 80,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    padding: 20,
+  },
+  mainText: {
+    color: WHITE,
+    fontSize: 24,
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  textBox: {
+    width: '90%',
+    padding: 20,
+    borderWidth: 1,
+    borderColor: WHITE,
+    borderRadius: 10,
+    backgroundColor: '#123456',
+  },
+  textItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+    backgroundColor: 'transparent',
   },
   text: {
     color: WHITE,
-    textAlign: 'center',
+    fontSize: 16,
+    textAlign: 'left',
   },
-  mapeoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  getStartedButton: {
+    marginTop: 30,
   },
 });

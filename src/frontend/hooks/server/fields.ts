3a1841207fb18636793a1c657eacaf900212a969
Andrew Chou <andrewchou@fastmail.com>
2024-08-01 16:51:49 -0400

    fix: prevent queries using project api from using stale instance

--- src/frontend/hooks/server/fields.ts
+++ src/frontend/hooks/server/fields.ts
@@ -2,13 +2,12 @@ import {useQuery} from '@tanstack/react-query';
 import {useActiveProject} from '../../contexts/ActiveProjectContext';
 
 export const useFieldsQuery = () => {
-  const project = useActiveProject();
+  const {projectId, projectApi} = useActiveProject();
 
   return useQuery({
-    queryKey: ['fields'],
+    queryKey: ['fields', projectId],
     queryFn: async () => {
-      if (!project) throw new Error('Project instance does not exist');
-      return project.field.getMany();
+      return projectApi.field.getMany();
     },
   });
 };

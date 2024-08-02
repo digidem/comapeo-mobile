60b39da73b3452eb3d8a6388074064cf4a71ac9c
Gregor MacLennan <gmaclennan@digital-democracy.org>
2024-08-02 02:14:11 +0100

    Update .detoxrc.js

--- .detoxrc.js
+++ .detoxrc.js
@@ -34,7 +34,7 @@ module.exports = {
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release -PreactNativeArchitectures=arm64-v8a'
+      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release -PreactNativeArchitectures=arm64-v8a,armeabi-v7a'
     },
     'android.cloud.release': {
       type: 'android.cloud',

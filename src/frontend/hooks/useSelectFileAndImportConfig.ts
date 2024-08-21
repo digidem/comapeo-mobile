740d091ee5466a580834195e8af30f9772a35797
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-08-21 13:47:44 -0700

    chore: create select file and import config hook

---
+++ src/frontend/hooks/useSelectFileAndImportConfig.ts
@@ -0,0 +1,30 @@
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {selectFile} from '../lib/selectFile';
+import * as FileSystem from 'expo-file-system';
+import {PROJECT_SETTINGS_KEY, useImportProjectConfig} from './server/projects';
+import {convertFileUriToPosixPath} from '../lib/file-system';
+
+export function useSelectFileAndImportConfig() {
+  const importProjectConfigMutation = useImportProjectConfig();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async () => {
+      const asset = await selectFile(['mapeoconfig']);
+      if (!asset) return;
+
+      try {
+        return await importProjectConfigMutation.mutateAsync(
+          convertFileUriToPosixPath(asset.uri),
+        );
+      } finally {
+        await FileSystem.deleteAsync(asset.uri).catch((err: unknown) => {
+          console.log(err);
+        });
+      }
+    },
+    onSuccess: () => {
+      return queryClient.invalidateQueries({queryKey: [PROJECT_SETTINGS_KEY]});
+    },
+  });
+}

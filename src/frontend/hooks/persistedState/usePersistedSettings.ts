58e5dfec0a38fd660c938f9de5c1bccec701ba7b
Cindy Green <cimigreen@hotmail.com>
2024-09-04 16:00:11 -0400

    Changes default

--- src/frontend/hooks/persistedState/usePersistedSettings.ts
+++ src/frontend/hooks/persistedState/usePersistedSettings.ts
@@ -18,7 +18,7 @@ type SettingsSlice = {
 const settingsSlice: StateCreator<SettingsSlice> = (set, get) => ({
   coordinateFormat: 'utm',
   manualCoordinateEntryFormat: 'utm',
-  syncSetting: 'previews',
+  syncSetting: 'everything',
   actions: {
     setCoordinateFormat: coordinateFormat => set({coordinateFormat}),
     setManualCoordinateEntryFormat: coordinateFormat =>

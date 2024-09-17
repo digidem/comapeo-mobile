6282c22bedf861bec269ecb42e46bca878d1fc33
Evan Hahn <me@evanhahn.com>
2024-09-17 18:38:45 +0000

    test: load env variables so `npm test` works locally
    *I recommend reviewing with whitespace changes disabled.*
    
    We [set environment variables in CI][0] so that `npm test` works, but it
    doesn't work locally because Jest isn't loading them.
    
    This loads them when running Jest. If an environment variable is already
    set, it is skipped.
    
    [0]: https://github.com/digidem/comapeo-mobile/blob/9b6cf8f71a0d960174c7e8cd5ce1b6bb8f1dd5a7/.github/workflows/ci.yml#L72-L75

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const dotenv = require('dotenv');
 
 const requiredEnvVars = [
   'MAPBOX_ACCESS_TOKEN',
@@ -6,15 +7,25 @@ const requiredEnvVars = [
   'COMAPEO_METRICS_API_KEY',
 ];
 
-for (const requiredEnvVar of requiredEnvVars) {
-  assert(
-    process.env[requiredEnvVar],
-    `Missing required environment variable: ${requiredEnvVar}`,
-  );
+function checkRequiredEnvVars() {
+  for (const requiredEnvVar of requiredEnvVars) {
+    assert(
+      process.env[requiredEnvVar],
+      `Missing required environment variable: ${requiredEnvVar}`,
+    );
+  }
 }
 
 module.exports = function (api) {
+  const isTest = api.env('test');
+  if (isTest) {
+    dotenv.config({override: false});
+  }
+
+  checkRequiredEnvVars();
+
   api.cache(true);
+
   return {
     //https://github.com/babel/babel/discussions/13676#discussioncomment-1183149
     compact: true,

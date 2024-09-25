db044f7e22cfbe7f638789f9b97c0b6c9c3f03ee
Cindy Green <cimigreen@hotmail.com>
2024-09-25 13:15:27 -0400

    Removes unneccesary import

--- src/frontend/screens/Audio/index.tsx
+++ src/frontend/screens/Audio/index.tsx
@@ -3,11 +3,10 @@ import {NativeStackNavigationOptions} from '@react-navigation/native-stack';
 
 import {DARK_GREY, WHITE} from '../../lib/styles';
 import {CustomHeaderLeft} from '../../sharedComponents/CustomHeaderLeft';
-import {NativeRootNavigationProps} from '../../sharedTypes/navigation';
 import {CreateRecording} from './CreateRecording';
 import {StatusBar} from 'expo-status-bar';
 
-export function Audio({route}: NativeRootNavigationProps<'Audio'>) {
+export function Audio() {
   return (
     <>
       <StatusBar style="light" />

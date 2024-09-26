8cf2bb7af5e93eb34c9b5137e18440ff1bab0aa1
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-09-26 12:14:58 -0700

    chore: remote archive as particpant

---
+++ src/frontend/screens/Settings/ProjectSettings/RemoteArchive/Particpant.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {Text} from '../../../../sharedComponents/Text';
+import {defineMessages, useIntl} from 'react-intl';
+
+const m = defineMessages({
+  archiveOff: {
+    id: 'ProjectSetting.RemoteArchive.Particpants.archiveOff',
+    defaultMessage: 'Remote Archive is Off',
+  },
+  dataNotShared: {
+    id: 'ProjectSetting.RemoteArchive.Particpants.dataNotShared',
+    defaultMessage:
+      'The data in your project is not shared over the internet. Only people in your project can see your data.',
+  },
+  experimentalFeature: {
+    id: 'ProjectSetting.RemoteArchive.Particpants.experimentalFeature',
+    defaultMessage:
+      'This is an experimental feature. You need a Remote Archive URL to enable Remote Archive.',
+  },
+  noServers: {
+    id: 'ProjectSetting.RemoteArchive.Particpants.noServers',
+    defaultMessage: 'No servers have been added to this project',
+  },
+  archiveOn: {
+    id: 'ProjectSetting.RemoteArchive.Particpants.archiveOn',
+    defaultMessage: 'Remote Archive is On',
+  },
+  dataSyncing: {
+    id: 'ProjectSetting.RemoteArchive.Particpants.dataSyncing',
+    defaultMessage:
+      'Your project data is syncing to the Remote Archive over the internet to the secure, encrypted server below. The server owner can view the data.',
+  },
+  coordinatorCanTurnOff: {
+    id: 'ProjectSetting.RemoteArchive.Particpants.coordinatorCanTurnOff',
+    defaultMessage: 'Only the Project Coordinator can turn off Remote Archive.',
+  },
+});
+
+export const Particpant = () => {
+  const [remoteArchiveOn] = React.useState(false);
+
+  return remoteArchiveOn ? <ArchiveOn /> : <ArchiveOff />;
+};
+
+const ArchiveOff = () => {
+  const {formatMessage} = useIntl();
+  return (
+    <View>
+      <Text>{formatMessage(m.archiveOff)}</Text>
+      <Text>{formatMessage(m.dataNotShared)}</Text>
+      <Text>{formatMessage(m.experimentalFeature)}</Text>
+      <Text>{formatMessage(m.noServers)}</Text>
+    </View>
+  );
+};
+
+const ArchiveOn = () => {
+  const {formatMessage} = useIntl();
+  return (
+    <View>
+      <Text>{formatMessage(m.archiveOn)}</Text>
+      <Text>{formatMessage(m.dataSyncing)}</Text>
+      <Text>{formatMessage(m.coordinatorCanTurnOff)}</Text>
+    </View>
+  );
+};

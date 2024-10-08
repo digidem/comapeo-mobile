5c86c6e8579e632be061c62c85d3bc789082db48
Evan Hahn <me@evanhahn.com>
2024-10-08 14:29:49 +0000

    Switch to dotenv-cli

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-const dotenv = require('dotenv');
 
 const requiredEnvVars = [
   'MAPBOX_ACCESS_TOKEN',
@@ -7,25 +6,15 @@ const requiredEnvVars = [
   'COMAPEO_METRICS_API_KEY',
 ];
 
-function checkRequiredEnvVars() {
-  for (const requiredEnvVar of requiredEnvVars) {
-    assert(
-      process.env[requiredEnvVar],
-      `Missing required environment variable: ${requiredEnvVar}`,
-    );
-  }
+for (const requiredEnvVar of requiredEnvVars) {
+  assert(
+    process.env[requiredEnvVar],
+    `Missing required environment variable: ${requiredEnvVar}`,
+  );
 }
 
 module.exports = function (api) {
-  const isTest = api.env('test');
-  if (isTest) {
-    dotenv.config({override: false});
-  }
-
-  checkRequiredEnvVars();
-
   api.cache(true);
-
   return {
     //https://github.com/babel/babel/discussions/13676#discussioncomment-1183149
     compact: true,

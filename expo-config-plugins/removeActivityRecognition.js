4ef381d68a6f44092af6bbc55b6fa0f351e97aa6
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-10-15 15:58:33 -0700

    fix plugin

--- expo-config-plugins/removeActivityRecognition.js
+++ expo-config-plugins/removeActivityRecognition.js
@@ -7,18 +7,20 @@ module.exports = function removeActivityRecognitionPermission(expoConfig) {
   return withAndroidManifest(expoConfig, config => {
     const manifest = config.modResults.manifest;
 
-    // Check and remove the ACTIVITY_RECOGNITION permission
-    const activityRecognitionIndex = manifest['uses-permission'].findIndex(
-      permission =>
-        permission.$['android:name'] ===
-        'com.google.android.gms.permission.ACTIVITY_RECOGNITION',
-    );
+    if (manifest['uses-permission']) {
+      // Check and remove the ACTIVITY_RECOGNITION permission
+      const activityRecognitionIndex = manifest['uses-permission'].findIndex(
+        permission =>
+          permission.$['android:name'] ===
+          'com.google.android.gms.permission.ACTIVITY_RECOGNITION',
+      );
 
-    if (
-      activityRecognitionIndex !== undefined &&
-      activityRecognitionIndex !== -1
-    ) {
-      manifest['uses-permission'].splice(activityRecognitionIndex, 1);
+      if (
+        activityRecognitionIndex !== undefined &&
+        activityRecognitionIndex !== -1
+      ) {
+        manifest['uses-permission'].splice(activityRecognitionIndex, 1);
+      }
     }
 
     return config;

e0e6e02e17d9aa0169b09e9b99314a4bfd95cb8d
Cindy Green <cimigreen@hotmail.com>
2024-10-29 10:27:39 -0400

    Adds start for detox testing locally.

---
+++ .detoxrc.js
@@ -0,0 +1,85 @@
+/** @type {Detox.DetoxConfig} */
+module.exports = {
+  testRunner: {
+    args: {
+      $0: 'jest',
+      config: 'e2e/jest.config.ts',
+    },
+    jest: {
+      setupTimeout: 120000,
+    },
+  },
+  apps: {
+    'android.debug': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      build:
+        'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [8081],
+    },
+    'android.release': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      build:
+        'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release -PreactNativeArchitectures=arm64-v8a,armeabi-v7a',
+    },
+    'android.cloud.release': {
+      type: 'android.cloud',
+      app: process.env.BROWSERSTACK_APP_URL,
+      appClient: process.env.BROWSERSTACK_APP_CLIENT_URL,
+    },
+  },
+  devices: {
+    attached: {
+      type: 'android.attached',
+      device: {
+        adbName: '.*',
+      },
+    },
+    emulator: {
+      type: 'android.emulator',
+      device: {
+        avdName: 'Pixel_6_API_34',
+      },
+    },
+    cloud: {
+      type: 'android.cloud',
+      device: {
+        name: DEVICE_NAME,
+        osVersion: DEVICE_OS_VERSION,
+      },
+    },
+    configurations: {
+      'android.att.debug': {
+        device: 'attached',
+        app: 'android.debug',
+      },
+      'android.att.release': {
+        device: 'attached',
+        app: 'android.release',
+      },
+      'android.emu.debug': {
+        device: 'emulator',
+        app: 'android.debug',
+      },
+      'android.emu.release': {
+        device: 'emulator',
+        app: 'android.release',
+      },
+      'android.cloud.release': {
+        device: 'cloud',
+        app: 'android.cloud.release',
+        cloudAuthentication: {
+          username: process.env.BROWSERSTACK_USERNAME,
+          accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
+        },
+        session: {
+          server: 'wss://detox.browserstack.com/init',
+          name: `${DEVICE_NAME} v${DEVICE_OS_VERSION}`,
+          build: BUILD_ID,
+          project: 'Detox Example',
+        },
+      },
+    },
+  },
+};

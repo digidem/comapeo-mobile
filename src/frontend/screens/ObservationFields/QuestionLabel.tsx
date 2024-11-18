dbbeda10b153f6f57a8ad8f31e612b386b944b5b
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-11-18 12:52:55 +0000

    feat: add number as field

--- src/frontend/screens/ObservationFields/QuestionLabel.tsx
+++ src/frontend/screens/ObservationFields/QuestionLabel.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 import {View, StyleSheet} from 'react-native';
 import {FormattedFieldProp} from '../../sharedComponents/FormattedData';
-import {Text} from '../../sharedComponents/Text';
 import {Field} from '@comapeo/schema';
+import {HeaderText} from '../../sharedComponents/Text/HeaderText';
+import {Header} from 'react-native/Libraries/NewAppScreen';
 
 interface Props {
   field: Field;
 }
 
 export const QuestionLabel = ({field}: Props) => {
-  const hint = <FormattedFieldProp field={field} propName="placeholder" />;
+  // const hint = <FormattedFieldProp field={field} propName="placeholder" />;
+
+  console.log({val: field.placeholder});
   return (
     <View style={styles.labelContainer}>
-      <Text style={styles.label}>
+      <HeaderText variant="header3">
         <FormattedFieldProp field={field} propName="label" />
-      </Text>
-      {hint ? <Text style={styles.hint}>{hint}</Text> : null}
+      </HeaderText>
+      {<HeaderText variant="header5">{field.helperText}</HeaderText>}
     </View>
   );
 };
@@ -26,11 +29,6 @@ const styles = StyleSheet.create({
     padding: 20,
     borderBottomWidth: 2,
     borderColor: '#F3F3F3',
-  },
-  label: {
-    fontSize: 20,
-    color: 'black',
-    fontWeight: '700',
   },
   hint: {
     fontSize: 16,

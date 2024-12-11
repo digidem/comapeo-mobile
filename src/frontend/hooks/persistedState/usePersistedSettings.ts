cb2cee3f336f25ceb9c6f2604d0886009972a53e
cimigree <cimigreen@hotmail.com>
2024-12-11 14:03:54 -0500

    chore: media management logic (#885)
    Adds new hook to use backend functions for getting and setting the media sync settings. Removes feature flag.

--- src/frontend/hooks/persistedState/usePersistedSettings.ts
+++ src/frontend/hooks/persistedState/usePersistedSettings.ts
@@ -1,29 +1,25 @@
 import {type StateCreator} from 'zustand';
 import {createPersistedState} from './createPersistedState';
-import {CoordinateFormat, MediaSyncSetting} from '../../sharedTypes';
+import {CoordinateFormat} from '../../sharedTypes';
 
 type SettingsSlice = {
   coordinateFormat: CoordinateFormat;
   manualCoordinateEntryFormat: CoordinateFormat;
-  mediaSyncSetting: MediaSyncSetting;
   actions: {
     setCoordinateFormat: (coordinateFormat: CoordinateFormat) => void;
     setManualCoordinateEntryFormat: (
       coordinateFormat: CoordinateFormat,
     ) => void;
-    setMediaSyncSetting: (mediaSyncSetting: MediaSyncSetting) => void;
   };
 };
 
 const settingsSlice: StateCreator<SettingsSlice> = (set, get) => ({
   coordinateFormat: 'utm',
   manualCoordinateEntryFormat: 'utm',
-  mediaSyncSetting: 'everything',
   actions: {
     setCoordinateFormat: coordinateFormat => set({coordinateFormat}),
     setManualCoordinateEntryFormat: coordinateFormat =>
       set({manualCoordinateEntryFormat: coordinateFormat}),
-    setMediaSyncSetting: mediaSyncSetting => set({mediaSyncSetting}),
   },
 });
 

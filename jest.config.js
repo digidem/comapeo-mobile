047aab78827673b9d7af00a6e43bba9abf083ad9
Andrew Chou <andrewchou@fastmail.com>
2024-12-18 13:36:58 -0500

    upgrade to @comapeo/core-react@1.1.0

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,6 @@ const config = {
   transformIgnorePatterns: [
     'node_modules/(?!(...|@rnmapbox|(jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
   ],
-  moduleNameMapper: {
-    // Jest seems to have issues with resolving '@comapeo/core-react' due to being a pure ESM module,
-    // but Metro doesn't when running the app
-    '^@comapeo/core-react': '<rootDir>/node_modules/@comapeo/core-react/dist',
-  },
 };
 
 module.exports = config;

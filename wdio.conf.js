ce3d036c6493a881f339239222e6e1d6eeb7f1a7
Andrew Chou <andrewchou@fastmail.com>
2025-02-13 10:05:24 -0500

    chore: use recommended type for wdio config (#967)

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,7 @@
 // @ts-check
 
-// `capabilities` missing in `Options.Testrunner` type: https://github.com/webdriverio/webdriverio/issues/13769
-/** @type {import('@wdio/types').Options.Testrunner & { capabilities: import('@wdio/types').Capabilities }} */
+// Type is very questionably declared in the global namespace via @wdio/types
+/** @type {WebdriverIO.Config} */
 const config = {
   runner: 'local',
   user: process.env.BROWSERSTACK_USERNAME,

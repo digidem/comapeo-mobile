b40ecc4208bbd1d232eeb52c6b5bc62d9cae5878
Cindy Green <cimigreen@hotmail.com>
2025-02-27 10:28:04 -0500

    Adds link to issue to comment

--- src/frontend/hooks/server/maps.ts
+++ src/frontend/hooks/server/maps.ts
@@ -36,6 +36,7 @@ export function useMapStyleJsonUrl() {
     queryFn: async () => {
       // If we're running E2E tests (e.g. on BrowserStack), fall back to a
       // public Mapbox style rather than our local style server to avoid 502 errors.
+      // (see https://github.com/digidem/comapeo-mobile/issues/1008)
       if (process.env.EXPO_PUBLIC_E2E_TEST) {
         return 'mapbox://styles/mapbox/streets-v11';
       }

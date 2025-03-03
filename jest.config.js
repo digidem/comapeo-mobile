fee2099d043f5edc8caa65389243560ec08cc69e
Andrew Chou <andrewchou@fastmail.com>
2025-03-03 11:20:48 -0500

    move default expo-localization mock to mocks directory

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@
 const config = {
   preset: 'jest-expo',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['@rnmapbox/maps/setup-jest', './jest.setup.js'],
+  setupFilesAfterEnv: ['@rnmapbox/maps/setup-jest'],
   transformIgnorePatterns: [
     'node_modules/(?!(...|@rnmapbox|(jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
   ],

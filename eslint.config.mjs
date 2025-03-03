4f2cd227a3984b8d2dc58bc426c445d9ad28adce
Andrew Chou <andrewchou@fastmail.com>
2025-03-03 11:49:50 -0500

    revert changes to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,7 +28,6 @@ const toolingConfig = pluginTs.config({
   name: 'tooling',
   files: [
     '*.config.{js,mjs,cjs}',
-    '*.setup.js',
     'scripts/*.{js,mjs,cjs}',
     'expo-config-plugins/*.{js,mjs,cjs}',
   ],
@@ -102,7 +101,7 @@ const frontendConfig = pluginTs.config(
   {
     ...pluginJest.configs['flat/recommended'],
     name: 'eslint-plugin-jest',
-    files: ['jest.setup.js', 'src/frontend/**/*.test.{js,jsx,mts,ts,tsx}'],
+    files: ['src/frontend/**/*.test.{js,jsx,mts,ts,tsx}'],
   },
 );
 

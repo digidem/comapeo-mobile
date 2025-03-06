d58ce11df188705e80929bd216e14d92924edb3f
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-03-06 12:10:31 -0800

    chore: update tab bar with

--- src/frontend/Navigation/Tab/TabBar.tsx
+++ src/frontend/Navigation/Tab/TabBar.tsx
@@ -4,6 +4,9 @@ import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import ObservationListIcon from '../../images/ObservationList.svg';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {COMAPEO_BLUE, MEDIUM_GREY} from '../../lib/styles';
+import {BodyText} from '../../sharedComponents/Text/BodyText';
+import {useTracking} from '../../hooks/useTracking';
+import {useTrackTimerContext} from '../../contexts/TrackTimerContext';
 
 const BUTTON_SIZE = 25;
 const HIT_SLOP = 20;
@@ -25,6 +28,7 @@ export const TabBar = ({navigation, state}: BottomTabBarProps) => {
         hitSlop={HIT_SLOP}
         testID="tabBarButtonObservationsList"
         accessibilityLabel="Go to ObservationsList"
+        style={styles.buttonStyle}
         onPress={() => navigation.navigate('ObservationsList')}>
         <ObservationListIcon
           color={
@@ -40,6 +44,7 @@ export const TabBar = ({navigation, state}: BottomTabBarProps) => {
         testID="tabBarButtonMap"
         accessibilityLabel="Go to Map"
         hitSlop={HIT_SLOP}
+        style={styles.buttonStyle}
         onPress={onPressMap}>
         <MaterialIcons
           color={
@@ -56,6 +61,7 @@ export const TabBar = ({navigation, state}: BottomTabBarProps) => {
         hitSlop={HIT_SLOP}
         testID="tabBarButtonCamera"
         accessibilityLabel="Go to Camera"
+        style={styles.buttonStyle}
         onPress={() => navigation.navigate('Camera')}>
         <MaterialIcons
           color={currentTab?.name === 'Camera' ? COMAPEO_BLUE : MEDIUM_GREY}
@@ -63,25 +69,47 @@ export const TabBar = ({navigation, state}: BottomTabBarProps) => {
           name="photo-camera"
         />
       </TouchableOpacity>
+
       <TouchableOpacity
         hitSlop={HIT_SLOP}
         testID="tabBarButtonTracking"
         accessibilityLabel="Go to Tracking"
+        style={styles.trackContainer}
         onPress={onPressTracks}>
-        <MaterialIcons
-          color={
-            currentTab?.name === 'Map' &&
-            currentTab?.params &&
-            'trackingOpen' in currentTab.params &&
-            currentTab.params.trackingOpen === true
-              ? COMAPEO_BLUE
-              : MEDIUM_GREY
+        <TrackButtonContent
+          isSelected={
+            !!(
+              currentTab?.name === 'Map' &&
+              currentTab?.params &&
+              'trackingOpen' in currentTab.params &&
+              currentTab.params.trackingOpen === true
+            )
           }
-          size={BUTTON_SIZE}
-          name="nordic-walking"
         />
       </TouchableOpacity>
     </View>
+  );
+};
+
+const TrackButtonContent = ({isSelected}: {isSelected: boolean}) => {
+  const {isTracking} = useTracking();
+  const {timer} = useTrackTimerContext();
+  return (
+    <>
+      {isTracking && (
+        <View style={styles.runtimeWrapper}>
+          <View style={styles.indicator} />
+          <BodyText variant="tinyMeta" style={styles.timer}>
+            {timer}
+          </BodyText>
+        </View>
+      )}
+      <MaterialIcons
+        color={isSelected ? COMAPEO_BLUE : MEDIUM_GREY}
+        size={BUTTON_SIZE}
+        name="nordic-walking"
+      />
+    </>
   );
 };
 
@@ -90,6 +118,31 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 25,
+    paddingHorizontal: 25,
+  },
+  buttonStyle: {
+    marginTop: 25,
+    marginBottom: 25,
+    flex: 1,
+    alignItems: 'center',
+  },
+  runtimeWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  indicator: {
+    marginRight: 5,
+    height: 10,
+    width: 10,
+    borderRadius: 99,
+    backgroundColor: '#59A553',
+  },
+  timer: {
+    marginLeft: 5,
+  },
+  trackContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
 });

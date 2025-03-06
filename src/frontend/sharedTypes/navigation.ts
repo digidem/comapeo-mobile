a04fdb94974a7df624b8dfffa546dc992b9862f1
Cindy Green <cimigreen@hotmail.com>
2025-03-06 09:58:28 -0500

    Makes observation id required to be passed to observation edit.

--- src/frontend/sharedTypes/navigation.ts
+++ src/frontend/sharedTypes/navigation.ts
@@ -49,7 +49,7 @@ export type RootStackParamsList = {
   PresetChooser: undefined;
   AddPhoto: undefined;
   Observation: {observationId: string};
-  ObservationEdit: {observationId: string} | undefined;
+  ObservationEdit: {observationId: string};
   ManualGpsScreen: undefined;
   ObservationDetails: {question: number};
   LeaveProjectScreen: undefined;

aa1dc21303aca0b57064ca96a53218b90c4297bd
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-03-12 15:44:17 -0700

    chore: track

--- src/frontend/screens/Track/index.tsx
+++ src/frontend/screens/Track/index.tsx
@@ -12,10 +12,10 @@ import {useObservations} from '../../hooks/server/observations.ts';
 import {NativeRootNavigationProps} from '../../sharedTypes/navigation';
 import {MapPreview} from './MapPreview.tsx';
 import {ObservationList} from './ObservationList.tsx';
-import {ErrorBottomSheetDeprecated} from '../../sharedComponents/ErrorBottomSheetDeprecated.tsx';
 import {ActionButtons} from '../../sharedComponents/ActionButtons.tsx';
 import {ScreenContentWithDock} from '../../sharedComponents/ScreenContentWithDock.tsx';
 import {TrackHeaderRight} from './TrackHeaderRight';
+import * as Sentry from '@sentry/react-native';
 
 const m = defineMessages({
   title: {
@@ -54,6 +54,10 @@ export const TrackScreen = ({
       onSuccess: () => {
         navigation.pop();
       },
+      onError: err => {
+        Sentry.captureException(err);
+        navigation.navigate('ErrorBottomSheet');
+      },
     });
   }
 
@@ -90,11 +94,6 @@ export const TrackScreen = ({
           <Text style={styles.text}>{track.tags.notes}</Text>
         </View>
       </ScreenContentWithDock>
-      <ErrorBottomSheetDeprecated
-        error={deleteTrackMutation.error}
-        clearError={deleteTrackMutation.reset}
-        tryAgain={deleteTrack}
-      />
     </SafeAreaView>
   );
 };

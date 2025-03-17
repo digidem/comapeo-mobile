3d8c4d2a5280b4148f89ce09ea3dc05f35201484
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-03-17 13:59:06 -0700

    chore: update track layer to only represent current track

--- src/frontend/screens/MapScreen/CurrentTrack/CurrrentTrackMapLayer.tsx
+++ src/frontend/screens/MapScreen/CurrentTrack/CurrrentTrackMapLayer.tsx
@@ -1,30 +1,18 @@
 import {LineJoin, LineLayer, ShapeSource} from '@rnmapbox/maps';
 import {usePersistedTrack} from '../../../hooks/persistedState/usePersistedTrack';
 import * as React from 'react';
-import {useLocation} from '../../../hooks/useLocation';
 import {convertToLineString} from '../../../lib/utils';
 
 export const CurrentTrackMapLayer = () => {
   const locationHistory = usePersistedTrack(state => state.locationHistory);
   const isTracking = usePersistedTrack(state => state.isTracking);
-  const {location} = useLocation({maxDistanceInterval: 3});
-  const finalLocationHistory = location?.coords
-    ? [
-        ...locationHistory,
-        {
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          timestamp: new Date().getTime(),
-        },
-      ]
-    : locationHistory;
 
   return (
     locationHistory.length > 1 &&
     isTracking && (
       <ShapeSource
         id="routeSource"
-        shape={convertToLineString(finalLocationHistory)}>
+        shape={convertToLineString(locationHistory)}>
         <LineLayer
           id="currentTrack"
           belowLayerID="mapboxUserLocationPulseCircle"

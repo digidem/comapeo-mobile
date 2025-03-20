423d5999e58378c3598e982bec957ecb5d788282
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-03-19 17:51:03 -0700

    chore: create opinionated button

---
+++ src/frontend/sharedComponents/Buttons.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {HeaderText} from './Text/HeaderText';
+import {BLUE_GREY, COMAPEO_BLUE, WARNING_RED, WHITE} from '../lib/styles';
+
+type ButtonProps = {
+  renderIcon?: (props: {color: string; size: number}) => React.ReactNode;
+  fullWidth: boolean;
+  text: string;
+  onPress: () => void;
+};
+
+export const PrimaryButton = ({
+  onPress,
+  renderIcon,
+  text,
+  fullWidth,
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.base,
+        {backgroundColor: COMAPEO_BLUE, width: fullWidth ? `100%` : 280},
+      ]}
+      onPress={onPress}>
+      {renderIcon && (
+        <View style={styles.iconSpacing}>
+          {renderIcon({color: WHITE, size: 25})}
+        </View>
+      )}
+      <HeaderText variant="header5" style={{color: WHITE}}>
+        {text}
+      </HeaderText>
+    </TouchableOpacity>
+  );
+};
+
+export const SecondaryButton = ({
+  onPress,
+  renderIcon,
+  text,
+  fullWidth,
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.base,
+        {
+          backgroundColor: WHITE,
+          borderWidth: 1.5,
+          borderColor: BLUE_GREY,
+          width: fullWidth ? `100%` : 280,
+        },
+      ]}
+      onPress={onPress}>
+      {renderIcon && (
+        <View style={styles.iconSpacing}>
+          {renderIcon({color: COMAPEO_BLUE, size: 25})}
+        </View>
+      )}
+      <HeaderText variant="header5" style={{color: COMAPEO_BLUE}}>
+        {text}
+      </HeaderText>
+    </TouchableOpacity>
+  );
+};
+
+export const DestructiveButton = ({
+  onPress,
+  renderIcon,
+  text,
+  fullWidth,
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.base,
+        {backgroundColor: WARNING_RED, width: fullWidth ? `100%` : 280},
+      ]}
+      onPress={onPress}>
+      {renderIcon && (
+        <View style={styles.iconSpacing}>
+          {renderIcon({color: WHITE, size: 25})}
+        </View>
+      )}
+      <HeaderText variant="header5" style={{color: WHITE}}>
+        {text}
+      </HeaderText>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  base: {
+    height: 50,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    overflow: 'hidden',
+  },
+  iconSpacing: {
+    marginRight: 10,
+  },
+});

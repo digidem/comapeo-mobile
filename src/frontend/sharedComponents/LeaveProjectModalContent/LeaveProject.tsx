7432b96761037b6e214d817de4b409ccfa7b3c87
Cindy Green <cimigreen@hotmail.com>
2025-03-21 18:03:16 -0400

    Fixes order in invite flow.

--- src/frontend/sharedComponents/LeaveProjectModalContent/LeaveProject.tsx
+++ src/frontend/sharedComponents/LeaveProjectModalContent/LeaveProject.tsx
@@ -95,8 +95,8 @@ export const LeaveProject = ({
             {projectId},
             {
               onSuccess: () => {
-                switchActiveProject(projectPublicId);
                 closeSheet();
+                switchActiveProject(projectPublicId);
               },
               onError: err => {
                 setLeaveProjectError(err);

8f8c090104afe491f8ca1e7b57eb458d808534c6
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-04-02 11:40:30 -0700

    chore: create resusable invite state listener

---
+++ src/frontend/hooks/useListenToInviteStateUpdate.ts
@@ -0,0 +1,41 @@
+import {Invite} from '@comapeo/core/dist/invite/invite-api';
+import {useEffect} from 'react';
+import {useNavigationFromRoot} from './useNavigationWithTypes';
+import * as Sentry from '@sentry/react-native';
+import {useClientApi} from '@comapeo/core-react';
+
+export function useListenToInviteStateUpdate(inviteId: string) {
+  const navigation = useNavigationFromRoot();
+  const {invite: mapeoApiInvite} = useClientApi();
+  useEffect(() => {
+    function navigateBasedOnInviteState(invite: Invite) {
+      if (invite.inviteId !== inviteId) return;
+
+      if (invite.state === 'canceled') {
+        navigation.replace('InviteCancelled', {
+          projectName: invite.projectName,
+        });
+        return;
+      }
+
+      if (invite.state === 'error') {
+        Sentry.captureException(invite.error);
+        navigation.replace('ErrorBottomSheet');
+        return;
+      }
+
+      if (invite.state !== 'pending') {
+        navigation.goBack();
+      }
+    }
+
+    mapeoApiInvite.addListener('invite-updated', navigateBasedOnInviteState);
+
+    return () => {
+      mapeoApiInvite.removeListener(
+        'invite-updated',
+        navigateBasedOnInviteState,
+      );
+    };
+  }, [mapeoApiInvite, inviteId, navigation]);
+}

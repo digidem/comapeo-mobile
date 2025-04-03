729809c18b52c0a69634e531479c1dbc19cbc8b3
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-04-03 14:34:00 -0700

    chore: update constants

--- src/frontend/constants.ts
+++ src/frontend/constants.ts
@@ -3,16 +3,17 @@ import {AppStackParamsList, HomeTabsParamsList} from './sharedTypes/navigation';
 // this has to be a string because js does not recognize 00000 as being 5 digits
 export const OBSCURE_PASSCODE = '00000';
 
-export const EDITING_SCREEN_NAMES:
-  | Omit<keyof AppStackParamsList, 'Home'>[]
-  | (keyof HomeTabsParamsList)[] = [
+export const EDITING_SCREEN_NAMES: (
+  | keyof AppStackParamsList
+  | keyof HomeTabsParamsList
+)[] = [
   'AddPhoto',
   'PresetChooser',
   'ManualGpsScreen',
   'ObservationCreate',
   'ObservationDetails',
   'ObservationEdit',
-  'SaveTrackScreen',
+  'SaveTrack',
   'TrackEdit',
   'Camera',
   'IntroToCoMapeo',
@@ -24,12 +25,12 @@ export const EDITING_SCREEN_NAMES:
   'ErrorBottomSheet',
 ];
 
-export const INVITE_SCREEN_NAME: Omit<keyof AppStackParamsList, 'Home'>[] = [
+export const INVITE_SCREEN_NAME: (keyof AppStackParamsList)[] = [
   'InviteReceived',
-  'InviteCancelled',
-  'InviteSuccessfullyJoined',
+  'InviteCanceled',
+  'InviteSuccessfullyAccepted',
   'LeaveProject',
-  'AlreadyOnProject',
+  'ExistingProjectWarning',
 ];
 
 // Replicates the root query key from comapeo/core-react v3.3.0

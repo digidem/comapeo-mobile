060b30088cc1a853f4b958fa2607b7535ae7560b
Cindy Green <cimigreen@hotmail.com>
2025-04-15 17:43:13 -0400

    Puts tests back.

--- tests/e2e/specs/observations/view-observations.test.ts
+++ tests/e2e/specs/observations/view-observations.test.ts
@@ -71,11 +71,9 @@ describe('Observations - View Observations Flow', () => {
   it('should display my role in the observations list', async () => {
     const myRoleText = await $(byResourceId('OBS.card-text'));
     await expect(myRoleText).toBeDisplayed();
-    // temporary change until project naming comes back easily
-    // await expect(myRoleText).toHaveText(
-    //   'You’re a coordinator on this project.',
-    // );
-    await expect(myRoleText).toHaveText('You’re mapping on your own.');
+    await expect(myRoleText).toHaveText(
+      'You’re a coordinator on this project.',
+    );
   });
 
   it('should toggle camera tab and back to confirm correct place', async () => {

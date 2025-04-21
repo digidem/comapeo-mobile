7b2f270f2647512896414739de00b13a36e4cde9
Andrew Chou <andrewchou@fastmail.com>
2025-04-21 15:43:20 -0400

    WIP: screen test example

---
+++ src/frontend/screens/Exchange.test.tsx
@@ -0,0 +1,35 @@
+import {render} from '@testing-library/react-native';
+
+import {SyncScreen} from './Exchange';
+import {createManager, setUpIPC} from '../../../tests/integration/helpers/core';
+import {createAppProvidersWrapper} from '../../../tests/integration/helpers/react';
+
+describe('Exchange screen', async () => {
+  const {manager, fastifyController} = createManager();
+  const {client, server} = setUpIPC({manager});
+
+  beforeAll(() => {
+    fastifyController.start();
+  });
+
+  afterAll(() => {
+    fastifyController.stop();
+    server.close();
+  });
+
+  test('basic', () => {
+    const wrapper = createAppProvidersWrapper({mapeoApi: client});
+
+    const navigation = {
+      goBack: jest.fn(),
+    };
+
+    render(
+      <SyncScreen
+        // @ts-expect-error Not ideal but bare minimum navigation needed by the screen
+        navigation={navigation}
+      />,
+      {wrapper},
+    );
+  });
+});

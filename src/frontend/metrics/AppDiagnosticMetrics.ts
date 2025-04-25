digidem/comapeo-mobile

commit c6db215964e474ca868998ab608ad79118229e4b

chore: send app diagnostic metrics (#480)
This sends app diagnostic metrics, which includes things like app and OS
versions, locale, and more.

Reports are queued for the month and sent when the user is online.

I tried to test as much as I could, but some of it is really tricky to
check automatically.

See also: 39773c54826ed1f13a679c835d0bfd81a24e719d

src/frontend/metrics/AppDiagnosticMetrics.ts
@@ -1,0 +1,204 @@
+import * as NetInfo from '@react-native-community/netinfo';
+import * as Sentry from '@sentry/react-native';
+import * as Application from 'expo-application';
+import {getLastKnownPositionAsync} from 'expo-location';
+import {AppState, Platform} from 'react-native';
+import {storage} from '../hooks/persistedState/createPersistedState';
+import {
+  getDeviceLanguageTag,
+  usePersistedLocale,
+} from '../hooks/persistedState/usePersistedLocale';
+import {assert} from '../lib/assert';
+import {MINUTE_MS, formatIsoUtc, parseIsoUtc} from '../lib/date';
+import {first} from '../lib/first';
+import {last} from '../lib/last';
+import {maybeJsonParse} from '../lib/maybeJsonParse';
+import {OneAtATimeQueue} from '../lib/OneAtATimeQueue';
+import {setIfNotNull} from '../lib/setIfNotNull';
+import {
+  type AppDiagnosticMetricsQueue,
+  type AppDiagnosticMetricsReport,
+  hasReportForToday,
+  truncateReportsByTime,
+  updateQueueHighWatermark,
+} from './AppDiagnosticMetricsQueue';
+import {getMetricsDeviceId} from './getMetricsDeviceId';
+import {getMetricsRequestInfo} from './getMetricsRequestInfo';
+import {getMonthlyHash} from './getMonthlyHash';
+import {positionToCountries} from './positionToCountries';
+import {sendMetricsData} from './sendMetricsData';
+
+const STORAGE_KEY = 'AppDiagnosticMetricsQueue';
+const CHECK_INTERVAL = 5 * MINUTE_MS;
+
+function loadQueueFromStorage(): AppDiagnosticMetricsQueue {
+  const storedString = storage.getString(STORAGE_KEY);
+  if (!storedString) return {reports: []};
+
+  const stored = maybeJsonParse(storedString);
+  if (!stored) return {reports: []};
+
+  return stored as AppDiagnosticMetricsQueue;
+}
+
+async function getCountry(): Promise<undefined | string> {
+  const lastKnownPosition = await getLastKnownPositionAsync().catch(() => null);
+  if (!lastKnownPosition) return undefined;
+  const {latitude, longitude} = lastKnownPosition.coords;
+  const countries = positionToCountries(latitude, longitude);
+  return first(countries);
+}
+
+async function generateAppDiagnosticMetricsData(): Promise<AppDiagnosticMetricsReport> {
+  const result: AppDiagnosticMetricsReport = {
+    dateGenerated: formatIsoUtc(new Date()),
+    os: Platform.OS,
+    osVersion: Platform.Version,
+    deviceLocale: getDeviceLanguageTag(),
+    appLocale: usePersistedLocale.getState().locale,
+    country: await getCountry(),
+  };
+
+  setIfNotNull(result, 'appId', Application.applicationId);
+  setIfNotNull(result, 'appName', Application.applicationName);
+  setIfNotNull(
+    result,
+    'nativeApplicationVersion',
+    Application.nativeApplicationVersion,
+  );
+  setIfNotNull(result, 'nativeBuildVersion', Application.nativeBuildVersion);
+
+  return result;
+}
+
+function saveQueueToStorage(queue: Readonly<AppDiagnosticMetricsQueue>): void {
+  storage.set(STORAGE_KEY, JSON.stringify(queue));
+}
+
+export class AppDiagnosticMetrics {
+  #isEnabled = false;
+  #isOnline = false;
+
+  #subscriptionCleanupFns: null | (() => void)[] = null;
+
+  #update;
+  #updateQueue = new OneAtATimeQueue();
+
+  constructor() {
+    this.#update = () => {
+      this.#updateQueue.add(() => this.#doUpdate());
+    };
+  }
+
+  setEnabled(isEnabled: boolean): void {
+    this.#isEnabled = isEnabled;
+
+    if (isEnabled) {
+      this.#addListenersIfNecessary();
+    } else {
+      this.#removeListeners();
+    }
+
+    this.#update();
+  }
+
+  #addListenersIfNecessary(): void {
+    if (this.#subscriptionCleanupFns) return;
+
+    const subscriptionCleanupFns: (() => void)[] = [];
+
+    subscriptionCleanupFns.push(
+      NetInfo.addEventListener(state => {
+        this.#isOnline = Boolean(state.isInternetReachable);
+        this.#update();
+      }),
+    );
+
+    const appStateSubscription = AppState.addEventListener(
+      'change',
+      this.#update,
+    );
+    subscriptionCleanupFns.push(() => appStateSubscription.remove());
+
+    // We could be smarter about this. For example, we could set the timer for
+    // beginning of the next day. However, timeouts are a bit unreliable (for
+    // example, if the app goes into the background), so we just check
+    // periodically. This also simplifies the code a bit.
+    setInterval(this.#update, CHECK_INTERVAL);
+
+    this.#subscriptionCleanupFns = subscriptionCleanupFns;
+  }
+
+  #removeListeners(): void {
+    const subscriptionCleanupFns = this.#subscriptionCleanupFns ?? [];
+    for (const fn of subscriptionCleanupFns) fn();
+  }
+
+  /**
+   * Update the queue and send metrics as needed.
+   *
+   * Should only be run once at a time. See `this.#update` for a queued alternative.
+   */
+  async #doUpdate(): Promise<void> {
+    if (!this.#isEnabled) {
+      storage.delete(STORAGE_KEY);
+      return;
+    }
+
+    let queue = loadQueueFromStorage();
+
+    const now = new Date();
+
+    const numberOfReportsBeforeTruncation = queue.reports.length;
+    queue = truncateReportsByTime(queue, now);
+    const numberOfReportsAfterTruncation = queue.reports.length;
+    let hasChangedQueue =
+      numberOfReportsBeforeTruncation !== numberOfReportsAfterTruncation;
+
+    if (!hasReportForToday(queue)) {
+      queue = {
+        ...queue,
+        reports: [...queue.reports, await generateAppDiagnosticMetricsData()],
+      };
+      hasChangedQueue = true;
+    }
+
+    if (hasChangedQueue) saveQueueToStorage(queue);
+
+    const newestReport = last(queue.reports);
+
+    const shouldSendMetrics =
+      this.#isEnabled &&
+      this.#isOnline &&
+      AppState.currentState === 'active' &&
+      !!newestReport;
+    if (!shouldSendMetrics) return;
+
+    try {
+      const newestReportDate = parseIsoUtc(newestReport.dateGenerated);
+      assert(newestReportDate, 'Expected report to be generated');
+
+      await sendMetricsData({
+        ...getMetricsRequestInfo(),
+        dataToSend: {
+          type: 'app diagnostics v1',
+          monthlyDeviceHash: await getMonthlyHash(
+            'app diagnostics',
+            getMetricsDeviceId(),
+            // We prefer the date from the newest report, not `new Date()`.
+            // These will usually be the same, but it's possible for a user to
+            // send metrics right at the end of the month which would generate
+            // a new monthly hash, letting us track users across months.
+            newestReportDate,
+          ),
+          reports: queue.reports,
+        },
+      });
+
+      queue = updateQueueHighWatermark(queue);
+      saveQueueToStorage(queue);
+    } catch (err) {
+      Sentry.captureException(err);
+    }
+  }
+}

src/frontend/App.tsx
@@ -14,6 +14,7 @@ import {storage} from './hooks/persistedState/createPersistedState';
 import {tracksStore} from './hooks/persistedState/usePersistedTrack';
 import {useOnBackgroundedAndForegrounded} from './hooks/useOnBackgroundedAndForegrounded';
 import {getSentryUserId} from './metrics/getSentryUserId';
+import {AppDiagnosticMetrics} from './metrics/AppDiagnosticMetrics';
 import {DeviceDiagnosticMetrics} from './metrics/DeviceDiagnosticMetrics';
 
 Sentry.init({
@@ -32,6 +33,7 @@ Sentry.init({
 const messagePort = new MessagePortLike();
 const mapeoApi = createMapeoClient(messagePort, {timeout: Infinity});
 const localDiscoveryController = createLocalDiscoveryController(mapeoApi);
+const appDiagnosticMetrics = new AppDiagnosticMetrics();
 const deviceDiagnosticMetrics = new DeviceDiagnosticMetrics();
 localDiscoveryController.start();
 initializeNodejs();
@@ -70,6 +72,11 @@ const App = () => {
   }, []);
 
   useOnBackgroundedAndForegrounded(mapeoApi);
+
+  React.useEffect(() => {
+    // TODO: Set this conditionally based on consent.
+    appDiagnosticMetrics.setEnabled(true);
+  }, []);
 
   React.useEffect(() => {
     // TODO: Set this conditionally based on consent.

src/frontend/hooks/persistedState/usePersistedLocale.ts
@@ -9,11 +9,14 @@ type LocaleSlice = {
   setLocale: (locale: string) => void;
 };
 
-const localeSlice: StateCreator<LocaleSlice> = set => ({
+export const getDeviceLanguageTag = (): string =>
   // We can use this non-null assertion with `getLocales()` because, according
   // to [the docs][1], the result is "guaranteed to contain at least 1 element."
   // [1]: https://github.com/expo/expo/blob/5585320eec9271038cd7c672b4cf9f0e945ca658/packages/expo-localization/src/Localization.ts#L123
-  locale: getSupportedLocale(getLocales()[0]!.languageTag) || 'en',
+  getLocales()[0]!.languageTag;
+
+const localeSlice: StateCreator<LocaleSlice> = set => ({
+  locale: getSupportedLocale(getDeviceLanguageTag()) || 'en',
   setLocale: newlocale => set({locale: newlocale}),
 });
 

src/frontend/lib/OneAtATimeQueue.test.ts
@@ -1,0 +1,45 @@
+import {setTimeout} from 'node:timers/promises';
+import {OneAtATimeQueue} from './OneAtATimeQueue';
+
+describe('OneAtATimeQueue', () => {
+  it('queues operations to be run one at a time', async () => {
+    const queue = new OneAtATimeQueue();
+
+    let opsRunning = 0;
+    let opsCompleted = 0;
+
+    const {promise, resolve} = promiseWithResolvers();
+
+    const op = async () => {
+      expect(opsRunning).toBe(0);
+
+      opsRunning++;
+
+      await setTimeout(10);
+
+      opsRunning--;
+      expect(opsRunning).toBe(0);
+
+      opsCompleted++;
+
+      if (opsCompleted >= 3) {
+        resolve();
+      }
+    };
+
+    queue.add(op);
+    queue.add(op);
+    queue.add(op);
+
+    await promise;
+    expect(opsCompleted).toBe(3);
+  });
+});
+
+function promiseWithResolvers(): {promise: Promise<void>; resolve: () => void} {
+  let resolve: () => void = () => {};
+  const promise = new Promise<void>(res => {
+    resolve = res;
+  });
+  return {promise, resolve};
+}

src/frontend/lib/OneAtATimeQueue.ts
@@ -1,0 +1,22 @@
+export class OneAtATimeQueue {
+  #queue: (() => Promise<void>)[] = [];
+  #isProcessing = false;
+
+  add(fn: () => Promise<void>) {
+    this.#queue.push(fn);
+    this.#processQueue();
+  }
+
+  #processQueue() {
+    if (this.#isProcessing) return;
+
+    const callback = this.#queue.shift();
+    if (!callback) return;
+
+    this.#isProcessing = true;
+    callback().finally(() => {
+      this.#isProcessing = false;
+      this.#processQueue();
+    });
+  }
+}

src/frontend/lib/date.test.ts
@@ -1,5 +1,32 @@
-import {isDateValid, isSameUtcMonthAndYear} from './date';
 import {parseISO} from 'date-fns';
+import {
+  beginningOfMonthUtc,
+  formatIsoUtc,
+  isDateValid,
+  isSameUtcMonthAndYear,
+  parseIsoUtc,
+} from './date';
+
+describe('beginningOfMonthUtc', () => {
+  it('returns the first day of the month (in UTC)', () => {
+    const input = parseISO('2012-03-04T06:09:42.0Z');
+    const result = beginningOfMonthUtc(input);
+    expect(result.getUTCFullYear()).toBe(2012);
+    expect(result.getUTCMonth()).toBe(2);
+    expect(result.getUTCDate()).toBe(1);
+    expect(result.getUTCHours()).toBe(0);
+    expect(result.getUTCMinutes()).toBe(0);
+    expect(result.getUTCSeconds()).toBe(0);
+    expect(result.getUTCMilliseconds()).toBe(0);
+  });
+});
+
+describe('formatIsoUtc', () => {
+  it('formats dates (without times)', () => {
+    const date = parseISO('2012-03-04T06:09:42.0Z');
+    expect(formatIsoUtc(date)).toBe('2012-03-04');
+  });
+});
 
 describe('isDateValid', () => {
   it('returns true for valid dates', () => {
@@ -27,5 +54,87 @@ describe('isSameUtcMonthAndYear', () => {
   it('returns false for dates not in the same month and year', () => {
     expect(isSameUtcMonthAndYear(day1, day3)).toBe(false);
     expect(isSameUtcMonthAndYear(day1, day4)).toBe(false);
+  });
+});
+
+describe('parseIsoUtc', () => {
+  it('returns null for invalid date strings', () => {
+    const testCases = [
+      '',
+      '1234',
+      '123-06-09',
+      '20000102',
+      '2000-13-01',
+      '2000-12-32',
+      '2001-02-29',
+      ' 2001-01-02 ',
+      new Date().toISOString(),
+    ];
+    for (const input of testCases) {
+      expect(parseIsoUtc(input)).toBeNull();
+    }
+  });
+
+  it('parses every day in a leap year', () => {
+    const year = 2024;
+    for (let month = 1; month <= 12; month++) {
+      let daysInMonth = 31;
+      if (month === 2) {
+        daysInMonth = 29;
+      } else if ([4, 6, 9, 11].includes(month)) {
+        daysInMonth = 30;
+      }
+      for (let date = 1; date <= daysInMonth; date++) {
+        const str = `${year}-${month.toString().padStart(2, '0')}-${date.toString().padStart(2, '0')}`;
+        const parsed = parseIsoUtc(str);
+        expect(parsed?.getUTCFullYear()).toBe(year);
+        expect(parsed?.getUTCMonth()).toBe(month - 1);
+        expect(parsed?.getUTCDate()).toBe(date);
+        expect(parsed?.getUTCHours()).toBe(0);
+        expect(parsed?.getUTCMinutes()).toBe(0);
+        expect(parsed?.getUTCSeconds()).toBe(0);
+        expect(parsed?.getUTCMilliseconds()).toBe(0);
+      }
+    }
+  });
+
+  it('parses every day in a non-leap year', () => {
+    const year = 2023;
+    for (let month = 1; month <= 12; month++) {
+      let daysInMonth = 31;
+      if (month === 2) {
+        daysInMonth = 28;
+      } else if ([4, 6, 9, 11].includes(month)) {
+        daysInMonth = 30;
+      }
+      for (let date = 1; date <= daysInMonth; date++) {
+        const str = `${year}-${month.toString().padStart(2, '0')}-${date.toString().padStart(2, '0')}`;
+        const parsed = parseIsoUtc(str);
+        expect(parsed?.getUTCFullYear()).toBe(year);
+        expect(parsed?.getUTCMonth()).toBe(month - 1);
+        expect(parsed?.getUTCDate()).toBe(date);
+        expect(parsed?.getUTCHours()).toBe(0);
+        expect(parsed?.getUTCMinutes()).toBe(0);
+        expect(parsed?.getUTCSeconds()).toBe(0);
+        expect(parsed?.getUTCMilliseconds()).toBe(0);
+      }
+    }
+  });
+
+  it("parses today's date", () => {
+    const today = new Date();
+    const parsed = parseIsoUtc(formatIsoUtc(today));
+    expect(parsed?.getUTCFullYear()).toBe(today.getUTCFullYear());
+    expect(parsed?.getUTCMonth()).toBe(today.getUTCMonth());
+    expect(parsed?.getUTCDate()).toBe(today.getUTCDate());
+    expect(parsed?.getUTCHours()).toBe(0);
+    expect(parsed?.getUTCMinutes()).toBe(0);
+    expect(parsed?.getUTCSeconds()).toBe(0);
+    expect(parsed?.getUTCMilliseconds()).toBe(0);
+  });
+
+  it('parses years far in the future', () => {
+    const parsed = parseIsoUtc('12345-02-03');
+    expect(parsed?.getUTCFullYear()).toBe(12345);
   });
 });

src/frontend/lib/date.ts
@@ -1,3 +1,19 @@
+export const SECOND_MS = 1000;
+export const MINUTE_MS = SECOND_MS * 60;
+export const HOUR_MS = MINUTE_MS * 60;
+export const DAY_MS = HOUR_MS * 24;
+const ISO_DATE_REGEXP = /^(?<year>\d{4,})-(?<month>\d{2})-(?<day>\d{2})$/;
+
+export const beginningOfMonthUtc = (date: Readonly<Date>): Date =>
+  new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1));
+
+export const formatIsoUtc = (date: Readonly<Date>): string =>
+  [
+    date.getUTCFullYear().toString().padStart(4, '0'),
+    (date.getUTCMonth() + 1).toString().padStart(2, '0'),
+    date.getDate().toString().padStart(2, '0'),
+  ].join('-');
+
 export const isDateValid = (date: Readonly<Date>): boolean =>
   !isNaN(date.valueOf());
 
@@ -11,3 +27,28 @@ export const isSameUtcMonthAndYear = (
   a: Readonly<Date>,
   b: Readonly<Date>,
 ): boolean => isSameUtcMonth(a, b) && isSameUtcYear(a, b);
+
+const parseMaybeInt = (str: undefined | string): number =>
+  parseInt(str || '', 10);
+
+const getDaysInMonth = (year: number, monthIndex: number): number =>
+  new Date(year, monthIndex + 1, 0, 0).getDate();
+
+export const parseIsoUtc = (str: string): null | Date => {
+  const matchGroups = str.match(ISO_DATE_REGEXP)?.groups;
+  if (!matchGroups) return null;
+
+  const year = parseMaybeInt(matchGroups.year);
+  if (!year) return null;
+
+  const monthIndex = parseMaybeInt(matchGroups.month) - 1;
+  if (monthIndex < 0 || monthIndex > 11) return null;
+
+  const day = parseMaybeInt(matchGroups.day);
+  if (!day || day > getDaysInMonth(year, monthIndex)) return null;
+
+  const result = new Date(
+    Date.UTC(year, monthIndex, parseMaybeInt(matchGroups.day)),
+  );
+  return isDateValid(result) ? result : null;
+};

src/frontend/lib/first.test.ts
@@ -1,0 +1,13 @@
+import {first} from './first';
+
+describe('first', () => {
+  it('returns undefined for empty iterables', () => {
+    expect(first([])).toBeUndefined();
+    expect(first(new Set())).toBeUndefined();
+  });
+
+  it('returns the first element of an iterable', () => {
+    expect(first([1, 2, 3])).toBe(1);
+    expect(first(new Set([1, 2, 3]))).toBe(1);
+  });
+});

commit f6aa076634001562ae20dc280800a63c6a0704c9

fix: clean up app metrics interval
The app diagnostic metrics class starts an interval but never cleans it
up. This has two problems:

1. If you enable, disable, and re-enable app metrics, two timers will be
	 started. Disabling and re-enabling will start a third timer, and so
	 on. Other code protects against catastrophic issues here, but this is
	 a memory leak.

2. When we add Jest tests (see [#910]), failure to clean this up will
	 prevent the test from finishing. Not yet an issue, but soon to be.

(I believe this was my mistake, introduced way back in
c6db215964e474ca868998ab608ad79118229e4b.)

[#910]: https://github.com/digidem/comapeo-mobile/issues/910

src/frontend/metrics/AppDiagnosticMetrics.ts
@@ -139,7 +139,8 @@ export class AppDiagnosticMetrics {
     // beginning of the next day. However, timeouts are a bit unreliable (for
     // example, if the app goes into the background), so we just check
     // periodically. This also simplifies the code a bit.
-    setInterval(this.#update, CHECK_INTERVAL);
+    const checkInterval = setInterval(this.#update, CHECK_INTERVAL);
+    subscriptionCleanupFns.push(() => clearInterval(checkInterval));
 
     this.#subscriptionCleanupFns = subscriptionCleanupFns;
   }
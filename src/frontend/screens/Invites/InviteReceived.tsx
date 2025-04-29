4eba6bb0ddfc2675a3bf1d64e25225eeb8d5c802
cimigree <cimigreen@hotmail.com>
2025-04-29 15:21:47 -0400

    feat: updates invite received sheet (#1116)
    * Adds new styling and UI for the invite received sheet.

--- src/frontend/screens/Invites/InviteReceived.tsx
+++ src/frontend/screens/Invites/InviteReceived.tsx
@@ -1,7 +1,5 @@
 import {BottomSheetWrapper} from '../../sharedComponents/BottomSheetWrapper';
-import InviteIcon from '../../images/AddPersonCircle.svg';
 import {StyleSheet, View} from 'react-native';
-import {LIGHT_GREY} from '../../lib/styles';
 import {PrimaryButton, SecondaryButton} from '../../sharedComponents/Buttons';
 import {defineMessages, useIntl} from 'react-intl';
 import {NativeRootNavigationProps} from '../../sharedTypes/navigation';
@@ -17,23 +15,32 @@ import {UIActivityIndicator} from 'react-native-indicators';
 import {useActiveProjectIdActions} from '../../contexts/ActiveProjectIdStoreContext';
 import * as Sentry from '@sentry/react-native';
 import {useListenToInviteCancel} from '../../hooks/useListenToInviteCancel';
+import {BLACK, NEW_DARK_GREY, VERY_LIGHT_GREY} from '../../lib/styles';
 
 const m = defineMessages({
-  acceptInvite: {
-    id: 'screens.InviteReceived.acceptInvite',
-    defaultMessage: 'Accept Invite',
+  joinProject: {
+    id: 'screens.InviteReceived.joinProject',
+    defaultMessage: 'Join Project',
   },
   declineInvite: {
     id: 'screens.InviteReceived.declineInvite',
     defaultMessage: 'Decline Invite',
   },
-  joinProject: {
-    id: 'screens.InviteReceived.joinProject',
-    defaultMessage: 'Join {projectName}',
-  },
   invitedToJoin: {
     id: 'screens.InviteReceived.invitedToJoin',
-    defaultMessage: "You've been invited to join {projectName}",
+    defaultMessage: "You've been invited to...",
+  },
+  joinAsRole: {
+    id: 'screens.InviteReceived.joinAsRole',
+    defaultMessage: 'Join as a {role}?',
+  },
+  coordinatorRole: {
+    id: 'screens.InviteReceived.coordinatorRole',
+    defaultMessage: 'coordinator',
+  },
+  participantRole: {
+    id: 'screens.InviteReceived.participantRole',
+    defaultMessage: 'participant',
   },
 });
 
@@ -44,6 +51,11 @@ export const InviteReceived = ({
   const {formatMessage} = useIntl();
   const inviteId = route.params.inviteId;
   const {data: invite} = useSingleInvite({inviteId});
+  const translatedRole =
+    invite.roleName === 'Coordinator'
+      ? formatMessage(m.coordinatorRole)
+      : formatMessage(m.participantRole);
+
   const acceptInvite = useAcceptInvite();
   const rejectInvite = useRejectInvite();
   const {setActiveProjectId} = useActiveProjectIdActions();
@@ -92,52 +104,81 @@ export const InviteReceived = ({
 
   return (
     <BottomSheetWrapper>
-      <View style={styles.inviteIcon}>
-        <InviteIcon fill={LIGHT_GREY} width={80} height={80} />
-      </View>
-      <HeaderText
-        style={{textAlign: 'center', marginTop: 20}}
-        variant="header2">
-        {formatMessage(m.joinProject, {projectName: invite.projectName})}
-      </HeaderText>
-      <BodyText style={{textAlign: 'center', marginTop: 20}}>
-        {formatMessage(m.invitedToJoin, {projectName: invite.projectName})}
-      </BodyText>
-      <View style={styles.buttonContainer}>
-        {acceptInvite.status === 'pending' ||
-        rejectInvite.status === 'pending' ? (
-          <UIActivityIndicator style={{marginVertical: 20}} />
-        ) : (
-          <>
-            <SecondaryButton
-              fullSize
-              onPress={reject}
-              text={formatMessage(m.declineInvite)}
-            />
-            <PrimaryButton
-              fullSize
-              style={{marginTop: 10}}
-              onPress={accept}
-              text={formatMessage(m.acceptInvite)}
-            />
-          </>
-        )}
+      <View style={styles.container}>
+        <BodyText variant="tinyMeta" style={styles.invitedLabel}>
+          {formatMessage(m.invitedToJoin)}
+        </BodyText>
+
+        <View style={styles.cardContainer}>
+          <HeaderText variant="header2" style={styles.projectName}>
+            {invite.projectName}
+          </HeaderText>
+          <BodyText variant="smallMeta" style={styles.rolePrompt}>
+            {formatMessage(m.joinAsRole, {role: translatedRole})}
+          </BodyText>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          {acceptInvite.status === 'pending' ||
+          rejectInvite.status === 'pending' ? (
+            <UIActivityIndicator style={{marginVertical: 20}} />
+          ) : (
+            <>
+              <SecondaryButton
+                fullSize
+                onPress={reject}
+                text={formatMessage(m.declineInvite)}
+              />
+              <PrimaryButton
+                fullSize
+                onPress={accept}
+                text={formatMessage(m.joinProject)}
+              />
+            </>
+          )}
+        </View>
       </View>
     </BottomSheetWrapper>
   );
 };
 
 const styles = StyleSheet.create({
-  inviteIcon: {
-    borderColor: LIGHT_GREY,
+  container: {
+    gap: 12,
+  },
+  invitedLabel: {
+    textTransform: 'uppercase',
+    fontWeight: '500',
+    alignSelf: 'stretch',
+    color: BLACK,
+  },
+  cardContainer: {
+    backgroundColor: '#fff5eb',
+    borderColor: VERY_LIGHT_GREY,
     borderWidth: 1,
-    borderRadius: 100,
-    alignItems: 'center',
-    alignSelf: 'center',
-    width: 80,
+    borderRadius: 6,
+    padding: 20,
+    gap: 20,
+    alignSelf: 'stretch',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    elevation: 1,
+  },
+  projectName: {
+    lineHeight: 28,
+    color: BLACK,
+  },
+  rolePrompt: {
+    lineHeight: 14,
+    color: NEW_DARK_GREY,
   },
   buttonContainer: {
+    paddingTop: 18,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 20,
     alignItems: 'center',
-    marginTop: 30,
+    alignSelf: 'stretch',
   },
 });

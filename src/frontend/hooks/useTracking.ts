71004aecae524b4372d8cfed25ffd1574ec8ade2
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-04-30 12:08:48 -0700

    chore: update hook

--- src/frontend/hooks/useTracking.ts
+++ src/frontend/hooks/useTracking.ts
@@ -5,34 +5,44 @@ import {
   useTrackState,
 } from '../contexts/TrackStoreContext.tsx';
 import {LOCATION_TASK_NAME} from '../sharedTypes/location.ts';
+import {useNavigation} from '@react-navigation/native';
 
 export function useTracking() {
-  const [loading, setLoading] = useState(false);
-  const {setTracking} = useTrackActions();
+  const {setTracking, clearCurrentTrack} = useTrackActions();
+  const navigation = useNavigation();
   const isTracking = useTrackState(state => state.isTracking);
+  const locationHistory = useTrackState(state => state.locationHistory);
 
-  const startTracking = useCallback(async () => {
+  const startTracking = useCallback(() => {
     if (isTracking) {
       console.warn('Start tracking attempt while tracking already enabled');
-      setLoading(false);
       return;
     }
-
-    setLoading(true);
-
-    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+    setTracking(true);
+    Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
       accuracy: Location.Accuracy.Highest,
       activityType: Location.LocationActivityType.Fitness,
+    }).catch(error => {
+      setTracking(false);
+      // @ts-expect-error - this is typing issue, we are using the not strongly typed hook as this can technically be used in any screen. But regardless of the screen, we want to show the error bottom sheet
+      navigation.navigate('ErrorBottomSheet');
     });
-
-    setTracking(true);
-    setLoading(false);
   }, [isTracking, setTracking]);
-
-  const cancelTracking = useCallback(async () => {
-    await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+  /**
+   * Cancels location tracking and stops background updates.
+   * @returns {boolean} True if location history has more than one entry (track should be kept), false otherwise.
+   */
+  const cancelTrackingAndReturnIfTracksSaved = useCallback(() => {
     setTracking(false);
+    Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+    const hasTrackWithMoreThan1Point = locationHistory.length > 1;
+
+    if (!hasTrackWithMoreThan1Point) {
+      clearCurrentTrack();
+    }
+
+    return hasTrackWithMoreThan1Point;
   }, [setTracking]);
 
-  return {isTracking, startTracking, cancelTracking, loading};
+  return {isTracking, startTracking, cancelTrackingAndReturnIfTracksSaved};
 }

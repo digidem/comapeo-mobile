6b328ec96295f907cea177b848b68ee62c2ac1b2
Cindy Green <cimigreen@hotmail.com>
2025-05-07 11:01:35 -0400

    Adjusts test for edited device name.

--- tests/e2e/specs/main/side-drawer-menu-no-proj.test.ts
+++ tests/e2e/specs/main/side-drawer-menu-no-proj.test.ts
@@ -7,7 +7,7 @@ describe('Main - Side Drawer Menu - No Project', () => {
   it('should open the side drawer and verify menu options', async () => {
     const drawerIcon = await $('~Open Menu');
     await drawerIcon.click();
-    const deviceName = await $(byText(output.names.device));
+    const deviceName = await $(byText(output.names.editdevice));
     await expect(deviceName).toBeDisplayed();
 
     await expect($(byTextMatches('CURRENT PROJECT'))).toBeDisplayed();

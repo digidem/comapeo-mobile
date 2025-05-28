fd493c2a16077e43abe148f94186afeca6fb4fbd
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-05-28 10:35:41 -0700

    chore: update use Tracking hook to be declarative

--- src/frontend/hooks/useTracking.ts
+++ src/frontend/hooks/useTracking.ts
@@ -7,50 +7,61 @@ import {
 import {LOCATION_TASK_NAME} from '../sharedTypes/location.ts';
 import {useNavigation} from '@react-navigation/native';
 import * as Sentry from '@sentry/react-native';
+import {calculateTotalDistance} from '../utils/distance.ts';
 
-export function useTracking() {
-  const {setTracking, clearCurrentTrack} = useTrackActions();
-  const navigation = useNavigation();
-  const isTracking = useTrackState(state => state.isTracking);
+export function useStartStopTracks() {
+  const {addNewLocations} = useTrackActions();
   const locationHistory = useTrackState(state => state.locationHistory);
+  const navigation = useNavigation();
 
-  const startTracking = useCallback(() => {
-    if (isTracking) {
-      console.warn('Start tracking attempt while tracking already enabled');
-      return;
-    }
-    setTracking(true);
-    Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-      accuracy: Location.Accuracy.Highest,
-      activityType: Location.LocationActivityType.Fitness,
-    }).catch(err => {
-      Sentry.captureException(err);
-      setTracking(false);
-      // @ts-expect-error - this is a typing issue, we are using the non-strongly typed hook as this can technically be used in any screen. But regardless of the screen, we want to show the error bottom sheet.
-      navigation.navigate('ErrorBottomSheet');
-    });
-  }, [isTracking, setTracking, navigation]);
+  const startTracking = useCallback(
+    (initialLocation: Location.LocationObject) => {
+      //Manually insert current location as the first point in the track
+      addNewLocations([
+        {
+          latitude: initialLocation.coords.latitude,
+          longitude: initialLocation.coords.longitude,
+          timestamp: initialLocation.timestamp,
+        },
+      ]);
+
+      Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+        accuracy: Location.Accuracy.Highest,
+        activityType: Location.LocationActivityType.Fitness,
+      }).catch(err => {
+        Sentry.captureException(err);
+        // @ts-expect-error - this is a typing issue, we are using the non-strongly typed hook as this can technically be used in any screen. But regardless of the screen, we want to show the error bottom sheet.
+        navigation.navigate('ErrorBottomSheet');
+      });
+    },
+    [navigation, addNewLocations],
+  );
   /**
    * Cancels location tracking and stops background updates.
-   * @returns {boolean} True if location history has more than one entry (track should be kept), false otherwise.
+   * @returns {number} returns the total distance recorded in meters.
    */
-  const cancelTracking = useCallback(() => {
-    setTracking(false);
+  const endTracking = useCallback(() => {
     Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME).catch(err => {
       Sentry.captureException(err);
     });
-    const hasTrackWithMoreThan1Point = locationHistory.length > 1;
 
-    if (!hasTrackWithMoreThan1Point) {
-      clearCurrentTrack();
-    }
+    const totalDistanceRecorded =
+      calculateTotalDistance(locationHistory) * 1000; // Convert to meters
 
-    return hasTrackWithMoreThan1Point;
-  }, [setTracking, clearCurrentTrack, locationHistory]);
+    return totalDistanceRecorded;
+  }, [locationHistory]);
 
   return {
-    isTracking,
+    endTracking,
     startTracking,
-    cancelTracking,
   };
 }
+
+export const useCurrentTrackState = () => {
+  const locationHistory = useTrackState(state => state.locationHistory);
+  return {
+    hasActiveTrack: locationHistory.length > 0,
+    totalDistance: calculateTotalDistance(locationHistory) * 1000, // Convert to meters
+    locationHistory,
+  };
+};

37afddf96f1ea2093d2b5cf203355322c51108ee
Evan Hahn <me@evanhahn.com>
2025-06-01 13:13:48 +0000

    test: add first sync screen assertion
    *I recommend reviewing this with whitespace changes disabled.*
    
    This adds a test assertion for the sync screen. The assertion itself is
    simple--all it does is check for text on screen--but some additional
    work was required.
    
    It turned out that the component wasn't actually being rendered! The
    [Safe Area Context library][0] wasn't including its children in tests,
    which meant that nothing was rendering. Fixing that uncovered several
    other problems, which I fixed:
    
    - I had to mock:
      - The Safe Area Context library, as mentioned, using [their built-in
        Jest mock][1]
      - `expo-location`
      - Native modules
      - Local discovery. I mocked our module rather than mapping lower-level
        things; for example, I could have mocked `NetInfo` and `AppState`
        and `Zeroconf`. I can probably find a way to change this but it
        feels tedious.
      - SVG imports, which otherwise import as `{ testUri: './path.svg' }`,
        which doesn't render and gives an error. I considered using [a
        library][2] to do this for me, but the mock was so simple that I
        just did it myself.
    - I had to fix two resource leaks:
      - Device diagnostic metrics needed to be disabled, matching app
        diagnostic metrics
      - Tanstack Query needed to have garbage collection disabled. See the
        comment in `helpers/react.tsx`
    
    This change should have no user impact.
    
    [0]: https://appandflow.github.io/react-native-safe-area-context/
    [1]: https://appandflow.github.io/react-native-safe-area-context/testing/
    [2]: https://www.npmjs.com/package/react-native-svg-mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,37 @@
 import 'react-native-gesture-handler/jestSetup';
 import mockNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+import {NativeModules} from 'react-native';
 import {setUpTests} from 'react-native-reanimated';
 import {jest} from '@jest/globals';
 
 setUpTests();
 
+const mockExpoLocation = {
+  Accuracy: {
+    Lowest: 1,
+    Low: 2,
+    Balanced: 3,
+    High: 4,
+    Highest: 5,
+    BestForNavigation: 6,
+  },
+  getForegroundPermissionsAsync: jest.fn(() =>
+    Promise.resolve({
+      status: 'granted',
+      expires: 'never',
+      granted: true,
+      canAskAgain: true,
+    }),
+  ),
+  getLastKnownPositionAsync: jest.fn(() => Promise.resolve(null)),
+  watchPositionAsync: jest.fn(() => Promise.resolve({remove: jest.fn()})),
+};
+jest.mock('expo-location', () => mockExpoLocation);
+
 jest.mock('@react-native-community/netinfo', () => mockNetInfo);
+
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 
 // https://github.com/callstack/react-native-testing-library/issues/1712
 jest.mock('expo-font', () => {
@@ -17,3 +43,9 @@ jest.mock('expo-font', () => {
 
   return module;
 });
+
+// Native modules won't work in tests.
+NativeModules.FlagSecureModule = {
+  activate: () => {},
+  deactivate: () => {},
+};

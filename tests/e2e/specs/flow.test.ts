0ebafaef021c39a818483910bac4dd016e4baee7
Cindy Green <cimigreen@hotmail.com>
2025-06-02 16:32:06 -0400

    Updates placement of tracks starting to go along with new ux and multiple projects.

--- tests/e2e/specs/flow.test.ts
+++ tests/e2e/specs/flow.test.ts
@@ -9,8 +9,8 @@ describe('CoMapeo E2E Flow', function () {
   require('./main/side-drawer-menu-no-proj.test');
   require('./project/project-settings-no-proj.test');
   require('./observations/create-observation.test');
-  require('./tracks/start-track.test');
   require('./multiple-projects/create-and-switch.test');
+  require('./tracks/start-track.test');
   require('./project/invite-collaborators.test');
   require('./project/rename-project-from-drawer.test');
   // require('./project/unjoin-project.test');

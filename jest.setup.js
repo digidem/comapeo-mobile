d164b79225803ee98c03ecc170215eabc01ba85d
Evan Hahn <me@evanhahn.com>
2025-06-04 21:40:11 +0000

    Move expo-location mock to `__mocks__/`

--- jest.setup.js
+++ jest.setup.js
@@ -7,28 +7,6 @@ import {jest} from '@jest/globals';
 
 setUpTests();
 
-const mockExpoLocation = {
-  Accuracy: {
-    Lowest: 1,
-    Low: 2,
-    Balanced: 3,
-    High: 4,
-    Highest: 5,
-    BestForNavigation: 6,
-  },
-  getForegroundPermissionsAsync: jest.fn(() =>
-    Promise.resolve({
-      status: 'granted',
-      expires: 'never',
-      granted: true,
-      canAskAgain: true,
-    }),
-  ),
-  getLastKnownPositionAsync: jest.fn(() => Promise.resolve(null)),
-  watchPositionAsync: jest.fn(() => Promise.resolve({remove: jest.fn()})),
-};
-jest.mock('expo-location', () => mockExpoLocation);
-
 jest.mock('@react-native-community/netinfo', () => mockNetInfo);
 
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);

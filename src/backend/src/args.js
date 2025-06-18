d546a0fc09829256b3b2791b297b5278c613ee92
Mauve Signweaver <contact@mauve.moe>
2025-06-18 17:23:06 -0400

    core: unify backend arg parsing

---
+++ src/backend/src/args.js
@@ -0,0 +1,35 @@
+import { parseArgs } from 'util'
+
+export default function args() {
+  const { values } = parseArgs({
+    options: {
+      sentryEnvironment: { type: 'string' },
+      sentryUserId: { type: 'string' },
+      metricsIsEnabled: { type: 'boolean', default: false },
+      version: { type: 'string' },
+      rootKey: { type: 'string' },
+    },
+    strict: true,
+  })
+
+  const {
+    sentryEnvironment,
+    sentryUserId,
+    metricsIsEnabled,
+    version,
+    rootKey,
+  } = values
+
+  if (typeof metricsIsEnabled !== 'boolean')
+    throw new Error('backend did not receive metricsIsEnabled')
+  if (typeof sentryUserId !== 'string')
+    throw new Error('backend did not receive sentryUserId')
+  if (typeof sentryEnvironment !== 'string')
+    throw new Error('backend did not receive sentryEnvironment')
+  if (typeof values.rootKey !== 'string')
+    throw new Error('backend did not receive root key from front end')
+  if (typeof values.version === 'string')
+    throw new Error('backend got invalid version')
+
+  return { sentryEnvironment, sentryUserId, metricsIsEnabled, version, rootKey }
+}

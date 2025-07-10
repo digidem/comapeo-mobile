ca7cf3512b0cdc543aba2881ab04f073ceaa25ad
Cindy Green <cimigreen@hotmail.com>
2025-07-10 13:44:11 -0400

    Adds e2e test for lockout

--- tests/e2e/specs/passcode/check-passcode-requirements.test.ts
+++ tests/e2e/specs/passcode/check-passcode-requirements.test.ts
@@ -46,8 +46,31 @@ describe('Passcode - Check Passcode Requirements Flow', () => {
     await driver.activateApp('com.comapeo.rc');
 
     await expect($(byTextMatches('Enter your passcode'))).toBeDisplayed();
+  });
+  it('should show lockout message after 5 failed passcode attempts', async () => {
+    const passcodeField = await $(byResourceId('SETTINGS.auth-passcode-inp'));
 
+    for (let i = 0; i < 5; i++) {
+      await passcodeField.click();
+      await driver.keys('54321'.split(''));
+      await driver.hideKeyboard();
+      await driver.pause(500);
+    }
+
+    const lockoutMessage = await $(byTextMatches('Try again in 1 minute'));
+    await expect(lockoutMessage).toBeDisplayed();
+
+    const keyboardShown = await driver.isKeyboardShown();
+    expect(keyboardShown).toBe(false);
+  });
+  it('should allow login again with correct passcode after lockout time', async () => {
+    await driver.pause(61000);
+
+    const passcodeField = await $(byResourceId('SETTINGS.auth-passcode-inp'));
     await passcodeField.click();
     await driver.keys(output.passcode.split(''));
+    await driver.hideKeyboard();
+
+    await expect($(byTextMatches('Enter your passcode'))).not.toBeDisplayed();
   });
 });

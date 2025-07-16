e505eac1c83c11b44d6652a602c5e0bb591c3c39
Andrew Chou <andrewchou@fastmail.com>
2025-07-16 15:50:00 -0400

    feat: expose more photo metadata (#1203)
    * improve SavedPhoto type
    
    * WIP
    
    * wip: accounting for potentially updated schema
    
    * cleanup
    
    * remove unused exif helper function
    
    * update displayed storage size
    
    * update expo-image dep declaration
    
    * changes based on feedback
    
    * fix type errors
    
    * remove todo comment

--- src/frontend/lib/exif.ts
+++ src/frontend/lib/exif.ts
@@ -2,12 +2,14 @@ import * as v from 'valibot';
 
 import type {PhotoEXIF} from '../sharedTypes';
 
+export type PhotoLayout = 'horizontal' | 'vertical';
+
 export const PhotoEXIFSchema = v.object({
   ApertureValue: v.optional(v.number()),
   ExposureTime: v.optional(v.number()),
   Flash: v.optional(v.number()),
-  FocalLength: v.optional(v.number()),
   FNumber: v.optional(v.number()),
+  FocalLength: v.optional(v.number()),
   ISOSpeedRatings: v.optional(v.number()),
   ImageLength: v.optional(v.number()),
   ImageWidth: v.optional(v.number()),
@@ -18,3 +20,32 @@ export const PhotoEXIFSchema = v.object({
 } satisfies Required<{
   [field in keyof PhotoEXIF]: v.GenericSchema<PhotoEXIF[field]>;
 }>) satisfies v.GenericSchema<PhotoEXIF>;
+
+/**
+ * Describes the layout of the photo based on its EXIF orientation tag value.
+ *
+ * @param exifOrientation Value of EXIF orientation tag
+ * @returns The layout
+ */
+export function getPhotoLayout(exifOrientation: number): PhotoLayout {
+  // See "Orientation" tag in https://exiftool.org/TagNames/EXIF.html
+  // Helpful explainer: https://www.ameto.de/blog/exif-orientation-primer/
+
+  switch (exifOrientation) {
+    case 1:
+    case 2:
+    case 3:
+    case 4: {
+      return 'horizontal';
+    }
+    case 5:
+    case 6:
+    case 7:
+    case 8: {
+      return 'vertical';
+    }
+    default: {
+      throw new Error(`Invalid orientation value: ${exifOrientation}`);
+    }
+  }
+}

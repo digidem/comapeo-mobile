2784254d7bf79cec347097d090d8a7c37dbc00cc
Gregor MacLennan <gmaclennan@digital-democracy.org>
2025-07-30 17:50:29 +0100

    chore: turn on Sentry tracing for non-production environments (#1295)

--- src/frontend/AppNavigator.tsx
+++ src/frontend/AppNavigator.tsx
@@ -1,10 +1,26 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  type NavigationContainerRef,
+} from '@react-navigation/native';
 import * as React from 'react';
 import * as SplashScreen from 'expo-splash-screen';
+import {type AppStackParamsList} from './sharedTypes/navigation';
+
 import {useSetUpInvitesListeners} from '@comapeo/core-react';
 import {RootStackNavigator} from './Navigation/Stack';
+import type Sentry from '@sentry/react-native';
 
-export const AppNavigator = ({permissionAsked}: {permissionAsked: boolean}) => {
+export const AppNavigator = ({
+  permissionAsked,
+  navigationIntegration,
+}: {
+  permissionAsked: boolean;
+  navigationIntegration:
+    | ReturnType<(typeof Sentry)['reactNavigationIntegration']>
+    | undefined;
+}) => {
+  const containerRef =
+    React.useRef<NavigationContainerRef<AppStackParamsList>>(null);
   useSetUpInvitesListeners();
 
   if (permissionAsked) {
@@ -12,7 +28,11 @@ export const AppNavigator = ({permissionAsked}: {permissionAsked: boolean}) => {
   }
 
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={containerRef}
+      onReady={() => {
+        navigationIntegration?.registerNavigationContainer(containerRef);
+      }}>
       <React.Suspense fallback={null}>
         <RootStackNavigator />
       </React.Suspense>

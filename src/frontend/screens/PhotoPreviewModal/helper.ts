e0e67e4ba193ef9c06d2cd913b98918053342cbb
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-05 16:22:49 -0700

    chore: create helper function for photo time

--- src/frontend/screens/PhotoPreviewModal/helper.ts
+++ src/frontend/screens/PhotoPreviewModal/helper.ts
@@ -12,3 +12,14 @@ export function calculateDistanceFromObservation({
   const observationPoint = point(observationLocation);
   return distance(photoPoint, observationPoint);
 }
+
+export function calcPhotoTimeRelativeToObs({
+  photoCreatedAt,
+  observationCreatedAt,
+}: {
+  photoCreatedAt: number;
+  observationCreatedAt: number;
+}): number {
+  const diffInMilliseconds = Math.abs(observationCreatedAt - photoCreatedAt);
+  return diffInMilliseconds / 1000 / 60;
+}

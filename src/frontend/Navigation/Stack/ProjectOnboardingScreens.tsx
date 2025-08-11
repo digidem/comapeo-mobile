43b6adc0df91c14eb63172d0a8ddea7f91ccaac6
Cindy Green <cimigreen@hotmail.com>
2025-08-11 13:52:40 -0400

    Removes unneeded international for now.

--- src/frontend/Navigation/Stack/ProjectOnboardingScreens.tsx
+++ src/frontend/Navigation/Stack/ProjectOnboardingScreens.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import {RootStack} from '.';
-import {MessageDescriptor} from 'react-intl';
 import {ProjectsIntro} from '../../screens/ProjectOnboarding/ProjectsIntro';
 
-export const createProjectOnboardingScreens = ({
-  intl,
-}: {
-  intl: (title: MessageDescriptor) => string;
-}) => (
+export const createProjectOnboardingScreens = () => (
   <RootStack.Group key="project-onboarding">
     <RootStack.Screen
       name="ProjectsIntro"

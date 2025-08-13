de2d5a21b2c87e735c7728e9b85ae6f4c49e79f5
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-13 15:06:06 -0700

    chore: select correct button

--- tests/e2e/specs/project/project-usage-stats.test.ts
+++ tests/e2e/specs/project/project-usage-stats.test.ts
@@ -5,7 +5,7 @@ import {byText} from '../../utils/selectors';
 describe('Project - Show Project Stats page After Project Rename or Creation', () => {
   it('should take user to Share Project Statistics screen', async () => {
     await expect($(byText('Share Project Statistics'))).toBeDisplayed();
-    const skipButton = await $(byText('No, Skip for Now'));
+    const skipButton = await $(byText('No, Skip for now'));
     await skipButton.click();
   });
 });

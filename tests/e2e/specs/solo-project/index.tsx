19d0adf8e48824438687f8f43512c35555b2b63c
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-20 14:19:55 -0700

    chore: side menu solo project

--- tests/e2e/specs/solo-project/index.tsx
+++ tests/e2e/specs/solo-project/index.tsx
@@ -3,6 +3,7 @@ import {describe} from 'mocha';
 describe('CoMapeo E2E Flow', function () {
   require('../onboarding/helper/minimal-onboarding-setup.test');
   require('./own-project-headers.test');
+  require('./side-drawer-menu-no-proj.test');
   require('./project-settings-no-proj.test');
   require('./rename-project-from-drawer.test');
   require('./project-settings-proj.test');

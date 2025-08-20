40c3e60806ab6a4e1445d6e5710efa18d81efab4
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-20 14:06:07 -0700

    chore: tracks setup

--- tests/e2e/specs/tracks/index.test.ts
+++ tests/e2e/specs/tracks/index.test.ts
@@ -1,7 +1,7 @@
 import {describe} from 'mocha';
 
 describe('CoMapeo E2E Flow', function () {
-  //   require('../onboarding/helper/minimal-onboarding-setup.test');
+  require('../onboarding/helper/minimal-onboarding-setup.test');
   require('./start-track.test');
   require('./no-movement-track.test');
   // require('./view-edit-track.test');

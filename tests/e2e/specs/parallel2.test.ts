078ae9deff86638d752f51f6d6c6c72ac52b0e56
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-20 11:38:01 -0700

    chore: remove fake parallel tests

--- tests/e2e/specs/parallel2.test.ts
+++
@@ -1,8 +0,0 @@
-import {describe} from 'mocha';
-
-describe('CoMapeo E2E Flow', function () {
-  require('./onboarding/data-privacy.test');
-  require('./onboarding/privacy-policy.test');
-  require('./onboarding/device-naming.test');
-  require('./main/side-drawer-menu-no-proj.test');
-});

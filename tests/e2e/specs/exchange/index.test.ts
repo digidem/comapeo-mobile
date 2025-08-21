0fe3e16b02e653009bfb6e13b02bc3d943b78f78
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-20 17:03:08 -0700

    chore: update import

--- tests/e2e/specs/exchange/index.test.ts
+++ tests/e2e/specs/exchange/index.test.ts
@@ -3,7 +3,7 @@ import {describe} from 'mocha';
 describe('CoMapeo E2E Flow', function () {
   require('../onboarding/helper/minimal-onboarding-setup.test');
   require('./solo.test');
-  require('../project/helper/minimal-project-creation.test');
+  require('../solo-project/helper/minimal-project-creation.test');
   require('./no-devices.test');
   require('./everything.test');
   require('./previews.test');

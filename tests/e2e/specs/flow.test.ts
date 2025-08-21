8d3d40114daa4cc20c3484145e31d673064536fb
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-21 13:04:28 -0700

    chore: uncomment tests

--- tests/e2e/specs/flow.test.ts
+++ tests/e2e/specs/flow.test.ts
@@ -1,13 +1,13 @@
 import {describe} from 'mocha';
 
 describe('CoMapeo E2E Flow', function () {
-  // require('./onboarding/data-privacy.test');
-  // require('./onboarding/privacy-policy.test');
-  // require('./onboarding/device-naming.test');
-  // require('./project/edit-device-name.test');
-  // require('./project/own-project-headers.test');
-  // require('./main/side-drawer-menu-no-proj.test');
-  // require('./project/project-settings-no-proj.test');
+  require('./onboarding/data-privacy.test');
+  require('./onboarding/privacy-policy.test');
+  require('./onboarding/device-naming.test');
+  require('./project/edit-device-name.test');
+  require('./project/own-project-headers.test');
+  require('./main/side-drawer-menu-no-proj.test');
+  require('./project/project-settings-no-proj.test');
   require('./exchange/solo.test');
   require('./observations/export-observations.test');
   require('./observations/create-observation.test');

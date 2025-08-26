2c8aa5dfd0122b1c28ef8d3e3df319e1d49c6544
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-08-26 16:49:15 -0700

    chore: update archive tests

--- tests/e2e/specs/remote-archive/index.test.ts
+++ tests/e2e/specs/remote-archive/index.test.ts
@@ -3,6 +3,6 @@ import {describe} from 'mocha';
 describe('CoMapeo E2E Flow', function () {
   require('../onboarding/helper/minimal-onboarding-setup.test');
   require('../solo-project/helper/minimal-project-creation.test');
-  require('./create-observation.test');
-  require('./add-details.test');
+  require('./add-remote-error.test');
+  require('./add-remote-success-and-removal.test');
 });

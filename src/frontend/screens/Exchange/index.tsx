5d0caa42c899722fa4c3cecd5a59d705af8d8990
Cindy Green <cimigreen@hotmail.com>
2025-09-17 17:13:56 -0400

    Removes changes from exchange screen.

--- src/frontend/screens/Exchange/index.tsx
+++ src/frontend/screens/Exchange/index.tsx
@@ -1,57 +1,64 @@
 import * as React from 'react';
-import {View} from 'react-native';
-import {useNetInfo} from '@react-native-community/netinfo';
 import {useSyncState} from '@comapeo/core-react';
+
 import {NativeRootNavigationProps} from '../../sharedTypes/navigation';
-
-import {useActiveProject} from '../../contexts/ActiveProjectContext';
-import {useLocalDiscoveryState} from '../../hooks/useLocalDiscoveryState';
 import {
-  useProjectSettings,
   useActiveArchiveServer,
+  useProjectSettings,
 } from '../../hooks/server/projects';
-
+import {useLocalDiscoveryState} from '../../hooks/useLocalDiscoveryState';
 import {ExchangeSoloScreen} from './ExchangeSoloScreen';
 import {NoWifiDisplay} from './NoWifiDisplay';
 import {ExchangeScreenContent} from './ExchangeScreenContent';
+import {Loading} from '../../sharedComponents/Loading';
+import {useNetInfo} from '@react-native-community/netinfo';
+import {useActiveProject} from '../../contexts/ActiveProjectContext';
 import {defineMessages} from 'react-intl';
 
 const m = defineMessages({
-  exchangeTitle: {id: 'screens.Sync.headerTitle', defaultMessage: 'Exchange'},
+  exchangeTitle: {
+    id: 'screens.Sync.headerTitle',
+    defaultMessage: 'Exchange',
+  },
 });
 
-const LoaderCard = () => (
-  <View
-    style={{height: 120, margin: 20, borderRadius: 12, backgroundColor: '#eee'}}
-  />
-);
-
 export const SyncScreen = ({navigation}: NativeRootNavigationProps<'Sync'>) => {
-  const projectSettings = useProjectSettings();
-  if (projectSettings.data?.name === undefined)
-    return <ExchangeSoloScreen onGoBack={() => navigation.goBack()} />;
-  return (
-    <React.Suspense fallback={<LoaderCard />}>
-      <ExchangeWifiSwitch onGoBack={() => navigation.goBack()} />
-    </React.Suspense>
-  );
-};
-SyncScreen.navTitle = m.exchangeTitle;
+  const wifiStatus = useLocalDiscoveryState(state => state.wifiStatus);
 
-function ExchangeWifiSwitch({onGoBack}: {onGoBack: () => void}) {
+  const hasInternetAccess = useNetInfo().isConnected;
+
   const {projectId} = useActiveProject();
-  const wifiStatus = useLocalDiscoveryState(s => s.wifiStatus);
-  const hasInternetAccess = !!useNetInfo().isConnected;
+  const syncState = useSyncState({projectId});
+  const projectSettingsQuery = useProjectSettings();
+
   const hasRemoteArchive = !!useActiveArchiveServer({projectId});
 
-  const syncState = useSyncState({projectId});
+  if (!syncState) {
+    return <Loading />;
+  }
 
-  const noWifi =
-    (!hasRemoteArchive && wifiStatus === 'off') ||
-    (hasRemoteArchive && !hasInternetAccess);
+  if (projectSettingsQuery.data?.name === undefined) {
+    return <ExchangeSoloScreen onGoBack={() => navigation.goBack()} />;
+  }
 
-  if (noWifi) return <NoWifiDisplay onGoBack={onGoBack} />;
-  if (!syncState) return <LoaderCard />;
+  const shouldShowNoWifiDisplay = () => {
+    // Case 1: No remote archive, sync is only possible on WiFi
+    if (!hasRemoteArchive && wifiStatus === 'off') {
+      return true;
+    }
+    // Case 2: Remote archive exists, but no general internet connection.
+    // If a user has a remote archive they can sync on NON-wifi internet connections
+    if (hasRemoteArchive && !hasInternetAccess) {
+      return true;
+    }
+    return false;
+  };
+
+  if (shouldShowNoWifiDisplay()) {
+    return <NoWifiDisplay onGoBack={() => navigation.goBack()} />;
+  }
 
   return <ExchangeScreenContent syncState={syncState} />;
-}
+};
+
+SyncScreen.navTitle = m.exchangeTitle;

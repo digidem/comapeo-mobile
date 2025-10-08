89c7f6f750d0ef217c3e89d0f2a8efc564e7ea67
Cindy Green <cimigreen@hotmail.com>
2025-10-08 12:12:12 -0400

    Updates tests as needed to use projects for new nav setup

--- src/frontend/contexts/ActiveProjectIdStoreContext.test.tsx
+++ src/frontend/contexts/ActiveProjectIdStoreContext.test.tsx
@@ -1,5 +1,7 @@
 import {act, renderHook} from '@testing-library/react-native';
 import {type ReactNode} from 'react';
+import type {MapeoManager} from '@comapeo/core';
+import type {MapeoClientApi} from '@comapeo/ipc';
 
 import {
   ActiveProjectIdStoreProvider,
@@ -8,33 +10,79 @@ import {
   useActiveProjectId,
   type ActiveProjectIdStore,
 } from './ActiveProjectIdStoreContext';
+import {createManager, setUpIPC} from '../../../tests/integration/helpers/core';
+import {createAppProvidersWrapper} from '../../../tests/integration/helpers/react';
 
-function createWrapper(store: ActiveProjectIdStore) {
+function createWrapper(
+  store: ActiveProjectIdStore,
+  appProviders: ReturnType<typeof createAppProvidersWrapper>,
+) {
   return ({children}: {children: ReactNode}) => {
+    const AppProviders = appProviders.wrapper;
     return (
-      <ActiveProjectIdStoreProvider store={store}>
-        {children}
-      </ActiveProjectIdStoreProvider>
+      <AppProviders>
+        <ActiveProjectIdStoreProvider store={store}>
+          {children}
+        </ActiveProjectIdStoreProvider>
+      </AppProviders>
     );
   };
 }
 
-test('usage of state and actions hooks', () => {
-  const activeProjectStore = createActiveProjectIdStore();
-  const wrapper = createWrapper(activeProjectStore);
+describe('ActiveProjectIdStore', () => {
+  let manager: MapeoManager;
+  let client: MapeoClientApi;
+  let onTeardown: Array<() => unknown> = [];
 
-  const actionsHook = renderHook(() => useActiveProjectIdActions(), {
-    wrapper,
+  beforeEach(async () => {
+    onTeardown = [];
+
+    const managerSetup = await createManager({
+      name: 'test',
+      deviceType: 'mobile',
+    });
+    ({manager} = managerSetup);
+    const {fastifyController} = managerSetup;
+
+    const ipcSetup = setUpIPC({manager});
+    ({client} = ipcSetup);
+    const {stop} = ipcSetup;
+    onTeardown.push(stop);
+
+    await fastifyController.start();
+    onTeardown.push(() => fastifyController.stop());
   });
-  const stateHook = renderHook(() => useActiveProjectId(), {
-    wrapper,
+
+  afterEach(async () => {
+    for (const fn of onTeardown) await fn();
   });
 
-  expect(stateHook.result.current).toStrictEqual(undefined);
+  test('usage of state and actions hooks', async () => {
+    const activeProjectStore = createActiveProjectIdStore();
+    const appProviders = createAppProvidersWrapper({
+      mapeoApi: client,
+    });
+    onTeardown.push(appProviders.teardown);
 
-  act(() => {
-    actionsHook.result.current.setActiveProjectId('project_1');
+    const wrapper = createWrapper(activeProjectStore, appProviders);
+
+    const actionsHook = renderHook(() => useActiveProjectIdActions(), {
+      wrapper,
+    });
+    const stateHook = renderHook(() => useActiveProjectId(), {
+      wrapper,
+    });
+
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 0));
+    });
+
+    expect(stateHook.result.current).toStrictEqual(undefined);
+
+    act(() => {
+      actionsHook.result.current.setActiveProjectId('project_1');
+    });
+
+    expect(stateHook.result.current).toStrictEqual('project_1');
   });
-
-  expect(stateHook.result.current).toStrictEqual('project_1');
 });

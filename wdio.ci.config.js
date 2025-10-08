4f7ad953e47d18114ec7badcdfbc4ef86753bc21
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-10-08 14:43:59 -0700

    chore: note

--- wdio.ci.config.js
+++ wdio.ci.config.js
@@ -20,7 +20,7 @@ const config = {
     path.resolve(__dirname, 'tests/e2e/specs/settings/index.test.ts'),
     path.resolve(__dirname, 'tests/e2e/specs/solo-project/index.tsx'),
     path.resolve(__dirname, 'tests/e2e/specs/tracks/index.test.ts'),
-    //temp removing as remote archive seems to be down
+    //temporarily removing as remote archive seems to be down
     //path.resolve(__dirname, 'tests/e2e/specs/remote-archive/index.test.ts'),
     // path.resolve(__dirname, 'tests/e2e/specs/photos/index.test.ts'),
   ],

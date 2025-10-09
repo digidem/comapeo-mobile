270be87a5cc2c3ec80cd2726a70402b93729f708
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-10-09 10:54:36 -0700

    chore: scaffold test

--- src/frontend/contexts/ActiveProjectIdStoreContext.test.tsx
+++ src/frontend/contexts/ActiveProjectIdStoreContext.test.tsx
@@ -1,5 +1,5 @@
 import {act, renderHook, waitFor} from '@testing-library/react-native';
-import {type ReactNode} from 'react';
+import React, {type ReactNode} from 'react';
 import type {MapeoManager} from '@comapeo/core';
 import type {MapeoClientApi} from '@comapeo/ipc';
 
@@ -11,20 +11,16 @@ import {
   type ActiveProjectIdStore,
 } from './ActiveProjectIdStoreContext';
 import {createManager, setUpIPC} from '../../../tests/integration/helpers/core';
-import {createAppProvidersWrapper} from '../../../tests/integration/helpers/react';
+import {MapeoApiWrapper} from '../../../tests/integration/helpers/MapeoApiWrapper';
 
-function createWrapper(
-  store: ActiveProjectIdStore,
-  appProviders: ReturnType<typeof createAppProvidersWrapper>,
-) {
+function createWrapper(store: ActiveProjectIdStore, client: MapeoClientApi) {
   return ({children}: {children: ReactNode}) => {
-    const AppProviders = appProviders.wrapper;
     return (
-      <AppProviders>
+      <MapeoApiWrapper mapeoApi={client}>
         <ActiveProjectIdStoreProvider store={store}>
           {children}
         </ActiveProjectIdStoreProvider>
-      </AppProviders>
+      </MapeoApiWrapper>
     );
   };
 }
@@ -61,13 +57,8 @@ describe('ActiveProjectIdStore', () => {
     const projectId = await client.createProject({name: 'test project'});
 
     const activeProjectStore = createActiveProjectIdStore();
-    const appProviders = createAppProvidersWrapper({
-      mapeoApi: client,
-      activeProjectId: projectId,
-    });
-    onTeardown.push(appProviders.teardown);
 
-    const wrapper = createWrapper(activeProjectStore, appProviders);
+    const wrapper = createWrapper(activeProjectStore, client);
 
     const actionsHook = renderHook(() => useActiveProjectIdActions(), {
       wrapper,

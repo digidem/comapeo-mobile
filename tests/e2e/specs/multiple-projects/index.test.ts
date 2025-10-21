247e5d6d1c1663fa6d06adefac586b74cafb2517
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-10-21 16:40:35 -0700

    chore: update back in test

--- tests/e2e/specs/multiple-projects/index.test.ts
+++ tests/e2e/specs/multiple-projects/index.test.ts
@@ -1,8 +1,8 @@
 import {describe} from 'mocha';
 
 describe('multi-project', function () {
-  require('../onboarding/helper/minimal-onboarding-setup.test');
-  require('./create-and-switch.test');
+  // require('../onboarding/helper/minimal-onboarding-setup.test');
+  // require('./create-and-switch.test');
   require('./project-retention.test');
   require('./all-projects-screen.test');
   // Access to this edit project is slightly changing, so lets update this test accordingly

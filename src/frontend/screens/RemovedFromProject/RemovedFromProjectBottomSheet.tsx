2d43bd30b80435629409c1dbcfd314e2ecec695e
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-11-05 17:36:45 -0800

    chore: set new active project id in bottom sheet

--- src/frontend/screens/RemovedFromProject/RemovedFromProjectBottomSheet.tsx
+++ src/frontend/screens/RemovedFromProject/RemovedFromProjectBottomSheet.tsx
@@ -5,8 +5,15 @@ import {defineMessages, useIntl} from 'react-intl';
 import {NativeRootNavigationProps} from '../../sharedTypes/navigation';
 import {HeaderText} from '../../sharedComponents/Text/HeaderText';
 import {VERY_LIGHT_GREY, BLACK} from '../../lib/styles';
-import {useOwnRoleInProject, useProjectSettings} from '@comapeo/core-react';
+import {
+  useCreateProject,
+  useManyProjects,
+  useOwnRoleInProject,
+  useProjectSettings,
+} from '@comapeo/core-react';
 import {useActiveProjectIdActions} from '../../contexts/ActiveProjectIdStoreContext';
+import {useEffect, useState} from 'react';
+import {Loading} from '../../sharedComponents/Loading';
 
 const m = defineMessages({
   close: {
@@ -35,38 +42,64 @@ export const RemovedFromProjectBottomSheet = ({
   const {
     data: {name, projectColor},
   } = useProjectSettings({projectId});
+  const {data: projects} = useManyProjects();
+  const defaultProject = projects.find(proj => !proj.name);
+  const [defaultProjectId, setdefaultProjectId] = useState<string | null>(
+    defaultProject ? defaultProject.projectId : null,
+  );
   const {setActiveProjectId} = useActiveProjectIdActions();
+  const createProject = useCreateProject();
+
+  // The user should ALWAYS have a default (solo) project. This was not implemented until after v6. So this creates one if it does not exist
+  useEffect(() => {
+    if (defaultProjectId) return;
+    createProject.mutate(undefined, {
+      onError: () => {
+        navigation.navigate('ErrorBottomSheet');
+      },
+      onSuccess: projectId => {
+        setdefaultProjectId(projectId);
+      },
+    });
+  }, [createProject, setdefaultProjectId, navigation, defaultProjectId]);
 
   return (
     <BottomSheetWrapper>
-      <View style={styles.container}>
-        <HeaderText variant="header6" style={styles.titleText}>
-          {formatMessage(m.title)}
-        </HeaderText>
-
-        <View
-          style={[
-            styles.projectCard,
-            {backgroundColor: projectColor || '#EEF6EE'},
-          ]}>
-          <HeaderText variant="header2" style={styles.projectName}>
-            {name}
+      {!defaultProjectId ? (
+        <Loading />
+      ) : (
+        <View style={styles.container}>
+          <HeaderText variant="header6" style={styles.titleText}>
+            {formatMessage(m.title)}
           </HeaderText>
-          {reason && (
-            <HeaderText variant="header5" style={styles.reasonText}>
-              {formatMessage(m.reasonLabel, {reason})}
-            </HeaderText>
-          )}
-        </View>
 
-        <View style={styles.buttonContainer}>
-          <SecondaryButton
-            fullSize
-            onPress={() => {}}
-            text={formatMessage(m.close)}
-          />
+          <View
+            style={[
+              styles.projectCard,
+              {backgroundColor: projectColor || '#EEF6EE'},
+            ]}>
+            <HeaderText variant="header2" style={styles.projectName}>
+              {name}
+            </HeaderText>
+            {reason && (
+              <HeaderText variant="header5" style={styles.reasonText}>
+                {formatMessage(m.reasonLabel, {reason})}
+              </HeaderText>
+            )}
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <SecondaryButton
+              fullSize
+              onPress={() => {
+                setActiveProjectId(defaultProjectId);
+                navigation.popToTop();
+              }}
+              text={formatMessage(m.close)}
+            />
+          </View>
         </View>
-      </View>
+      )}
     </BottomSheetWrapper>
   );
 };

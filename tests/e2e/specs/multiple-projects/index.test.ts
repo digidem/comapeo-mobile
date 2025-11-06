4d3eaf8c49e26c4407177a60686de451e41161a4
Cindy Green <cimigreen@hotmail.com>
2025-11-06 17:59:57 +0100

    Updates documentation for some tests.

--- tests/e2e/specs/multiple-projects/index.test.ts
+++ tests/e2e/specs/multiple-projects/index.test.ts
@@ -5,6 +5,5 @@ describe('multi-project', function () {
   require('./create-and-switch.test');
   require('./project-retention.test');
   require('./all-projects-screen.test');
-  // Access to this edit project is slightly changing, so lets update this test accordingly
-  //require('./edit-project-details.test');
+  require('./edit-project-details.test');
 });

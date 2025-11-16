4c8cbcf806142c29d300422f08361dfb562b6608
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-11-16 15:26:46 -0800

    Revert "fix: use better-sqlite3 build with 16kb page-size support (#1517)"
    This reverts commit f73e58c8906dba7e904b978f2b758e7a916d166b.

--- scripts/download-prebuilds.mjs
+++ scripts/download-prebuilds.mjs
@@ -56,6 +56,12 @@ export async function downloadPrebuilds(modules, {verbose} = {verbose: false}) {
 
           fs.unlinkSync(path.join(targetDir, artifactInfo.name));
 
+          // better-sqlite3 includes an additional native module for testing purposes
+          // removing since it's not needed and also causes issues with nodejs-mobile-react-native
+          if (name === 'better-sqlite3') {
+            fs.unlinkSync(path.join(targetDir, 'test_extension.node'));
+          }
+
           if (verbose) {
             console.log(`${name}: prebuild done (${target})`);
           }
@@ -98,10 +104,7 @@ function getArtifactInfo({name, version, target, nodeAbi}) {
   const assetName = nodeAbi
     ? `${name}-${version}-node-${nodeAbi}-${target}.tar.gz`
     : `${name}-${version}-${target}.tar.gz`;
-  if (name === 'better-sqlite3') {
-    // We currently use this release suffix for better-sqlite3 prebuilds
-    version += '-bare-make';
-  }
+
   return {
     name,
     url: `https://github.com/digidem/${name}-nodejs-mobile/releases/download/${version}/${assetName}`,

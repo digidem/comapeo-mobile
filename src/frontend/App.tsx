e90edf920d8962eaac8912cb2abba99df5812eeb
ErikSin <67773827+ErikSin@users.noreply.github.com>
2025-11-25 08:26:54 -0800

    chore: update prod deps (#1559)
    * chore: update patch package
    
    * chore:update minor version of vector icons
    
    * chore: update nvm rc for vector icons
    
    * chore: update bottoms tabs
    
    * chore: update drawer layour
    
    * chore: minor stack update
    
    * chore: minor patches
    
    * chore: update map-obj
    
    * chore: update p-timeout
    
    * chore: upgrade react-native-device-info
    
    * chore: upgrade react-native-url-polyfill
    
    * chore: minor updates and document picker
    
    * chore: upgrade sentry
    
    * chore: pin ci node versions to nvmrc
    
    * chore: update EAS node version
    
    * chore: add comment to nvmrc
    
    * chore: move comment line?
    
    * chore: jk comments in nvmrc break things
    
    * chore: fix node versioning
    
    * chore: disable sentry metrics

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -69,6 +69,7 @@ Sentry.init({
   environment: sentryEnvironment,
   debug: sentryDebug, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
   initialScope: {user: {id: sentryUserId}},
+  enableMetrics: false,
 });
 
 if (appMetricsOptIn) {

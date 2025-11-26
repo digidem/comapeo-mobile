489e62f6a83943bd02b927902cdce3ca875b0b32
cimigree <cimigreen@hotmail.com>
2025-11-26 23:18:59 +0100

    fix: invite available during onboarding (#1515)
    * Adds invite screens to onboarding.
    
    * Creates a default project for someone accepting an invite during onboarding that is when there is no active project
    
    * Adds group for bottom sheets in onboarding. Uses different way to check for default project when accepting an invite.

--- src/frontend/Navigation/Stack/OnboardingScreens.tsx
+++ src/frontend/Navigation/Stack/OnboardingScreens.tsx
@@ -9,57 +9,67 @@ import {JoinProjectIntro} from '../../screens/Onboarding/JoinProjectIntro';
 import {MapOnYourOwnIntro} from '../../screens/Onboarding/MapOnYourOwnIntro';
 import {MessageDescriptor} from 'react-intl';
 import {ErrorBottomSheet} from '../../sharedComponents/ErrorBottomSheet';
+import {InviteReceived} from '../../screens/Invites/InviteReceived';
+import {InviteSuccessfullyAccepted} from '../../screens/Invites/InviteSuccessfullyAccepted';
+import {InviteCanceled} from '../../screens/Invites/InviteCanceled';
 
 export const createOnboardingScreens = ({
   intl,
 }: {
   intl: (title: MessageDescriptor) => string;
 }) => (
-  <RootStack.Group key="onboarding">
-    <RootStack.Screen
-      name="IntroToCoMapeo"
-      component={IntroToCoMapeo}
-      options={{headerShown: false}}
-    />
-    <RootStack.Screen
-      name="DataPrivacy"
-      component={DataPrivacy}
-      options={{headerShown: false}}
-    />
-    <RootStack.Screen
-      name="OnboardingPrivacyPolicy"
-      component={OnboardingPrivacyPolicy}
-      options={{headerTitle: intl(OnboardingPrivacyPolicy.navTitle)}}
-    />
-    <RootStack.Screen
-      name="DeviceNaming"
-      component={DeviceNaming}
-      options={{headerShown: false}}
-    />
-    <RootStack.Screen
-      name="Success"
-      component={Success}
-      options={{headerShown: false}}
-    />
-    <RootStack.Screen
-      name="JoinProjectIntro"
-      component={JoinProjectIntro}
-      options={{headerShown: false}}
-    />
-    <RootStack.Screen
-      name="MapOnYourOwnIntro"
-      component={MapOnYourOwnIntro}
-      options={{headerShown: false}}
-    />
-    <RootStack.Screen
-      name="ErrorBottomSheet"
-      options={{
+  <>
+    <RootStack.Group key="onboarding">
+      <RootStack.Screen
+        name="IntroToCoMapeo"
+        component={IntroToCoMapeo}
+        options={{headerShown: false}}
+      />
+      <RootStack.Screen
+        name="DataPrivacy"
+        component={DataPrivacy}
+        options={{headerShown: false}}
+      />
+      <RootStack.Screen
+        name="OnboardingPrivacyPolicy"
+        component={OnboardingPrivacyPolicy}
+        options={{headerTitle: intl(OnboardingPrivacyPolicy.navTitle)}}
+      />
+      <RootStack.Screen
+        name="DeviceNaming"
+        component={DeviceNaming}
+        options={{headerShown: false}}
+      />
+      <RootStack.Screen
+        name="Success"
+        component={Success}
+        options={{headerShown: false}}
+      />
+      <RootStack.Screen
+        name="JoinProjectIntro"
+        component={JoinProjectIntro}
+        options={{headerShown: false}}
+      />
+      <RootStack.Screen
+        name="MapOnYourOwnIntro"
+        component={MapOnYourOwnIntro}
+        options={{headerShown: false}}
+      />
+    </RootStack.Group>
+    <RootStack.Group
+      screenOptions={{
         presentation: 'transparentModal',
         headerShown: false,
         animation: 'none',
         contentStyle: {backgroundColor: 'transparent'},
-      }}
-      component={ErrorBottomSheet}
-    />
-  </RootStack.Group>
+      }}>
+      <RootStack.Screen name="ErrorBottomSheet" component={ErrorBottomSheet} />
+      <RootStack.Screen name="InviteReceived" component={InviteReceived} />
+      <RootStack.Screen
+        name="InviteSuccessfullyAccepted"
+        component={InviteSuccessfullyAccepted}
+      />
+      <RootStack.Screen name="InviteCanceled" component={InviteCanceled} />
+    </RootStack.Group>
+  </>
 );

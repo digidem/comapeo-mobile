fa524cbfef60e74a505d552deab9b58ba5312c44
Cindy Green <cimigreen@hotmail.com>
2025-12-11 20:03:21 +0100

    Adds a bottom sheet for when discarding a track.

--- src/frontend/Navigation/Stack/AppScreens.tsx
+++ src/frontend/Navigation/Stack/AppScreens.tsx
@@ -134,6 +134,7 @@ import {LeaveProject} from '../../screens/YourTeam/LeaveProject.tsx';
 import {LeaveProjectWarning} from '../../screens/YourTeam/LeaveProjectWarning.tsx';
 import {LeftProjectConfirmation} from '../../screens/YourTeam/LeftProjectConfirmation.tsx';
 import {ConfirmDiscardBottomSheet} from '../../screens/TrackEdit/ConfirmDiscardBottomSheet.tsx';
+import {ConfirmDiscardTrackBottomSheet} from '../../screens/SaveTrack/ConfirmDiscardTrackBottomSheet.tsx';
 
 export const TAB_BAR_HEIGHT = 70;
 
@@ -605,6 +606,10 @@ export const createAppScreens = ({
       <RootStack.Screen
         name="ConfirmTrackDiscardBottomSheet"
         component={ConfirmDiscardBottomSheet}
+      />
+      <RootStack.Screen
+        name="ConfirmDiscardTrackBottomSheet"
+        component={ConfirmDiscardTrackBottomSheet}
       />
     </RootStack.Group>
   </>
--- src/frontend/sharedTypes/navigation.ts
+++ src/frontend/sharedTypes/navigation.ts
@@ -184,6 +184,7 @@ export type RootStackParamsList = {
     memberType: 'coordinator' | 'participant';
   };
   ConfirmTrackDiscardBottomSheet: {trackId: string};
+  ConfirmDiscardTrackBottomSheet: undefined;
 };
 
 export type OnboardingParamsList = {

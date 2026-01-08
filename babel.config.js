df1d2fb6d150184d1e2fff27dcb17c6eab0a37a9
Cindy Green <cimigreen@hotmail.com>
2026-01-08 12:19:46 -0500

    Incremental adoption of compiler

--- babel.config.js
+++ babel.config.js
@@ -18,9 +18,22 @@ module.exports = function (api) {
   return {
     //https://github.com/babel/babel/discussions/13676#discussioncomment-1183149
     compact: true,
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          'react-compiler': {
+            // eslint-disable-next-line no-unused-vars
+            sources: filename => {
+              // Incremental adoption: Start with just one simple component
+              // For now, compile nothing until we verify it works
+              return false;
+            },
+          },
+        },
+      ],
+    ],
     plugins: [
-      ['babel-plugin-react-compiler'],
       ['transform-inline-environment-variables', {include: requiredEnvVars}],
       // react-native-reanimated/plugin has to be last
       'react-native-reanimated/plugin',

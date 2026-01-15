5f5d34a0df074d8bb70c6ae95dfcfc2d836e2582
Gregor MacLennan <gmaclennan@awana.digital>
2026-01-15 12:07:14 +0000

    throw error if no name defined for translated language

--- scripts/build-translations.mjs
+++ scripts/build-translations.mjs
@@ -4,6 +4,7 @@ import path from 'node:path';
 import fs from 'node:fs/promises';
 import {compile} from '@formatjs/cli-lib';
 import {fileURLToPath} from 'node:url';
+import LANGUAGE_NAME_TRANSLATIONS from '../src/frontend/languages.json' with {type: 'json'};
 
 const PROJECT_ROOT_DIR_PATH = fileURLToPath(new URL('../', import.meta.url));
 const TRANSLATIONS_DIR_PATH = path.join(PROJECT_ROOT_DIR_PATH, 'translations');
@@ -24,7 +25,16 @@ async function run() {
     const locale = folder.name;
     const inputPath = path.join(MESSAGES_DIR_PATH, locale);
     const outputPath = path.join(TRANSLATIONS_DIR_PATH, `${locale}.json`);
-    promises.push(compileFolder(inputPath, outputPath));
+    promises.push(
+      compileFolder(inputPath, outputPath).then(hasTranslations => {
+        if (hasTranslations && !LANGUAGE_NAME_TRANSLATIONS[locale]) {
+          throw new Error(`Locale '${locale}' has no language name defined in \`src/frontend/languages.json\`,
+so it will not appear as a language option in CoMapeo.
+Add the language name in English and the native language to \`languages.json\`
+in order to allow users to select '${locale}' in CoMapeo`);
+        }
+      }),
+    );
   }
 
   await Promise.all(promises);
@@ -32,6 +42,10 @@ async function run() {
   console.log(`Successfully built translations to ${TRANSLATIONS_DIR_PATH}`);
 }
 
+/**
+ * Compile all JSON files in a folder into a single translation file
+ * @returns {Promise<boolean>} whether any translations were compiled
+ */
 async function compileFolder(inputPath, outputPath) {
   const entries = await fs.readdir(inputPath, {withFileTypes: true});
   const messagesPaths = entries
@@ -41,5 +55,10 @@ async function compileFolder(inputPath, outputPath) {
     ast: true,
     format: 'crowdin',
   });
+  if (compiled.length < 5) {
+    // ignore empty translations
+    return false;
+  }
   await fs.writeFile(outputPath, compiled);
+  return true;
 }

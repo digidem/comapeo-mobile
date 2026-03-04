a2c90a42977f107b2cb3506cb4c0db3dacd69974
Cindy Green <cimigreen@hotmail.com>
2026-03-04 15:06:09 -0500

    Fixes screen name

--- src/frontend/sharedComponents/SelectDevice.tsx
+++ src/frontend/sharedComponents/SelectDevice.tsx
@@ -114,7 +114,7 @@ export const SelectDevice = ({
                   receiverDeviceId: deviceId,
                   mapId: 'custom',
                 });
-                navigation.navigate('WaitingForMapAccept', {
+                navigation.navigate('SendingBackgroundMap', {
                   shareId: mapShare.shareId,
                 });
               } catch (err) {

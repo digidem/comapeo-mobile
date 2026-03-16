f94f6aaa39f38e93df7c83abf708226db5512fa3
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-03-16 15:18:37 -0700

    chore: update e2e tests

--- tests/e2e/specs/exchange/everything.test.ts
+++ tests/e2e/specs/exchange/everything.test.ts
@@ -3,33 +3,33 @@ import {describe, it} from 'mocha';
 import {byResourceId, byText, byTextMatches} from '../../utils/selectors';
 
 describe('Exchange - Exchange Settings for Everything', () => {
-  it('should update to Everything and reflect it on main screen', async () => {
-    const changeSettingsBtn = await $(byTextMatches('Change Settings'));
-    await changeSettingsBtn.click();
+  it('should have exchange everything set to defaul', async () => {
     await expect($(byTextMatches('Exchange Everything'))).toBeDisplayed();
     await expect(
       $(byTextMatches('Full size photos and audio')),
     ).toBeDisplayed();
     await expect($(byTextMatches('Uses more storage'))).toBeDisplayed();
-    const everythingOption = await $(
-      byResourceId('EXCHANGE.option-everything'),
-    );
-    await everythingOption.click();
+  });
+
+  it('should have exchange everything selected', async () => {
+    const changeSettingsBtn = await $(byTextMatches('Change Settings'));
+    await changeSettingsBtn.click();
     await expect(
       $(byResourceId('EXCHANGE.radio-selected-everything')),
     ).toBeDisplayed();
-    const saveBtn = await $(byText('Save'));
-    await saveBtn.click();
+    await $(byText('Close')).click();
+    // const saveBtn = await $(byText('Save'));
+    // await saveBtn.click();
 
-    await $(byTextMatches('Exchange everything')).waitForDisplayed();
+    // await $(byTextMatches('Exchange everything')).waitForDisplayed();
 
-    const everythingTitle = await $(byTextMatches('Exchange everything'));
-    expect(await everythingTitle.isDisplayed()).toBe(true);
-    await expect(
-      $(byTextMatches('Full size photos and audio')),
-    ).toBeDisplayed();
-    await expect($(byTextMatches('Uses more storage'))).toBeDisplayed();
-    await $(byResourceId('MAIN.header-back-btn')).click();
-    await $(byResourceId('MAIN.map-screen')).click();
+    // const everythingTitle = await $(byTextMatches('Exchange everything'));
+    // expect(await everythingTitle.isDisplayed()).toBe(true);
+    // await expect(
+    //   $(byTextMatches('Full size photos and audio')),
+    // ).toBeDisplayed();
+    // await expect($(byTextMatches('Uses more storage'))).toBeDisplayed();
+    // await $(byResourceId('MAIN.header-back-btn')).click();
+    // await $(byResourceId('MAIN.map-screen')).click();
   });
 });

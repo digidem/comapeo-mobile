dd95c55d971f169afdd97475e6a0a79a318b7f14
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-04-20 15:32:34 -0700

    chore: update keyboard listener to use useEffect

--- src/frontend/hooks/useKeyboardListener.ts
+++ src/frontend/hooks/useKeyboardListener.ts
@@ -1,29 +1,26 @@
-import {useFocusEffect} from '@react-navigation/native';
-import {useCallback, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {Keyboard} from 'react-native';
 
 export function useKeyboardListener() {
   const [keyboardVisible, setKeyboardVisible] = useState(false);
   const [keyboardHeight, setKeyboardHeight] = useState(0);
 
-  useFocusEffect(
-    useCallback(() => {
-      const keyboardHideUnsub = Keyboard.addListener('keyboardDidHide', () => {
-        setKeyboardVisible(false);
-        setKeyboardHeight(0);
-      });
+  useEffect(() => {
+    const keyboardHideUnsub = Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardVisible(false);
+      setKeyboardHeight(0);
+    });
 
-      const keyboardShowUnsub = Keyboard.addListener('keyboardDidShow', e => {
-        setKeyboardVisible(true);
-        setKeyboardHeight(e.endCoordinates.height);
-      });
+    const keyboardShowUnsub = Keyboard.addListener('keyboardDidShow', e => {
+      setKeyboardVisible(true);
+      setKeyboardHeight(e.endCoordinates.height);
+    });
 
-      return () => {
-        keyboardHideUnsub.remove();
-        keyboardShowUnsub.remove();
-      };
-    }, []),
-  );
+    return () => {
+      keyboardHideUnsub.remove();
+      keyboardShowUnsub.remove();
+    };
+  }, []);
 
   return {keyboardVisible, keyboardHeight};
 }

a9f9d856487c450e733d512dc5078ba4f6926573
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-04-23 08:23:51 -0700

    chore: show guaranteed language

--- src/frontend/screens/Settings/AppSettings/LanguageSettings.tsx
+++ src/frontend/screens/Settings/AppSettings/LanguageSettings.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import {defineMessages, useIntl} from 'react-intl';
-import {ScrollView} from 'react-native';
+import {FlatList, StyleSheet, TouchableOpacity, View} from 'react-native';
 
 import {
   useLocaleActions,
   useLocaleState,
 } from '../../../contexts/LocaleStoreContext';
 import {USABLE_LANGUAGES} from '../../../lib/intl';
-import {SelectOne} from '../../../sharedComponents/SelectOne';
 import {type NativeNavigationComponent} from '../../../sharedTypes/navigation';
+import MaterialIcon from '@react-native-vector-icons/material-icons';
+import {HeaderText} from '../../../sharedComponents/Text/HeaderText';
+import {BodyText} from '../../../sharedComponents/Text/BodyText';
+import {BLUE_GREY} from '../../../lib/styles';
+import {useAppLanguageTag} from '../../../hooks/useAppLanguageTag';
 
 const m = defineMessages({
   title: {
@@ -20,6 +24,14 @@ const m = defineMessages({
     id: '$1screens.LanguageSettings.followSystemPreferences',
     defaultMessage: 'Follow system preferences',
     description: 'Label for language option to follow system preferences',
+  },
+  currentLang: {
+    id: '$1screens.LanguageSettings.currentLang',
+    defaultMessage: 'Current Language',
+  },
+  otherLang: {
+    id: '$1screens.LanguageSettings.otherLang',
+    defaultMessage: 'Other Languages',
   },
 });
 
@@ -33,33 +45,101 @@ const LANGUAGE_OPTIONS = USABLE_LANGUAGES.map(
 
 export const LanguageSettings: NativeNavigationComponent<
   'LanguageSettings'
-> = ({navigation}) => {
-  const {formatMessage: t} = useIntl();
+> = () => {
+  const {formatMessage} = useIntl();
   const locale = useLocaleState();
+  const appLocale = useAppLanguageTag();
   const {setLanguageTag} = useLocaleActions();
 
   const options = React.useMemo(() => {
     return [
       {
         value: null,
-        label: t(m.followSystemPreferences),
+        label: formatMessage(m.followSystemPreferences),
       },
       ...LANGUAGE_OPTIONS,
     ];
-  }, [t]);
+  }, [formatMessage]);
 
   return (
-    <ScrollView testID="languageScrollView">
-      <SelectOne
-        value={locale.languageTag}
-        options={options}
-        onChange={selected => {
-          setLanguageTag(selected);
-          navigation.popTo('AppSettings');
-        }}
-      />
-    </ScrollView>
+    <FlatList
+      testID="languageScrollView"
+      style={styles.container}
+      data={options}
+      ListHeaderComponent={
+        <>
+          <HeaderText style={{marginBottom: 12}} variant="header6">
+            {formatMessage(m.currentLang)}
+          </HeaderText>
+          <View style={styles.currentContainer}>
+            <MaterialIcon
+              name={'language'}
+              size={24}
+              color={'rgba(0, 0, 0, 0.54)'}
+            />
+            <HeaderText variant="header6">
+              {LANGUAGE_OPTIONS.find(o => o.value === appLocale)?.label ??
+                appLocale}
+            </HeaderText>
+          </View>
+          <View style={styles.divider} />
+          <HeaderText
+            style={{marginTop: 30, marginBottom: 15}}
+            variant="header6">
+            {formatMessage(m.otherLang)}
+          </HeaderText>
+        </>
+      }
+      renderItem={({item}) => {
+        const isSelected = item.value === locale.languageTag;
+        return (
+          <TouchableOpacity
+            onPress={() => {
+              setLanguageTag(item.value);
+            }}
+            style={styles.languageContainer}>
+            <MaterialIcon
+              name={
+                isSelected ? 'radio-button-checked' : 'radio-button-unchecked'
+              }
+              size={24}
+              color={'rgba(0, 0, 0, 0.54)'}
+            />
+            <View>
+              <HeaderText variant="header6">{item.label}</HeaderText>
+              {'hint' in item && (
+                <BodyText variant="tinyMeta">{item.hint}</BodyText>
+              )}
+            </View>
+          </TouchableOpacity>
+        );
+      }}
+    />
   );
 };
 
 LanguageSettings.navTitle = m.title;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  languageContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 15,
+    gap: 15,
+  },
+  currentContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 15,
+    marginBottom: 15,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: BLUE_GREY,
+  },
+});

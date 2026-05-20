351233e5e18af95f24d9fc02510ec3811b2f7907
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-05-20 15:57:19 -0700

    chore: make render navigation async

--- src/frontend/screens/ObservationFields/index.test.tsx
+++ src/frontend/screens/ObservationFields/index.test.tsx
@@ -1,1 +1,24 @@
-//write test here
+import {act, screen, userEvent} from '@testing-library/react-native';
+import {setupIntegrationTest} from '../../../../tests/integration/helpers/setupIntegrationTest';
+
+describe('Observation Fields', () => {
+  const integrationSetup = setupIntegrationTest();
+
+  test('has the correct fields', async () => {
+    const user = userEvent.setup();
+    await integrationSetup.renderNavigation({
+      activeProjectId: integrationSetup.projectId,
+    });
+
+    const observationButton = await screen.findByTestId(
+      'MAIN.add-observation-btn',
+    );
+    expect(observationButton).toBeVisible();
+
+    await act(async () => {
+      await user.press(observationButton);
+      const categoryScreen = await screen.findByText('Choose a category');
+      await expect(categoryScreen).toBeVisible();
+    });
+  });
+});

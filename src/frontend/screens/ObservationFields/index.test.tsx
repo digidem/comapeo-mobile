ad59a5c1fc579e0c49af7b64e1d5144781660143
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-05-20 16:09:00 -0700

    chore: add config

--- src/frontend/screens/ObservationFields/index.test.tsx
+++ src/frontend/screens/ObservationFields/index.test.tsx
@@ -1,11 +1,23 @@
-import {act, screen, userEvent} from '@testing-library/react-native';
+import path from 'path';
+import {screen, userEvent} from '@testing-library/react-native';
 import {setupIntegrationTest} from '../../../../tests/integration/helpers/setupIntegrationTest';
+
+const DEFAULT_CONFIG_PATH = path.join(
+  __dirname,
+  '../../../../src/backend/node_modules/@comapeo/default-categories/dist/comapeo-default-categories.comapeocat',
+);
 
 describe('Observation Fields', () => {
   const integrationSetup = setupIntegrationTest();
 
   test('has the correct fields', async () => {
     const user = userEvent.setup();
+
+    const project = await integrationSetup.manager.getProject(
+      integrationSetup.projectId,
+    );
+    await project.$importCategories({filePath: DEFAULT_CONFIG_PATH});
+
     await integrationSetup.renderNavigation({
       activeProjectId: integrationSetup.projectId,
     });
@@ -15,10 +27,11 @@ describe('Observation Fields', () => {
     );
     expect(observationButton).toBeVisible();
 
-    await act(async () => {
-      await user.press(observationButton);
-      const categoryScreen = await screen.findByText('Choose a category');
-      await expect(categoryScreen).toBeVisible();
-    });
+    await user.press(observationButton);
+    const categoryScreen = await screen.findByTestId('MAIN.categories-scrn');
+    expect(categoryScreen).toBeVisible();
+
+    const animalCategory = await screen.findByText('Animal');
+    expect(animalCategory).toBeVisible();
   });
 });

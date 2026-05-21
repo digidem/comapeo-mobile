bfc9ccd7ca84c9b2308e685000dfe69227661604
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-05-21 13:12:37 -0700

    chore: update testing setup

--- tests/integration/helpers/setupIntegrationTest.tsx
+++ tests/integration/helpers/setupIntegrationTest.tsx
@@ -1,4 +1,4 @@
-import {render} from '@testing-library/react-native';
+import {render, renderAsync} from '@testing-library/react-native';
 import type {MapeoManager} from '@comapeo/core';
 import type {MapeoClientApi} from '@comapeo/ipc';
 import {createManager, setUpIPC} from './core';
@@ -70,6 +70,70 @@ export function setupIntegrationTest() {
     get projectId() {
       return projectId;
     },
+    get client() {
+      return client;
+    },
+    get manager() {
+      return manager;
+    },
+  };
+}
+
+export function setupIntegrationTestWithoutProject() {
+  let manager: MapeoManager;
+  let client: MapeoClientApi;
+  let onTeardown: Array<() => unknown> = [];
+
+  beforeEach(async () => {
+    onTeardown = [];
+
+    const managerSetup = await createManager({
+      name: 'test',
+      deviceType: 'mobile',
+    });
+    ({manager} = managerSetup);
+    const {fastifyController} = managerSetup;
+
+    const ipcSetup = setUpIPC({manager});
+    ({client} = ipcSetup);
+    const {stop} = ipcSetup;
+    onTeardown.push(stop);
+
+    await fastifyController.start();
+    onTeardown.push(() => fastifyController.stop());
+  });
+
+  afterEach(async () => {
+    for (const fn of onTeardown) await fn();
+  });
+
+  const renderNavigationAsync = async ({
+    isOnline = true,
+  }: Readonly<{isOnline?: boolean; activeProjectId?: string}> = {}) => {
+    const appProviders = createAppProvidersWrapper({
+      mapeoApi: client,
+      isOnline,
+    });
+    onTeardown.push(appProviders.teardown);
+
+    const {unmountAsync} = await renderAsync(<MockedAppNavigator />, {
+      wrapper: appProviders.wrapper,
+    });
+    const actualTeardown = async () => {
+      await unmountAsync();
+    };
+
+    onTeardown.unshift(actualTeardown);
+
+    return async () => {
+      const result = await actualTeardown();
+      onTeardown = onTeardown.filter(fn => fn !== actualTeardown);
+      return result;
+    };
+  };
+
+  return {
+    renderNavigationAsync,
     get client() {
       return client;
     },

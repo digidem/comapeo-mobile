ded5fbace42d0331dabfd3fa79e2a429ee88b9c1
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-08-20 11:59:33 -0700

    chore: revert to MetricsDiagnosticsPermissionToggle

--- src/frontend/screens/Onboarding/OnboardingPrivacyPolicy.tsx
+++ src/frontend/screens/Onboarding/OnboardingPrivacyPolicy.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
-import {View, ScrollView, StyleSheet, Text} from 'react-native';
+import {View, ScrollView, StyleSheet} from 'react-native';
 import {useIntl, defineMessages} from 'react-intl';
 import {PrivacyPolicy} from '../PrivacyPolicy';
-import {BLACK, BLUE_GREY, WHITE} from '../../lib/styles';
+import {BLUE_GREY, WHITE} from '../../lib/styles';
 import {HeaderText} from '../../sharedComponents/Text/HeaderText';
-import {Checkbox} from '../../sharedComponents/Checkbox';
-import {
-  getDiagnosticsEnabled,
-  setDiagnosticsEnabled,
-} from '@comapeo/core-react-native/sentry';
+import {MetricsDiagnosticsPermissionToggle} from '../../sharedComponents/MetricsDiagnosticsPermissionToggle';
 
 const m = defineMessages({
   navTitle: {
@@ -19,19 +15,10 @@ const m = defineMessages({
     id: 'screens.OnboardingPrivacyPolicy.permissionsTitle',
     defaultMessage: 'Current Permissions',
   },
-  shareDiagnostics: {
-    id: '$1screens.OnboardingPrivacyPolicy.shareDiagnostics',
-    defaultMessage: 'Share Diagnostic Information',
-  },
 });
 
 export const OnboardingPrivacyPolicy = () => {
   const {formatMessage} = useIntl();
-  //this is a non-reactive value
-  const nonReactiveDiagnosticsEnabled = getDiagnosticsEnabled();
-  const [isEnabled, setIsEnabled] = React.useState(
-    nonReactiveDiagnosticsEnabled,
-  );
 
   return (
     <ScrollView contentContainerStyle={styles.scrollContent}>
@@ -41,21 +28,7 @@ export const OnboardingPrivacyPolicy = () => {
         {formatMessage(m.permissionsTitle)}
       </HeaderText>
       <View style={styles.permissionToggleContainer}>
-        <View style={styles.container}>
-          <Text style={styles.permissionText}>
-            {formatMessage(m.shareDiagnostics)}
-          </Text>
-          <Checkbox
-            value={isEnabled}
-            error={false}
-            onPress={() => {
-              const newIsEnabledValue = !isEnabled;
-              setDiagnosticsEnabled(newIsEnabledValue);
-              setIsEnabled(newIsEnabledValue);
-            }}
-            hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}
-          />
-        </View>
+        <MetricsDiagnosticsPermissionToggle />
       </View>
     </ScrollView>
   );
@@ -84,15 +57,5 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: BLUE_GREY,
     borderRadius: 10,
-  },
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: WHITE,
-  },
-  permissionText: {
-    fontSize: 16,
-    color: BLACK,
-    flex: 1,
   },
 });

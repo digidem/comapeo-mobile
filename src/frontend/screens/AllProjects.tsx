0933dbbb7e58fc9ece8cfef81d4b70af963b4ccd
ErikSin <67773827+ErikSin@users.noreply.github.com>
2026-09-23 12:33:40 -0700

    chore: types

--- src/frontend/screens/AllProjects.tsx
+++ src/frontend/screens/AllProjects.tsx
@@ -80,7 +80,7 @@ export const AllProjects = () => {
   );
 
   return (
-    <BottomSheetWrapper closeOnBackButtonPress>
+    <BottomSheetWrapper>
       <FlatList<ProjectListItem>
         contentContainerStyle={{gap: 20, paddingBottom: 2}}
         data={dataWithCurrentProjOnTop}
